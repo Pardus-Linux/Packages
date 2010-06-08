24212ac5009f171b09310cbb7ac418761d29c98b
Ozan Çağlayan <ozan@pardus.org.tr>
2010-06-08 12:43:42 +0000

    Merge from corporate2/devel/system/base/e2fsprogs:
    rev. 93853, by ozan.caglayan on 2010-06-08
    Version bump to 1.41.12
    
    - Version bump,
    - Depend on libuuid from util-linux for avoiding circular dependency.

--- system/base/e2fsprogs/actions.py
+++ system/base/e2fsprogs/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -13,7 +13,9 @@ from pisi.actionsapi import get
 def setup():
     autotools.rawConfigure("--enable-elf-shlibs \
                             --disable-e2initrd-helper \
-                            --disable-libblkid")
+                            --disable-libblkid \
+                            --disable-libuuid \
+                            --disable-uuidd")
 
 def build():
     autotools.make()
