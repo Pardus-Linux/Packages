87630593e729014fde6c80a99c94b7300f762ea2
Ozan Çağlayan <ozan@pardus.org.tr>
2010-06-09 18:20:45 +0000

    Version bump, refactor package, update PCI ID database.

--- system/base/pciutils/actions.py
+++ system/base/pciutils/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -11,18 +11,14 @@ from pisi.actionsapi import get
 
 def build():
     autotools.make('OPT="%s" \
-                    STRIP="" \
                     SHARED="yes" \
-                    PREFIX="/usr" \
                     IDSDIR="/usr/share/misc" \
                     MANDIR="/usr/share/man" \
                     all' % get.CFLAGS())
 
 def install():
     autotools.rawInstall('DESTDIR="%s" \
-                          STRIP="" \
                           SHARED="yes" \
-                          PREFIX="/usr" \
                           IDSDIR="/usr/share/misc" \
                           MANDIR="/usr/share/man" \
                           install-lib' % get.installDIR())
