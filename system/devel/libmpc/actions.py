190312254b96794120e99c590c1471743ee0dd1e
Onur Küçük <onur@pardus.org.tr>
2010-06-10 13:27:39 +0000

    Move libmpc from review to 2011
    BUG:FIXED:13381

---
+++ system/devel/libmpc/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "mpc-%s" % get.srcVERSION()
+
+
+def setup():
+    autotools.configure("--disable-static")
+
+def build():
+    autotools.make()
+
+def check():
+    autotools.make("check")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING.LIB", "README*", "NEWS")
+
