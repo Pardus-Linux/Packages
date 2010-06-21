ba83da2cb90271fce39e1de31b87cac303263687
Onur Küçük <onur@pardus.org.tr>
2010-06-21 07:58:20 +0000

    Remove symlinks since grub no longer understands argv[0], fixes extended usage, and compilation of xulrunner Grab some patches from Fedora

--- system/base/grep/actions.py
+++ system/base/grep/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -20,11 +20,5 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    # Override the default shell scripts... grep knows how to act
-    # based on how it's called
-    for f in ["fgrep","egrep"]:
-        pisitools.remove("/bin/%s" % f)
-        pisitools.dosym("/bin/grep", "/bin/%s" % f)
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "THANKS", "TODO")
