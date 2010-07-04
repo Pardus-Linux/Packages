cb3c1b3ed262acc19e064dc627f3957af160dcd1
Ozan Çağlayan <ozan@pardus.org.tr>
2010-07-04 09:15:54 +0000

    Version bump to 2.87.
    Split the package to sysvinit and sysvinit-tools for systemd
    integration in 2011 (PACKAGES-10).

--- system/base/sysvinit/actions.py
+++ system/base/sysvinit/actions.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
+WorkDir = "sysvinit-%sdsf" % get.srcVERSION()
+
 def build():
-    autotools.make("-C src CC=\"%s\" DISTRO=\"Pardus\" LCRYPT=\"-lcrypt\"" % (get.CC()))
+    autotools.make("-C src CC=\"%s\" CFLAGS=\"%s -D_GNU_SOURCE\" LCRYPT=\"-lcrypt\"" % (get.CC(), get.CFLAGS()))
 
 def install():
-    shelltools.cd("src/")
-    autotools.rawInstall("DISTRO=\"Pardus\" ROOT=%s" % get.installDIR())
+    shelltools.cd("src")
+    autotools.rawInstall("ROOT='%s' STRIP=/bin/true" % get.installDIR())
+
+    pisitools.remove("/bin/pidof")
+    pisitools.dosym("killall5", "/sbin/pidof")
+
+    # FIXME: Fixed in trunk/pisi
+    #autotools.rawInstall("-C src ROOT='%s' STRIP=/bin/true" % get.installDIR())
