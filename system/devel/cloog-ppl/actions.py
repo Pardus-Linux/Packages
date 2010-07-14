ed9b07246831518c537a35fabc9d6c0dcd63750b
Onur Küçük <onur@pardus.org.tr>
2010-07-14 10:24:30 +0000

    cloog-ppl goes to 2011 repository
    BUG:FIXED:13716

---
+++ system/devel/cloog-ppl/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+
+def setup():
+    shelltools.sym(".", "m4")
+    shelltools.sym("LICENSE", "COPYING")
+    for i in ["NEWS", "AUTHORS", "ChangeLog"]:
+        shelltools.touch(i)
+
+    autotools.autoreconf("-vfi")
+    autotools.configure("--disable-static \
+                         --enable-shared \
+                         --with-ppl")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("LICENSE", "README*")
