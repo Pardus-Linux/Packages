69ceb715317f81693dda2a5c1670089b0193c0b2
Onur Küçük <onur@pardus.org.tr>
2010-08-02 09:24:43 +0000

    version bump, cosmetics to actions.py

--- system/devel/elfutils/actions.py
+++ system/devel/elfutils/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -12,15 +12,18 @@ from pisi.actionsapi import get
 
 
 def setup():
-    autotools.autoreconf("-fi")
+    autotools.autoreconf("-vfi")
 
     # Remove -Wall from default flags. The makefiles enable enough warnings
     # themselves, and they use -Werror.
     shelltools.export("CFLAGS", get.CFLAGS().replace("-Wall", ""))
 
     autotools.configure("--program-prefix=\"eu-\" \
+                         --enable-thread-safety \
+                         --disable-nls \
                          --with-zlib \
                          --with-bzlib")
+                         # --with-lzma \
 
 def build():
     autotools.make()
