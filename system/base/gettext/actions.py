1bbde2ea752d83a5ce8ed9bb870426c2202e7479
Onur Küçük <onur@pardus.org.tr>
2010-08-02 08:43:47 +0000

    version bump to 0.18.1.1

--- system/base/gettext/actions.py
+++ system/base/gettext/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -12,23 +12,28 @@ from pisi.actionsapi import get
 
 docdir = "/%s/%s" % (get.docDIR(), get.srcNAME())
 
+shelltools.export("LC_ALL", "C")
+
 def setup():
-    shelltools.export("CFLAGS","%s -Dgcc_is_lint" % get.CFLAGS())
-    shelltools.export("CXXFLAGS","%s -Dgcc_is_lint" % get.CXXFLAGS())
+    # shelltools.export("CFLAGS","%s -Dgcc_is_lint" % get.CFLAGS())
+    # shelltools.export("CXXFLAGS","%s -Dgcc_is_lint" % get.CXXFLAGS())
 
     # Build with --without-included-gettext (will use that of glibc), as we
     # need preloadable_libintl.so for new help2man
-    autotools.autoreconf("-fi")
+    autotools.autoreconf("-vfi")
     autotools.configure("--disable-java \
+                         --disable-native-java \
                          --disable-csharp \
                          --without-included-gettext \
                          --with-included-libcroco \
                          --with-included-glib \
                          --with-included-libxml \
+                         --with-pic=yes \
                          --without-emacs \
                          --disable-openmp \
                          --enable-nls \
                          --enable-shared \
+                         --disable-git \
                          --disable-rpath \
                          --disable-static")
 
