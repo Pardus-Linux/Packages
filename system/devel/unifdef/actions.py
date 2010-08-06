47617dac8b550fa388a6c3d6406f3b45ebef126e
Onur Küçük <onur@pardus.org.tr>
2010-08-06 09:21:50 +0000

    minor version bump for new toolchain

--- system/devel/unifdef/actions.py
+++ system/devel/unifdef/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2009 TUBITAK/UEKAE
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,11 +9,15 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+WorkDir = "unifdef-%s/Debian" % get.srcVERSION()
+
+def setup():
+    autotools.configure()
+
 def build():
     autotools.make('CC="%s" CFLAGS="%s"' % (get.CC(), get.CFLAGS()))
 
 def install():
-    pisitools.dobin("unifdef")
-    pisitools.dobin("unifdefall.sh")
+    autotools.rawInstall('DESTDIR="%s"' % get.installDIR())
+    pisitools.dobin("src/unifdefall.sh")
 
-    pisitools.doman("unifdef.1")
