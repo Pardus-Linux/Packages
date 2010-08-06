5736c89f52a5293ae73332a30c1b32114746173f
Onur Küçük <onur@pardus.org.tr>
2010-08-06 11:26:27 +0000

    version bump, upstream highly suggests to use this version

--- system/devel/ppl/actions.py
+++ system/devel/ppl/actions.py
@@ -11,7 +11,7 @@ from pisi.actionsapi import get
 
 
 def setup():
-    autotools.autoreconf("-vfi")
+    # autotools.autoreconf("-vfi")
     autotools.configure('--disable-rpath \
                          --disable-static \
                          --disable-watchdog \
