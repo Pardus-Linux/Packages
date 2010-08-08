b8a37c2f1bab6e42c3f1c891fbf8ac2e49067ebd
Onur Küçük <onur@pardus.org.tr>
2010-08-08 22:05:39 +0000

    Fix build on 2011 Fix buffer overflow triggered by fortifysource Fix old gnuarchive name Disable rpath Add a lot fixes

--- system/base/tar/actions.py
+++ system/base/tar/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2007 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,13 +10,18 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    autotools.autoreconf("-vfi")
     autotools.configure("--enable-backup-scripts \
                          --bindir=/bin \
                          --libexecdir=/usr/sbin \
+                         --disable-rpath \
                          --enable-nls")
 
 def build():
     autotools.make()
+
+def check():
+    autotools.make("check")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
@@ -31,5 +36,3 @@ def install():
 
     pisitools.dodoc("AUTHORS", "ChangeLog*", "NEWS", "README*", "THANKS")
 
-def check():
-    autotools.make("check")
