ac4a7dfe4ba65368cb72f00d342a7f4fdd16ce82
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-09 17:32:18 +0000

    New stable release (1.9)
    - Split development files
    - Do not ship Lisp bindings
    - Add description

--- system/base/libgpg-error/actions.py
+++ system/base/libgpg-error/actions.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,7 +8,8 @@ from pisi.actionsapi import pisitools
 
 def setup():
     autotools.configure("--enable-nls \
-                         --disable-static")
+                         --disable-rpath \
+                         --disable-languages")
 
 def build():
     autotools.make()
