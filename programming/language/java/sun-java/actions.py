a84df770a9e3b8715dc33f56eb0481d75b26b3e5
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-10 13:31:38 +0000

    Copy sun-java to 2011

---
+++ programming/language/java/sun-java/actions.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006,2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "."
+NoStrip = "/"
+Name = "6u19"
+Arch = "amd64" if get.ARCH() == "x86_64" else "i586"
+
+def setup():
+    shelltools.system("sh jdk-%s-dlj-linux-%s.bin --accept-license" % (Name, Arch))
+
+def install():
+    pisitools.dodir("/opt")
+    shelltools.system("./construct . %s/opt/sun-jdk %s/opt/sun-jre"% (get.installDIR(),get.installDIR()))
+
+    pisitools.dodir("/usr/lib/nsbrowser/plugins")
+
+    # Next generation Java plugin is needed by Firefox 3.6+
+    # http://java.sun.com/javase/6/webnotes/install/jre/manual-plugin-install-linux.html
+    pisitools.dosym("/opt/sun-jre/lib/%s/libnpjp2.so" % Arch.replace("i586", "i386"), "/usr/lib/nsbrowser/plugins/javaplugin.so")
+
+    for doc in ["COPYRIGHT", "LICENSE", "README.html", "README_ja.html", "README_zh_CN.html", "THIRDPARTYLICENSEREADME.txt"]:
+        file = "%s/opt/sun-jdk/%s" % (get.installDIR(), doc)
+        pisitools.dodoc(file)
+        shelltools.unlink(file)
