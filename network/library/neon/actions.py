d6b16651f1715681912dc92b4223ad0b57f51407
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-10 12:21:32 +0000

    Copy neon to 2011

---
+++ network/library/neon/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--with-libxml2 \
+                         --with-expat \
+                         --without-gssapi \
+                         --without-libproxy \
+                         --with-ssl=openssl \
+                         --with-ca-bundle=/etc/ssl/certs/ca-bundle.crt \
+                         --enable-threadsafe-ssl=posix \
+                         --enable-shared \
+                         --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("THANKS", "TODO", "ChangeLog", "AUTHORS", "BUGS", "NEWS", "README", "doc/*.txt")
