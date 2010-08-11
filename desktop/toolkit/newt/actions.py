b33091460fb0f7d603005da7bb6560c7ae1ab802
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-11 09:44:51 +0000

    Merge newt

---
+++ desktop/toolkit/newt/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    # remove static lib
+    pisitools.remove("/usr/lib/libnewt.a")
+
+    pisitools.dodoc("CHANGES", "COPYING")
+
