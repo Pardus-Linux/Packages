04f81ec60e3e4c50865cf26a0513a26913a5afce
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-11 09:52:20 +0000

    Copy docbook-xml4_5 to 2011

---
+++ office/docbook/docbook-xml4_5/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="."
+
+def install():
+    pisitools.insinto("/usr/share/sgml/docbook/xml-dtd-%s" % get.srcVERSION(), "*.dtd")
+    pisitools.insinto("/usr/share/sgml/docbook/xml-dtd-%s" % get.srcVERSION(), "*.mod")
+    pisitools.insinto("/usr/share/sgml/docbook/xml-dtd-%s" % get.srcVERSION(), "docbook.cat")
+    pisitools.insinto("/usr/share/sgml/docbook/xml-dtd-%s/ent" % get.srcVERSION(), "ent/*.ent")
+
+    pisitools.dodoc("ChangeLog", "README")
+
