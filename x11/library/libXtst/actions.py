f1bb18716a17aecc4a82ac16f14b3e6c969a1ab7
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-14 11:53:12 +0000

    Split development files

--- x11/library/libXtst/actions.py
+++ x11/library/libXtst/actions.py
@@ -4,9 +4,12 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    pisitools.dosed("man/Makefile.am", "HAVE_XMLTO", "TRUE")
+    autotools.autoreconf("-vif")
     autotools.configure("--disable-static")
 
 def build():
