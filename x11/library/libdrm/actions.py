153f8952eb26ba8472e8b32b7696ce4bd737ec58
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-14 12:44:11 +0000

    Merge libdrm to 2011

---
+++ x11/library/libdrm/actions.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+
+def setup():
+    autotools.autoreconf("-vif")
+    autotools.configure("--enable-udev \
+                         --enable-nouveau-experimental-api")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
