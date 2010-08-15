1be74cb7fd7792b8af56157b1ffaed976464cc57
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-15 12:42:18 +0000

    Merge libva to 2011

---
+++ x11/library/libva/actions.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    pisitools.dosed("test/Makefile.am", "^SUBDIRS = .*$", "")
+
+    autotools.autoreconf("-vif")
+    autotools.configure("--enable-glx \
+                         --enable-dummy-driver \
+                         --enable-i965-driver")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("COPYING")
