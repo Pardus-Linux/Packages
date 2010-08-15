330426954c35c8f6fbbe9b5d6dc70ace38b1fc09
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-08-15 18:16:08 +0000

    Merge sip for 2011

---
+++ programming/language/python/sip/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import get
+
+def setup():
+    pythonmodules.run('configure.py \
+                       -b /usr/bin \
+                       -d /usr/lib/%s/site-packages \
+                       -e /usr/include/%s \
+                       CFLAGS+="%s" CXXFLAGS+="%s"' % (get.curPYTHON(), get.curPYTHON(), get.CFLAGS(), get.CXXFLAGS()))
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("LICENSE*", "NEWS", "README")
