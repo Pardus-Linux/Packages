416a0e39dba7eed76973832658fd89ab6f66a9f0
Bahadır Kandemir <bahadir@pardus.org.tr>
2010-08-16 11:34:13 +0000

    ip route 2.6.35

---
+++ network/filter/iproute2/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make('CC="%s" RPM_OPT_FLAGS="%s"' % (get.CC(), get.CFLAGS()))
+
+def install():
+    autotools.rawInstall("DESTDIR=\"%s\" \
+                          SBINDIR=/sbin \
+                          DOCDIR=/%s/%s \
+                          MANDIR=/usr/share/man \
+                          " % (get.installDIR(), get.docDIR(), get.srcNAME()))
+
+    pisitools.dodir("/usr/sbin")
+    pisitools.dodir("/var/lib/arpd")
