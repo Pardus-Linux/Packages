926e77d1458d8389d995c6c1fc73d0349dedd7be
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-08-16 21:13:02 +0000

    Merge cairo to 2011

---
+++ desktop/toolkit/gtk/cairo/actions.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-static \
+                         --disable-gtk-doc \
+                         --enable-glitz \
+                         --enable-xlib \
+                         --enable-xlib-xrender \
+                         --enable-xcb \
+                         --enable-ft \
+                         --enable-ps \
+                         --enable-pdf \
+                         --enable-svg \
+                         --enable-png \
+                         --with-x")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.removeDir("/usr/share/gtk-doc")
+    pisitools.dodoc("AUTHORS", "README", "ChangeLog", "NEWS", "COPYING", "COPYING-LGPL-2.1", "COPYING-MPL-1.1")
