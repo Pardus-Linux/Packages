f8951c0a13f6adfa9da153a44f3781c36c06fda8
Onur Küçük <onur@pardus.org.tr>
2010-08-17 13:56:15 +0000

    version bump

--- system/base/cpio/actions.py
+++ system/base/cpio/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2007 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -12,10 +12,14 @@ from pisi.actionsapi import get
 def setup():
     autotools.configure("--enable-nls \
                          --bindir=/bin \
-                         --with-rmt=/usr/sbin/rmt")
+                         --with-rmt=/usr/sbin/rmt \
+                         --disable-rpath")
 
 def build():
     autotools.make()
+
+def check():
+    autotools.make("check")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
@@ -25,5 +29,3 @@ def install():
 
     pisitools.dodoc("ChangeLog", "NEWS", "README")
 
-def check():
-    autotools.make("check")
