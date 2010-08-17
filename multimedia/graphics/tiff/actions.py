4865724fe786d2159c7ef8937462fcd49f2fe684
Onur Küçük <onur@pardus.org.tr>
2010-08-17 10:47:28 +0000

    version bump, split devel part

--- multimedia/graphics/tiff/actions.py
+++ multimedia/graphics/tiff/actions.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-fi")
+    shelltools.export("CFLAGS", "%s -fno-strict-aliasing" % get.CFLAGS())
+
+    autotools.autoreconf("-vfi")
     autotools.configure("--disable-static \
                          --disable-rpath \
                          --without-x \
