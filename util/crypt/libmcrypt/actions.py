37fa175a6dd8638cfc885dc313361f6a8b42e84e
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-17 05:47:42 +0000

    libmcrypt is ready for 2011

---
+++ util/crypt/libmcrypt/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def check():
+    autotools.make("check")
+
+def install():
+    autotools.install()
+
+    # Empty
+    pisitools.removeDir("/usr/lib/libmcrypt")
+
+    pisitools.dodoc("ChangeLog","COPYING.LIB","KNOWN-BUGS","NEWS","README")
