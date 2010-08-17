e5ed12ae52626ca554e509670b048d74d0588265
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-08-17 13:54:30 +0000

    Merge gd

---
+++ multimedia/misc/gd/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-static \
+                         --with-fontconfig \
+                         --with-png \
+                         --with-freetype \
+                         --with-jpeg \
+                         --without-xpm")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dohtml(".")
+    pisitools.dodoc("README*")
