c9d1694f6cf092b90383ad118429dc4b14aeb640
Ozan Çağlayan <ozan@pardus.org.tr>
2010-08-18 21:02:40 +0000

    gobject-introspection is ready.

---
+++ programming/misc/gobject-introspection/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-static \
+                         --disable-gtk-doc-html")
+
+def build():
+    autotools.make("V=1")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING*", "NEWS", "README")
+
