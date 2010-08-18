e656e4fc4b307dc8d280697b577a91ab266e3cd8
Ozan Çağlayan <ozan@pardus.org.tr>
2010-08-18 09:50:57 +0000

    openslp is ready.

---
+++ network/analyzer/openslp/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-static \
+                         --disable-dependency-tracking \
+                         --enable-slpv1 \
+                         --enable-slpv2-security")
+
+def build():
+    autotools.make("RPM_OPT_FLAGS='%s'" % get.CFLAGS())
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.removeDir("/usr/doc")
+
+    pisitools.dohtml("doc/html/*")
+    pisitools.dodoc("AUTHORS", "FAQ", "ChangeLog", "NEWS", "README", "THANKS")
