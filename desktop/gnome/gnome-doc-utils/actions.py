eeac563f87eeaed53492301f21304efe2a5aaf84
Burak Çalışkan <burakclskn@gmail.com>
2010-08-18 23:56:02 +0000

    fix bootstrap problem

--- desktop/gnome/gnome-doc-utils/actions.py
+++ desktop/gnome/gnome-doc-utils/actions.py
@@ -7,8 +7,10 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 def setup():
+    shelltools.copy("tools/gnome-doc-utils.m4", "m4/")
     autotools.autoreconf("-fiv")
     autotools.configure()
 
