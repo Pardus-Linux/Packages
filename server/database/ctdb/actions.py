0ef2b53745133b163ee1311b1afaea9644523e42
Ozan Çağlayan <ozan@pardus.org.tr>
2010-08-19 07:55:50 +0000

    ctdb is ready.

---
+++ server/database/ctdb/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.system("./autogen.sh")
+
+    shelltools.export("CFLAGS", "%s -D_GNU_SOURCE -DCTDB_VERS=\"%s\"" % (get.CFLAGS(), get.srcVERSION()))
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING")
