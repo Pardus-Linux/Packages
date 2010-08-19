668a34401ca7e3cf7ba5e5d3b3b94c4f726b8147
Necdet Yücel <necdetyucel@gmail.com>
2010-08-19 12:37:27 +0000

    merge perl-Tidy

---
+++ programming/language/perl/perl-Tidy/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import perlmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "Perl-Tidy-%s" % get.srcVERSION()
+
+def setup():
+    perlmodules.configure()
+
+def build():
+    perlmodules.make()
+
+def check():
+    perlmodules.make("test")
+
+def install():
+    perlmodules.install()
+    pisitools.insinto("%s/%s/examples/" % (get.docDIR(), get.srcNAME()), "examples/*")
+    pisitools.removeDir("/usr/lib/perl5/vendor_perl/%s/%s-linux-thread-multi/" % (get.curPERL(), get.ARCH()))
