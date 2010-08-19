fc7ce75d9813ad266b939341e7287ad8d82b0fbe
Ozan Çağlayan <ozan@pardus.org.tr>
2010-08-19 08:05:06 +0000

    speex is ready.

---
+++ multimedia/sound/speex/actions.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir="speex-%s" % get.srcVERSION().replace("_","")
+
+def setup():
+    shelltools.export("CFLAGS", "%s -D_FILE_OFFSET_BITS=64" % get.CFLAGS())
+
+    pisitools.dosed("libspeex/Makefile.am", "noinst_PROGRAMS", "check_PROGRAMS")
+
+    autotools.autoreconf("-fi")
+    autotools.configure("--enable-ogg \
+                         --enable-sse \
+                         --disable-static")
+
+    # Remove rpath from speexenc and speexdec
+    pisitools.dosed("libtool", "^hardcode_libdir_flag_spec=.*", "hardcode_libdir_flag_spec=\"\"")
+    pisitools.dosed("libtool", "^runpath_var=LD_RUN_PATH", "runpath_var=DIE_RPATH_DIE")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s docdir=/usr/share/doc/speex" % get.installDIR())
+
+    pisitools.dodoc("README")
