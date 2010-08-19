45e4eccbe5ce2ef3a1ddd62f8868a822804ee22e
Necdet Yücel <necdetyucel@gmail.com>
2010-08-19 13:11:00 +0000

    merge pyzor

---
+++ network/mail/pyzor/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import pythonmodules
+
+def build():
+    pythonmodules.compile()
+
+def install():
+    pythonmodules.install()
+    pisitools.domove("/usr/bin/pyzord", "/usr/sbin")
+    pisitools.dohtml("docs/usage.html")
+    pisitools.doman("docs/pyzor*")
