4aa387b36b90e3f172ebdb0dbab1b39edb646c2a
Ozan Çağlayan <ozan@pardus.org.tr>
2010-08-19 09:15:11 +0000

    Version bump to 0.14.2  Split devel packages, ship glib bindings through this package.
    Only binding left to the poppler-bindings package is qt4 thus
    I'll rename that package to poppler-qt.

---
+++ office/postscript/poppler/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-static \
+                         --disable-poppler-qt \
+                         --disable-poppler-qt4 \
+                         --disable-gtk-doc-html \
+                         --disable-zlib \
+                         --enable-cairo-output \
+                         --enable-xpdf-headers \
+                         --enable-libjpeg \
+                         --enable-libopenjpeg")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.removeDir("/usr/share/gtk-doc")
+    pisitools.dodoc("README", "AUTHORS", "ChangeLog", "NEWS", "README-XPDF", "TODO")
