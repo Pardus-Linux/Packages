25c8d3e7133f8220e1e3bb22687a4515b48709ef
Onur Küçük <onur@pardus.org.tr>
2010-08-19 16:12:15 +0000

    libnut is ready

---
+++ multimedia/converter/libnut/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+#
+# nut is done with checkouts from
+# svn://svn.mplayerhq.hu/nut/src/trunk
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "nut"
+
+def build():
+    shelltools.export("CFLAGS", "%s -fPIC" % get.CFLAGS())
+    autotools.make("-j1 CC=%s" % get.CC())
+
+def install():
+    autotools.rawInstall("PREFIX=%s/usr" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "README*")
