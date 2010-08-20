c2e29c8f07830fee2b6d8579c63c8f2f1c8ef719
Onur Küçük <onur@pardus.org.tr>
2010-08-20 19:18:26 +0000

    a52dec is ready

---
+++ multimedia/sound/a52dec/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-vfi")
+    autotools.configure("--enable-shared \
+                         --disable-static \
+                         --enable-djbfft")
+
+def build():
+    autotools.make('CFLAGS="%s"' % get.CFLAGS())
+
+def install():
+    autotools.rawInstall('DESTDIR="%s" docdir=/%s/%s/html' % (get.installDIR(), get.docDIR(), get.srcNAME()))
+
+    pisitools.insinto("/usr/include/a52dec", "liba52/a52_internal.h")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "doc/liba52.txt")
