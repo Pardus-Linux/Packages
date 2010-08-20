5523ab294440a6310d316671840f90b289d55d26
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-20 20:01:38 +0000

    startup-notification is ready

---
+++ desktop/misc/startup-notification/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--disable-static")
+
+    # Put flags in front of the libs. Needed for --as-needed.
+    replace = (r"(\\\$deplibs) (\\\$compiler_flags)", r"\2 \1")
+    pisitools.dosed("libtool", *replace)
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "doc/startup-notification.txt")
