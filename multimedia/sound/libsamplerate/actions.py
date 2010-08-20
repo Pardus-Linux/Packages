20a3dcc4628ddbbb87fb7cbbe290d934423b4d0c
Onur Küçük <onur@pardus.org.tr>
2010-08-20 13:33:01 +0000

    libsamplerate is ready

---
+++ multimedia/sound/libsamplerate/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import libtools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-vfi")
+    libtools.libtoolize("--force --install")
+    autotools.configure("--with-pic \
+                         --enable-sndfile \
+                         --disable-static \
+                         --enable-shared \
+                         --disable-fftw \
+                         --disable-dependency-tracking")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README")
