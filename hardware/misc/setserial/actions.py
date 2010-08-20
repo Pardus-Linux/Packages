9cc9d53c8f965ebd3a0f4345131c7355862b38e3
Onur Küçük <onur@pardus.org.tr>
2010-08-20 20:49:59 +0000

    setserial is ready

---
+++ hardware/misc/setserial/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+docdir = "%s/%s" % (get.docDIR(), get.srcNAME())
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make("setserial")
+
+def install():
+    pisitools.doman("setserial.8")
+    pisitools.dobin("setserial", "/bin")
+
+    pisitools.insinto("/etc", "serial.conf")
+    pisitools.dodoc("README")
+    pisitools.dodir("%s/txt" % docdir)
+    pisitools.insinto("%s/txt" % docdir, "Documentation/*")
+
