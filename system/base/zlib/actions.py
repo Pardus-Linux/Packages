a8b2439545e1d44257e274d6a310fd0da09c0500
Ozan Çağlayan <ozan@pardus.org.tr>
2010-08-20 11:20:22 +0000

    Version bump and split devel packages.

--- system/base/zlib/actions.py
+++ system/base/zlib/actions.py
@@ -12,18 +12,31 @@ from pisi.actionsapi import get
 
 def setup():
     shelltools.makedirs("m4")
-    autotools.autoreconf("-fi")
+    shelltools.makedirs("contrib/minizip/m4")
 
-    autotools.configure("--disable-static")
+    shelltools.copy("minigzip.c", "contrib/minizip")
 
+    autotools.rawConfigure("--libdir=/usr/lib \
+                            --includedir=/usr/include \
+                            --prefix=/usr")
 
 def build():
     autotools.make()
 
+    shelltools.cd("contrib/minizip")
+    autotools.autoreconf("-vi")
+
+    autotools.configure()
+    autotools.make()
+
+def check():
+    autotools.make("test")
+
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     # Copy zlib to /lib
+    pisitools.remove("/usr/lib/*.a")
     pisitools.domove("/usr/lib/libz*", "/lib")
 
     # Create symlinks in /usr/lib
@@ -31,9 +44,9 @@ def install():
     pisitools.dosym("libz.so.%s" % get.srcVERSION(), "/usr/lib/libz.so.1")
     pisitools.dosym("libz.so.1", "/usr/lib/libz.so")
 
-    for header in ["zconf.h","zlib.h","zutil.h"]:
-        pisitools.insinto("/usr/include", header)
+    # Install minizip
+    autotools.rawInstall("-C contrib/minizip DESTDIR=%s" % get.installDIR())
 
     pisitools.doman("zlib.3")
-    pisitools.dodoc("FAQ", "README", "ChangeLog", "algorithm.txt")
+    pisitools.dodoc("FAQ", "README", "ChangeLog", "doc/algorithm.txt", "example.c")
 
