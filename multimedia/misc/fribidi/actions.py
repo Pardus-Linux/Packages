ce7ee62dbb7dcb13f452cdacd7da4c1f726fde95
Serdar Dalgıç <serdar@pardus.org.tr>
2010-08-20 11:41:29 +0000

    Split devel package and make the package ready for 2011

---
+++ multimedia/misc/fribidi/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--disable-static \
+                         --with-pic")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "NEWS", "README", "ChangeLog", "THANKS", "TODO")
