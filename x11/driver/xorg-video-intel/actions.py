55427d4f2748b49505199616a4d611cb23e2dcce
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-20 06:18:16 +0000

    xorg-video-intel is ready

---
+++ x11/driver/xorg-video-intel/actions.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+WorkDir = "xf86-video-intel-%s" % get.srcVERSION()
+
+def setup():
+    autotools.autoreconf("-vif")
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
