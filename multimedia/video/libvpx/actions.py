6a7e2446336e28057d6aa80f101fecb9b772d0cf
Onur Küçük <onur@pardus.org.tr>
2010-08-20 12:55:06 +0000

    libvpx is ready

---
+++ multimedia/video/libvpx/actions.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "libvpx"
+# libdir = "lib64" if get.ARCH() == "x86_64" else "lib"
+libdir = "lib"
+
+def setup():
+    shelltools.export("LC_ALL", "en_US.UTF-8")
+    shelltools.export("CFLAGS", "%s -O3" % get.CFLAGS())
+
+    autotools.rawConfigure("--prefix=/usr \
+                            --libdir=/usr/%s \
+                            --enable-pic \
+                            --enable-vp8 \
+                            --enable-shared \
+                            --enable-runtime-cpu-detect \
+                            --enable-postproc \
+                            --enable-multithread \
+                            --disable-install-docs \
+                            --disable-debug \
+                            --disable-debug-libs" % libdir)
+
+
+def build():
+    autotools.make("verbose=true")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.remove("/usr/%s/*.a" % libdir)
+
+    pisitools.dodoc("AUTHORS", "CHANGELOG", "LICENSE", "PATENTS")
