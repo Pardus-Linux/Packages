b869c13f6ac455deb2543914c492ec73c8ee026a
Onur Küçük <onur@pardus.org.tr>
2010-08-20 12:13:32 +0000

    liboil is ready

---
+++ multimedia/misc/liboil/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.export("CFLAGS", "%s -O2" % get.CFLAGS())
+    autotools.configure("--disable-static \
+                         --disable-gtk-doc")
+
+def build():
+    autotools.make("-j1")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "BUG-REPORTING", "HACKING", "NEWS", "README")
+    pisitools.removeDir("/usr/share/gtk-doc")
