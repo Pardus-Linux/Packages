2df4dc19cc21857188fa3239f448c5aae63ffe15
Onur Küçük <onur@pardus.org.tr>
2010-08-20 12:48:05 +0000

    jasper is ready

---
+++ multimedia/graphics/jasper/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import libtools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-fi")
+    libtools.libtoolize("--force --install")
+
+    autotools.configure("--enable-libjpeg \
+                         --enable-opengl \
+                         --enable-shared \
+                         --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("NEWS", "README", "doc/*")
