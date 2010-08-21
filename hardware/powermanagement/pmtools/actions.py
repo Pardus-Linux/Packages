9c64e8436ae9431448d99cdc61534c08df220ef8
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-21 20:22:36 +0000

    pmtools is ready for 2011

---
+++ hardware/powermanagement/pmtools/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    # Don't build acpixtract as a newer one is shipped with acpica
+    pisitools.dosed("Makefile", "acpixtract ", "")
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dosbin("acpidump/acpidump")
+    pisitools.dobin("madt/madt")
+
+    pisitools.newdoc("madt/README", "README.madt")
+    pisitools.dodoc("COPYING", "README")
