fbd38cb28eebcf82783fb99d9fc4026185819b11
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-08-21 08:45:54 +0000

    Merge PyQt

---
+++ programming/language/python/PyQt/actions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import get
+
+WorkDir="%s-x11-gpl-%s" % (get.srcNAME(), get.srcVERSION())
+
+def setup():
+    pisitools.dosed("configure.py", "  check_license()", "# check_license()")
+    pythonmodules.run("configure.py -q /usr/bin/qmake")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%(DESTDIR)s INSTALL_ROOT=%(DESTDIR)s" % {'DESTDIR':get.installDIR()})
+
+    pisitools.dohtml("doc/html/*")
+    pisitools.dosym("/usr/share/doc/%s/html/classes.html" % get.srcNAME(),"/usr/share/doc/%s/html/index.html" % get.srcNAME())
+
+    pisitools.dodoc("NEWS", "README", "THANKS", "LICENSE*", "GPL*", "OPENSOURCE*")
+
