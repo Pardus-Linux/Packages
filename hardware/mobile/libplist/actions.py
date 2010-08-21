7eeae868e4476ebcd9b517e301d9595fa153a600
İnanç Yıldırgan <inancyildirgan@gmail.com>
2010-08-21 01:04:29 +0000

    split package ready for 2011

---
+++ hardware/mobile/libplist/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "%s-%s" % (get.srcNAME(),get.srcVERSION())
+
+def setup():
+    cmaketools.configure("-DCMAKE_SKIP_RPATH=ON")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING", "COPYING.LESSER", "README")
