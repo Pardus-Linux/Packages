d255349100fe56ea65f9ea777cee511a954a4c6a
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-22 13:07:36 +0000

    libglade is ready for 2011

---
+++ desktop/toolkit/gtk/libglade/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005,2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--disable-static \
+                         --disable-gtk-doc")
+
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS","ChangeLog", "COPYING", "NEWS", "README")
