4d1bef49db7afcb71b44d4a5e189d3a7f1010ecb
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-08-22 07:34:15 +0000

    No need for devel in this package. Add missing dep

--- desktop/toolkit/gtk/gtk-doc/actions.py
+++ desktop/toolkit/gtk/gtk-doc/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--disable-static")
+    autotools.configure("--disable-static --disable-scrollkeeper")
 
 def build():
     autotools.make()
