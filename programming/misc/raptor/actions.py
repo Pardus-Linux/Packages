de9e14b378fb82d109b36ea6469bec23fb4be8b5
Necdet Yücel <necdetyucel@gmail.com>
2010-08-22 11:06:21 +0000

    raptor is ready

---
+++ programming/misc/raptor/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "raptor2-%s" % get.srcVERSION()
+
+def setup():
+    autotools.autoreconf("-fi")
+
+    autotools.configure("--with-xml-parser=libxml\
+                         --with-www=curl \
+                         --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README")
