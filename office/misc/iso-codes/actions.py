9d7be981f9645c8f0014b14927d016e90a8c652f
Burak Çalışkan <burakclskn@gmail.com>
2010-08-22 12:21:14 +0000

    copy iso-codes

---
+++ office/misc/iso-codes/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.domove("/usr/share/pkgconfig/iso-codes.pc","/usr/lib/pkgconfig")
+    pisitools.removeDir("/usr/share/pkgconfig")
+
+    pisitools.dodoc("ChangeLog","LICENSE", "README", "TODO")
