6e970a3564124ad7fbb0e16ff09f54a4f6faf9ed
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-08-22 18:50:37 +0000

    Add liblqr

---
+++ multimedia/misc/liblqr/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "%s-1-%s" % (get.srcNAME(), get.srcVERSION())
+
+def setup():
+    autotools.configure("--disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.removeDir("/usr/share/man")
+
+    pisitools.dodoc("AUTHORS", "COPYING*", "ChangeLog", "README", "NEWS")
