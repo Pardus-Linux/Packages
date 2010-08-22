a833412c3958904a43d8dcc3022f7365ea853f4b
Burak Çalışkan <burakclskn@gmail.com>
2010-08-22 12:46:43 +0000

    copy libxklavier

---
+++ programming/misc/libxklavier/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--disable-static\
+                         --disable-rpath\
+                         --enable-xkb-support\
+                         --enable-xmodmap-support")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("NEWS", "README", "CREDITS", "AUTHORS", "ChangeLog")
