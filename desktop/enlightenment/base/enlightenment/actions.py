007d2b85129281c93c0a0a6f8056fbd1b6fd2e42
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-08-23 10:23:17 +0000

    enlightenment is ready

---
+++ desktop/enlightenment/base/enlightenment/actions.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.export("AUTOPOINT", "/bin/true")
+
+    autotools.autoreconf("-fi")
+    autotools.configure("--prefix=/usr \
+                         --disable-static \
+                         --enable-shared \
+                         --enable-nls \
+                         --enable-pam \
+                         --disable-rpath \
+                         --disable-illume \
+                         --disable-illume2 \
+                         --with-x")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING", "README")
