12e8d42bc3d78b93dfef34c36c1cc46705c2a2c4
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-08-23 10:22:36 +0000

    evas is ready

---
+++ desktop/enlightenment/base/evas/actions.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--enable-gl-x11 \
+                         --enable-fribidi \
+                         --enable-buffer \
+                         --enable-software-xlib \
+                         --enable-xrender-x11 \
+                         --enable-pthreads \
+                         --enable-async-events \
+                         --enable-async-preload \
+                         --disable-pipe-render \
+                         --disable-async-render \
+                         --with-x \
+                         --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING*", "README")
