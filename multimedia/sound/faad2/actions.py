280f969abbf62bfbececad9bcc0102ba693600d1
Onur Küçük <onur@pardus.org.tr>
2010-08-23 13:35:53 +0000

    faad2 is in

---
+++ multimedia/sound/faad2/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+
+def setup():
+    pisitools.dosed("libfaad/Makefile.am", "iquote ", "I")
+    autotools.autoreconf("-vfi")
+
+    autotools.configure("--without-xmms \
+                         --with-drm \
+                         --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "README.linux", "TODO")
