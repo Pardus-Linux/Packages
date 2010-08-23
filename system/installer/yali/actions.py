ade8265e3ca0043b2264f0187ad23480dddb2944
Mete Alpaslan <mete@pardus.org.tr>
2010-08-23 07:39:07 +0000

    yali ready for test

---
+++ system/installer/yali/actions.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "yali-2.4.0_20100823"
+def setup():
+    if get.ARCH() == "i686":
+        repo_uri = "http://packages.pardus.org.tr/pardus/2011/devel/i686/pisi-index.xml.bz2"
+    else:
+        repo_uri = "http://packages.pardus.org.tr/pardus/2011/devel/x86_64/pisi-index.xml.bz2"
+
+    pisitools.dosed("yali/constants.py", "@REPO_URI@", repo_uri)
+
+def build():
+    pythonmodules.compile()
+
+def install():
+    pythonmodules.install()
