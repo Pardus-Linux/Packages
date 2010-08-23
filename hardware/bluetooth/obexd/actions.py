93f5493957c8acab6dbea0464aab707b3475e339
Ozan Çağlayan <ozan@pardus.org.tr>
2010-08-23 07:56:44 +0000

    obexd is ready

---
+++ hardware/bluetooth/obexd/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-server \
+                         --disable-dependency-tracking")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING", "README", "doc/client-api.txt", "test/send-files")
