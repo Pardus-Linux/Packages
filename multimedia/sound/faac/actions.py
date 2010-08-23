1a15dfedbb1460aaa704fdc10d33f813a46771cf
Onur Küçük <onur@pardus.org.tr>
2010-08-23 13:31:25 +0000

    faac is in

---
+++ multimedia/sound/faac/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import libtools
+from pisi.actionsapi import get
+
+# WorkDir = "faac"
+
+def setup():
+    autotools.autoreconf("-vfi")
+    #libtools.libtoolize("--force --install")
+    autotools.configure("--disable-static \
+                         --with-external-mp4v2")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("ChangeLog", "AUTHORS", "NEWS", "README", "TODO", "docs/libfaac.pdf")
