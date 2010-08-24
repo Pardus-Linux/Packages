c1e35f5e4216671efca51c10debb6c8628ad6e01
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-24 10:59:07 +0000

    cln is ready for 2011

---
+++ science/mathematics/cln/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-static")
+
+def build():
+    autotools.make()
+
+def check():
+    autotools.make("check")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("ChangeLog", "COPYING", "TODO*", "NEWS", "README")
