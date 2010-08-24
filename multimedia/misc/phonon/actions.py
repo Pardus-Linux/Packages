8e037219fe94e4de247de0e592768aa9d96a6e09
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-08-24 18:25:55 +0000

    Recompile phonon with pulseaudio and enable gstreamer backend

--- multimedia/misc/phonon/actions.py
+++ multimedia/misc/phonon/actions.py
@@ -9,6 +9,8 @@ import os
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import qt4
+from pisi.actionsapi import kde4
 
 WorkDir = "%s-%s" % (get.srcNAME(), get.srcVERSION().partition("_")[0])
 
@@ -18,19 +20,10 @@ def setup():
 def install():
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    """
-    #move kde related stuff to KDE dirs
-    pisitools.dodir("/usr/kde/4/lib/kde4/plugins/phonon_backend")
-    pisitools.domove("%s/lib/kde4/plugins/phonon_backend/*" % qtPrefix, "/usr/kde/4/lib/kde4/plugins/phonon_backend")
-
-    pisitools.dodir("/usr/kde/4/share/kde4/services/phononbackends")
-    pisitools.domove("%s/share/kde4/services/phononbackends/*" % qtPrefix, "%s/share/kde4/services/phononbackends" % kdePrefix)
-
     #pure Qt4 apps look for this path to use phonon backends
-    pisitools.dodir("%s/plugins/phonon_backend" % qtPrefix)
-    pisitools.dosym("%s/lib/kde4/plugins/phonon_backend/phonon_gstreamer.so" % kdePrefix, "%s/plugins/phonon_backend/libphonon_gstreamer.so" % qtPrefix)
-    pisitools.dosym("%s/lib/kde4/plugins/phonon_backend/phonon_xine.so" % kdePrefix, "%s/plugins/phonon_backend/libphonon_xine.so" % qtPrefix)
-    """
+    pisitools.dodir("%s/phonon_backend" % qt4.plugindir)
+    pisitools.dosym("%s/plugins/phonon_backend/phonon_gstreamer.so" % kde4.modulesdir, "%s/phonon_backend/libphonon_gstreamer.so" % qt4.plugindir)
+    #pisitools.dosym("%s/plugins/phonon_backend/phonon_xine.so" % kde4.modulesdir, "%s/phonon_backend/libphonon_xine.so" % qt4.plugindir)
 
     #some applications like mediaplayer example of Qt needs this #11648
     pisitools.dosym("/usr/include/KDE/Phonon", "/usr/include/Phonon")
