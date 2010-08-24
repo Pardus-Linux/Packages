ad144e9bfaebb4497e74c8e989af9f6711a81b6e
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-08-24 07:24:42 +0000

    libecap is ready

---
+++ programming/misc/libecap/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--enable-shared \
+                         --disable-static \
+                         --enable-std-include")
+
+    flagorderrx = (r"(\\\$deplibs \\\$postdep_objects) (\\\$compiler_flags)", r"\2 \1")
+    pisitools.dosed("libtool", *flagorderrx)
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("CREDITS", "LICENSE", "NOTICE", "README")
