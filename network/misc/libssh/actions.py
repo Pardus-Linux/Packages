d2bc40a7dd504b2fa41cc1f0ab02c71f1174318b
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-24 10:10:11 +0000

    libssh is ready for 2011
    - Bump to new stable release
    - Split devel package
    - Do not build documents

---
+++ network/misc/libssh/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+shelltools.export("HOME", "%s" % get.workDIR())
+
+def setup():
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    cmaketools.configure(sourceDir="..")
+
+def build():
+    shelltools.cd("build")
+    cmaketools.make()
+
+def install():
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    shelltools.cd("..")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "INSTALL", "README")
