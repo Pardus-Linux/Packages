35809055c932e6d9aef13ef70cf64cec2bda2428
Onur Küçük <onur@pardus.org.tr>
2010-08-25 16:12:58 +0000

    openal is in

---
+++ multimedia/sound/openal/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "openal-soft-%s" % get.srcVERSION()
+
+def setup():
+    cmaketools.configure("-DALSA=1 \
+                          -DPULSEAUDIO=1 \
+                          -DOSS=1")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "alsoftrc.sample")
