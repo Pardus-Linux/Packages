ce2a8718dec95241518791191117eabf3ca20230
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-08-25 08:12:09 +0000

    gssdp is ready

---
+++ network/analyzer/gssdp/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-static \
+                         --with-html-dir=%s/usr/share/doc/%s/html"
+                         % (get.installDIR(), get.srcNAME()))
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
