7c8c2b715724948d68c2a6ea67405ea227401647
Onur Küçük <onur@pardus.org.tr>
2010-08-25 12:38:19 +0000

    libebml is in

---
+++ multimedia/misc/libebml/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    pisitools.dosed("make/linux/Makefile", "CXXFLAGS=", "CXXFLAGS+=")
+    shelltools.export("CFLAGS", "%s -fPIC" % get.CFLAGS())
+    shelltools.export("CXXFLAGS", "%s -fPIC" % get.CXXFLAGS())
+
+def build():
+    shelltools.cd("make/linux")
+    autotools.make("PREFIX=/usr")
+
+def install():
+    shelltools.cd("make/linux")
+    autotools.install("libdir=%s/usr/lib" % get.installDIR())
+
+    # No static libs
+    pisitools.remove("/usr/lib/*.a")
+
+    pisitools.dodoc("../../LICENSE.*")
