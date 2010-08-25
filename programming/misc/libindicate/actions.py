db319d86517278ecdfa75a15b822c49cf354cfcc
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-08-25 13:43:59 +0000

    Add libindicate twins

---
+++ programming/misc/libindicate/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-vfi")
+
+    autotools.configure("--disable-gtk-doc --disable-static --enable-introspection")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "README", "NEWS")
+
+    pisitools.removeDir("/usr/share/gtk-doc")
