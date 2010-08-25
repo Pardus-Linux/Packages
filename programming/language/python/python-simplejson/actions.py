11ed2ad28059168976e6f49ba32371b1f5776ef2
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-08-25 07:14:27 +0000

    remove strict dependency

---
+++ programming/language/python/python-simplejson/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import get
+
+WorkDir = "simplejson-%s" % get.srcVERSION()
+
+def install():
+    pythonmodules.install()
+
+    pisitools.insinto("%s/%s/html" % (get.docDIR(), get.srcNAME()),"docs/*")
+    pisitools.dodoc("LICENSE.txt")
