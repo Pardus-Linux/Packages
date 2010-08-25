68fcfaa09531f11ffc6ca945c0ec6e28ac2d1573
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-08-25 11:16:32 +0000

    fix sandbox violation when ccache is enabled

--- network/analyzer/gssdp/actions.py
+++ network/analyzer/gssdp/actions.py
@@ -6,13 +6,14 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+
+shelltools.export("HOME", get.workDIR())
 
 def setup():
     autotools.autoreconf("-fi")
-    autotools.configure("--disable-static \
-                         --with-html-dir=%s/usr/share/doc/%s/html"
-                         % (get.installDIR(), get.srcNAME()))
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
