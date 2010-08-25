065177a691b81483f34e82717ee8c3d807aa5232
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-08-25 13:22:00 +0000

    python-evas is ready

---
+++ programming/language/python/python-evas/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import pythonmodules
+
+def build():
+    # we do have r49539, setup.cfg and setup.py are wrong
+    pisitools.dosed("setup.*", "49539", "0")
+
+    pythonmodules.compile()
+
+def install():
+    pythonmodules.install()
+
