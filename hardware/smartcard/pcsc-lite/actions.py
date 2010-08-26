763a733406990a0068de0973fc3874244f20d307
Fatih Aşıcı <fatih@pardus.org.tr>
2010-08-26 07:41:59 +0000

    pcsc-lite is ready for 2011

---
+++ hardware/smartcard/pcsc-lite/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-fi")
+
+    autotools.configure("--enable-usbdropdir=/usr/lib/pcsc/drivers \
+                         --enable-libhal \
+                         --disable-dependency-tracking \
+                         --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "DRIVERS", "HELP", "NEWS",
+                    "README", "SECURITY", "doc/README.DAEMON")
