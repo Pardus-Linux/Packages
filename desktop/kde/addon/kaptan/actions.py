54028400c9a7e6f403ba268843191182c5222d0e
Renan Çakırerk <renan@pardus.org.tr>
2010-08-26 05:39:24 +0000

    remove kaptan: no svn history

--- desktop/kde/addon/kaptan/actions.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-
-from pisi.actionsapi import pythonmodules
-
-def install():
-    pythonmodules.install()
