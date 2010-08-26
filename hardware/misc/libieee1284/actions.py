fc5829511215fd6658e699ba3cef6d0ad3cce754
Ozan Çağlayan <ozan@pardus.org.tr>
2010-08-26 08:07:36 +0000

    $1 is ready

---
+++ hardware/misc/libieee1284/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-static")
+
+def configure():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "NEWS", "TODO", "README", "doc/interface*")
