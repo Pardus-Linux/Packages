c245af0b4eab44ffbd72750f0e1aa68eae57219a
Ozan Çağlayan <ozan@pardus.org.tr>
2010-08-26 07:53:39 +0000

    $1 is ready

---
+++ office/misc/ebook-tools/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+
+    cmaketools.configure(sourceDir="..")
+
+def build():
+    shelltools.cd("build")
+    cmaketools.make()
+
+def install():
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    shelltools.cd("..")
+
+    pisitools.remove("/usr/bin/lit2epub")
+
+    pisitools.dodoc("README", "TODO", "LICENSE")
+
