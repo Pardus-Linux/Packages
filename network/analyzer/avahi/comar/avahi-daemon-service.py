fa841f4e919585f0f6618ec7ed668d848fc047bf
Ozan Çağlayan <ozan@pardus.org.tr>
2010-08-28 12:20:40 +0000

    Ship more bindings, split autoipd and dnsconfd daemons, fix autoipd user/group and a lot of other improvements.

--- network/analyzer/avahi/comar/avahi-daemon-service.py
+++ network/analyzer/avahi/comar/avahi-daemon-service.py
@@ -1,8 +1,8 @@
 from comar.service import *
 
 serviceType="server"
-serviceDesc = _({"en": "Avahi ZeroConf Server",
-                 "tr": "Avahi ZeroConf Sunucusu"})
+serviceDesc = _({"en": "Avahi Server",
+                 "tr": "Avahi Hizmeti"})
 
 @synchronized
 def start():
@@ -10,27 +10,14 @@ def start():
                  args="-D",
                  donotify=True)
 
-    startService(command="/usr/sbin/avahi-dnsconfd",
-                 args="-D",
-                 pidfile="/var/run/avahi-daemon.pid",
-                 donotify=False)
-
 @synchronized
 def stop():
-    stopService(command="/usr/sbin/avahi-dnsconfd",
-                args="-k",
-                donotify=False)
-
     stopService(command="/usr/sbin/avahi-daemon",
                 args="-k",
                 donotify=True)
 
 def reload():
     stopService(command="/usr/sbin/avahi-daemon",
-                args="-r",
-                donotify=True)
-
-    stopService(command="/usr/sbin/avahi-dnsconfd",
                 args="-r",
                 donotify=True)
 
