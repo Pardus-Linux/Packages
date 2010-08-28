749d0bb424115ec5fbf903f736d428405f8786d7
Ozan Çağlayan <ozan@pardus.org.tr>
2010-08-28 13:51:45 +0000

    ready

---
+++ hardware/printer/psutils/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "psutils"
+
+def build():
+    autotools.make("-f Makefile.unix CC='%s' RPM_OPT_FLAGS='%s'" % (get.CC(), get.CFLAGS()))
+
+def install():
+    autotools.make("-f Makefile.unix MANDIR=%s/usr/share/man/man1 DESTDIR=%s install" % (get.installDIR(), get.installDIR()))
+
+    pisitools.dodoc("README", "LICENSE")
