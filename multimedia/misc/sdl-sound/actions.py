6f137442b033981211fec29315a41d80fe3c3a6d
Onur Küçük <onur@pardus.org.tr>
2010-08-29 18:17:47 +0000

    sdl-sound is ready

---
+++ multimedia/misc/sdl-sound/actions.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "SDL_sound-%s" % get.srcVERSION()
+
+def setup():
+    shelltools.export("CFLAGS", "%s -I/usr/include/libmodplug" % get.CFLAGS())
+
+    for f in ["NEWS", "AUTHORS", "ChangeLog"]:
+        shelltools.touch(f)
+
+    autotools.autoreconf("-fi")
+    pisitools.dosed("configure", "modplug\.h", "libmodplug/modplug.h")
+
+    autotools.configure("--disable-dependency-tracking \
+                         --disable-static \
+                         --disable-smpeg \
+                         --enable-midi \
+                         --enable-flac \
+                         --enable-speex \
+                         --enable-mikmod \
+                         --enable-modplug \
+                         --enable-mpglib \
+                         --enable-physfs \
+                         --enable-ogg")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("CHANGELOG", "CREDITS", "README", "TODO")
