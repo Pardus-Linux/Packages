482d936abbd8ce3ba7cfeb00ab1fb3107702c653
Onur Küçük <onur@pardus.org.tr>
2010-08-29 18:21:39 +0000

    enet is ready

---
+++ network/misc/enet/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.export("CFLAGS", "%s -fPIC" % get.CFLAGS())
+    for i in ["NEWS", "AUTHORS", "COPYING"]:
+        shelltools.touch(i)
+
+    autotools.autoreconf("-vfi")
+    autotools.configure("--disable-dependency-tracking \
+                         --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("LICENSE", "ChangeLog", "README*", "*txt")
