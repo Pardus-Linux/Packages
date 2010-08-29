69aaaf02077bdc0ecd352f4029a086e8313926a6
Necdet Yücel <necdetyucel@gmail.com>
2010-08-29 15:27:38 +0000

    txt2man is ready for 2011

---
+++ office/misc/txt2man/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+
+def install():
+    pisitools.dobin("bookman")
+    pisitools.dobin("src2man")
+    pisitools.dobin("txt2man")
+
+    pisitools.doman("*.1")
+
+    pisitools.dodoc("Changelog", "README")
+
