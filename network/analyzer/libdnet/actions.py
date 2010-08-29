ef14b6759a5214ffd121d39e18fdf1fc17c770a9
Onur Küçük <onur@pardus.org.tr>
2010-08-29 18:22:10 +0000

    libdnet is ready

---
+++ network/analyzer/libdnet/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+
+def setup():
+    autotools.autoreconf("-vfi -I config")
+    autotools.configure("--with-python \
+                         --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("LICENSE", "README", "THANKS", "TODO")
