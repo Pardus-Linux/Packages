b1fc4ef499df0d4abc7f7422a13582bf4b9fb51c
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-08-30 13:16:57 +0000

    Add zope-interface and twisted. Note that, upstream name for zope interface is zope.interface, not ZopeInterface. So I think it's OK to make it python-zope-interface. But Twisted is Twisted in upstream.

---
+++ programming/language/python/python-zope-interface/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+WorkDir = "zope.interface-%s" % get.srcVERSION()
+
+def install():
+    pythonmodules.install()
+
+    pythonmodules.fixCompiledPy()
+
+    pisitools.dodoc("README.txt", "PKG-INFO")
+
