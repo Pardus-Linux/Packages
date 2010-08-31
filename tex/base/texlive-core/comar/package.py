826e3e54c58d53b77bb62b6955056184e3e46269
Fatih Arslan <farslan@pardus.org.tr>
2010-08-31 13:08:41 +0000

    Texlivemodules has changed again, should be merged slowly

--- tex/base/texlive-core/comar/package.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import os
-
-def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    os.system("/usr/bin/texmf-update")
-    os.system("/usr/bin/texconfig font rw")
-
