41ea93f7fd428d2e2b3b49e186fe381fd358ca5b
Serdar Dalgıç <serdar@pardus.org.tr>
2010-08-31 22:16:02 +0000

    merge libupnp to 2011.

---
+++ network/analyzer/libupnp/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-vif")
+    autotools.configure("--disable-static \
+                         --without-docdir")
+                         #FIXME: documentation is temporarily disabled since update release 7.
+                         #--with-documentation=/usr/share/doc/%s" % get.srcNAME())
+
+def build():
+    autotools.make()
+
+def check():
+    autotools.make("check")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    #pisitools.remove("/usr/share/doc/libupnp/IXML_Programming_Guide.pdf")
+    #pisitools.remove("/usr/share/doc/libupnp/UPnP_Programming_Guide.pdf")
+
