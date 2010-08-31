74578fa84c67f55fc9518566bea3c6de79a62764
Serdar Dalgıç <serdar@pardus.org.tr>
2010-08-31 18:40:58 +0000

    add python-Jinja2 to 2011.

---
+++ programming/language/python/python-Jinja2/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+examples = "%s/%s/examples" % (get.docDIR(), get.srcNAME())
+
+WorkDir = "Jinja2-%s" % get.srcVERSION()
+
+def build():
+    pythonmodules.compile()
+
+def install():
+    pythonmodules.install()
+
+    pisitools.insinto(examples, "examples/*")
+    pisitools.dohtml("docs/_build/html/*")
+    pisitools.dodoc("CHANGES")
