768c9aa079f03c6d650f36f1f369b177fa74c4bc
Fatih Arslan <farslan@pardus.org.tr>
2010-09-01 12:54:50 +0000

    Ready for 2011

---
+++ tex/addon/texlive-pstricks/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import texlivemodules
+
+from pisi.actionsapi import get
+
+WorkDir = "%s-%s" % (get.srcNAME(), get.srcVERSION().split("_")[-1])
+
+def build():
+    texlivemodules.compile()
+
+def install():
+    texlivemodules.install()
+
+    # Install texmf bin scripts
+    pisitools.dosym("texmf-dist/scripts/pst2pdf/pst2pdf.pl", "/usr/bin/pst2pdf")
+
