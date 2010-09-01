1a4321a6ba5b566609c2f8cde2c26840581e5fc0
Fatih Arslan <farslan@pardus.org.tr>
2010-09-01 13:45:17 +0000

    Fix conflict with texlive-core

--- tex/addon/texlive-context/actions.py
+++ tex/addon/texlive-context/actions.py
@@ -11,14 +11,10 @@ from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
 WorkDir = "%s-%s" % (get.srcNAME(), get.srcVERSION().split("_")[-1])
+
 def build():
     texlivemodules.compile()
 
 def install():
-    pisitools.dosym("%s/%s/texmf-dist/scripts/context/ruby/texmfstart.rb" % (get.workDIR(),WorkDir), "/usr/bin/texmfstart")
-
-    for i in ["context", "ctxtools", "exatools", "luatools", "makempy", "mpstools", "mptopdf", "mtxrun", "mtxtools", "pdftools", "pdftrimwhite", "pstopdf", "rlxtools", "runtools", "texexec", "texfind", "texfont" ,"texshow", "textools", "texutil", "tmftools", "xmltools"]:
-        pisitools.dosym("%s/%s/texmf-dist/scripts/context/stubs/unix/%s" % (get.workDIR(), WorkDir, i), "/usr/bin/%s" % i)
-
     texlivemodules.install()
 
