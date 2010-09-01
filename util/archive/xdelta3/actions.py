de1d5faf3943ce4d09b45f3d6c25dad1c717a40e
Onur Küçük <onur@pardus.org.tr>
2010-09-01 11:04:13 +0000

    xdelta3 is in

---
+++ util/archive/xdelta3/actions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import get
+
+WorkDir="xdelta%s" % get.srcVERSION()
+
+def build():
+    autotools.make('CFLAGS="%s -Wno-deprecated" all xdelta3-decoder xdelta3-tools' % get.CFLAGS())
+    pythonmodules.compile()
+
+def install():
+    for i in ["xdelta3", "xdelta3-decoder", "xdelta3-tools"]:
+        pisitools.dobin("xdelta3")
+
+    pythonmodules.install()
+
+    pisitools.doman("*.1")
+    pisitools.dodoc("COPYING", "README")
+
+    shelltools.chmod("examples/*", 0644)
+    pisitools.insinto("/%s/%s/examples/" % (get.docDIR(), get.srcNAME()), "examples/*")
