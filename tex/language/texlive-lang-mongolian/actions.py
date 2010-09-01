7c17b8d49c147ee4f078f568340b767d987de527
Fatih Arslan <farslan@pardus.org.tr>
2010-09-01 10:30:21 +0000

    Ready for 2011

---
+++ tex/language/texlive-lang-mongolian/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import texlivemodules
+
+from pisi.actionsapi import get
+
+WorkDir = "%s-%s" % (get.srcNAME(), get.srcVERSION().split("_")[-1])
+
+def build():
+    texlivemodules.compile()
+
+def install():
+    texlivemodules.install()
+
