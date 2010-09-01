f5fbe47b6498289b9423e776cbe6e6701ab87cc4
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-09-01 11:59:42 +0000

    qjackctl is ready, we dont need to disable RT by default as jackaudio can do RT stuff with the latest jackaudio commits

---
+++ multimedia/sound/qjackctl/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    #autotools.configure("--with-qt=/usr/qt/4")
+    autotools.configure("--with-qt=/usr/lib/qt4")
+
+def build():
+    autotools.make("CXX=%s CXXFLAGS='%s'" % (get.CXX(), get.CXXFLAGS()))
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "README", "TODO", "TRANSLATORS")
