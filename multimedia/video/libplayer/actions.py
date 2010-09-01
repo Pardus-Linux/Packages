ab81f6b7b3ad80c716237505e58a14c644227e72
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-09-01 06:08:03 +0000

    libplayer is ready

---
+++ multimedia/video/libplayer/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.rawConfigure("--prefix=/usr \
+                            --enable-gstreamer \
+                            --enable-xine \
+                            --enable-mplayer \
+                            --disable-vlc \
+                            --enable-binding-python \
+                            --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "README")
