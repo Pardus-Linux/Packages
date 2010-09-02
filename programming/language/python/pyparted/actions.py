714f07966fc06e9555e6768bdde0f8717c6c49f0
Mete Alpaslan <mete@pardus.org.tr>
2010-09-02 08:52:30 +0000

    Update reverted release.

--- programming/language/python/pyparted/actions.py
+++ programming/language/python/pyparted/actions.py
@@ -9,6 +9,9 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+def setup():
+    autotools.configure()
+
 def build():
     autotools.make()
 
