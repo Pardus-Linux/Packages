ec5bd770a398df452d5a2e773e80e9f1c0e40795
Fethican Coşkuner <fethicanc@gmail.com>
2010-09-02 00:21:49 +0000

    ready for 2011

---
+++ programming/language/python/python-weberror/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "WebError-%s" % get.srcVERSION()
+
+EggDir = "/usr/lib/%s/site-packages/%s-py2.7.egg-info" % (get.curPYTHON(), WorkDir)
+
+def install():
+    pythonmodules.install()
+
+    pisitools.remove("%s/requires.txt" % EggDir)
+    pisitools.remove("%s/entry_points.txt" % EggDir)
+    pisitools.remove("%s/top_level.txt" % EggDir)
