023772e6b7898ad16180c15cd3e6f91c8ab1a39e
Fatih Arslan <farslan@pardus.org.tr>
2010-09-02 11:34:29 +0000

    Ready for 2011

---
+++ tex/tool/kile/actions.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kde
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+import os
+
+WorkDir = "kile-2.1b4"
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr/kde/4", sourceDir="..")
+
+def build():
+    shelltools.cd("build")
+    cmaketools.make()
+
+def install():
+    shelltools.cd("build")
+    cmaketools.install("DESTDIR=%s" % get.installDIR())
+
+    shelltools.cd("..")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
