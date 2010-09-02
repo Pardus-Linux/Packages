c6627f75a3511f5a4b3142234ade5f38dd718d3a
Mete Alpaslan <mete@pardus.org.tr>
2010-09-02 09:00:23 +0000

    New release to development version

--- system/installer/yali/actions.py
+++ system/installer/yali/actions.py
@@ -9,14 +9,13 @@ from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir ="yali4-%s" % get.srcVERSION()
 def setup():
     if get.ARCH() == "i686":
         repo_uri = "http://packages.pardus.org.tr/pardus/2011/stable/i686/pisi-index.xml.bz2"
     else:
         repo_uri = "http://packages.pardus.org.tr/pardus/2011/stable/x86_64/pisi-index.xml.bz2"
 
-    pisitools.dosed("yali4/constants.py", "@REPO_URI@", repo_uri)
+    pisitools.dosed("yali/constants.py", "@REPO_URI@", repo_uri)
 
 def build():
     pythonmodules.compile()
