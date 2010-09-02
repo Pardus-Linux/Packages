8b97170ccd6b19ddf7870af86a3a4f0679457976
Onur Küçük <onur@pardus.org.tr>
2010-09-02 13:47:47 +0000

    update to 2010l

--- system/base/timezone/actions.py
+++ system/base/timezone/actions.py
@@ -11,8 +11,8 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 WorkDir = "tzdata"
-tzcode  = "tzcode2010c"
-tzdata  = "tzdata2010c"
+tzcode  = "tzcode2010l"
+tzdata  = "tzdata2010l"
 
 configTemplate = """
 objpfx = %(pwd)s/obj/
