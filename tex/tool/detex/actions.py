8c333e68582d3ce6dfd0abb2d24856ade6e96f4e
Fatih Arslan <farslan@pardus.org.tr>
2010-09-03 05:45:23 +0000

    Ready for 2011

---
+++ tex/tool/detex/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dobin("detex")
+
+    pisitools.doman("detex.1l")
+
+    pisitools.dodoc("README", "COPYRIGHT")
