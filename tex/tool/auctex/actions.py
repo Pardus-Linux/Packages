322ac9d6ac24dd25595d512b95480492c1b295b8
Fatih Arslan <farslan@pardus.org.tr>
2010-09-03 05:38:34 +0000

    Ready for 2011

---
+++ tex/tool/auctex/actions.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.unlink("%s/%s/doc/auctex.info" % (get.workDIR(), get.srcDIR()))
+
+    autotools.configure("EMACS_NAME=emacs EMACS_FLAVOUR=emacs econf --disable-build-dir-test \
+                         --with-auto-dir='/var/lib/auctex' \
+                         --with-lispdir='/usr/share/emacs/site-lisp/%s' \
+                         --with-packagelispdir='/usr/share/emacs/site-lisp/%s' \
+                         --with-packagedatadir='/usr/share/emacs/etc/%s' \
+                         --with-texmf-dir='/usr/share/texmf-site'" % (get.srcNAME(),get.srcNAME(),get.srcNAME()))
+
+def build():
+    autotools.make()
+
+    shelltools.cd("doc/")
+    autotools.make("tex-ref.pdf")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR()) 
+
+    pisitools.dodoc("CHANGES", "ChangeLog", "README", "COPYING", "FAQ", "RELEASE", "TODO", "doc/tex-ref.pdf")
