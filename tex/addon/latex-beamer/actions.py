3e3fb16cdcf1d7353ee44ee6b4959491282f48bd
Fatih Arslan <farslan@pardus.org.tr>
2010-09-05 17:13:56 +0000

    Remove unnecessary lines

--- tex/addon/latex-beamer/actions.py
+++ tex/addon/latex-beamer/actions.py
@@ -14,8 +14,6 @@ def install():
     
     pisitools.insinto("/usr/share/texmf-dist/tex/latex/beamer", "base/")
 
-    #pisitools.insinto("/usr/share/texmf-dist/tex/latex/beamer/emulation", "base/emulation/*.sty")
-
     pisitools.dodoc("README","ChangeLog","doc/licenses/LICENSE", "AUTHORS")
     pisitools.insinto("/usr/share/doc/%s/doc" % get.srcNAME(), "doc/*")
     
@@ -24,8 +22,3 @@ def install():
     for dir in ["examples", "examples", "solutions"]:
         pisitools.insinto("/usr/share/doc/%s/" % get.srcNAME(), dir)
 
-#    rms= []
-#    for i in ["*.tex~","._beamerexample-lecture-pic*"]:
-#        rms.extend(shelltools.ls(i))
-#    for rm in rms:
-#        pisitools.remove("examples/a-lecture/%s" % rms)
