2f0f1b7ad6f72842e05592c03ab169826cf05361
Onur Küçük <onur@pardus.org.tr>
2010-09-05 10:54:08 +0000

    version bump, split devel

--- system/base/slang/actions.py
+++ system/base/slang/actions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+
+slshdoc = "/%s/slsh" % get.docDIR()
+slangdoc = "/%s/slang/" % get.docDIR()
 
 def setup():
     autotools.configure("--disable-static \
+                         --with-pcre \
                          --with-png \
+                         --with-z \
                          --with-readline=gnu")
 
 def build():
@@ -21,3 +27,9 @@ def check():
 
 def install():
     autotools.rawInstall("DESTDIR=%s INST_LIB_DIR=%s/usr/lib" % (get.installDIR(),get.installDIR()))
+
+    pisitools.domove("%s/*" % slshdoc, "%s/" % slangdoc)
+    pisitools.domove("%s/v2/*" % slangdoc, "%s/" % slangdoc)
+    pisitools.removeDir("%s/v2" % slangdoc)
+    pisitools.removeDir("%s" % slshdoc)
+
