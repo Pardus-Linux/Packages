7b752d1a2ba2e18ce15b18462ea32b2b2a17605b
Onur Küçük <onur@pardus.org.tr>
2010-09-05 11:43:20 +0000

    tv-fonts is in

---
+++ desktop/font/tv-fonts/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def build():
+    shelltools.export("DISPLAY", "")
+    autotools.make("-j1")
+
+def install():
+    pisitools.insinto("/usr/share/fonts/xawtv", "*.gz")
+    pisitools.insinto("/usr/share/fonts/xawtv", "fonts.alias")
