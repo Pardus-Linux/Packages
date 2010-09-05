5a6989db8abf5a5bc20c792c9fab64607ab08681
Onur Küçük <onur@pardus.org.tr>
2010-09-05 11:03:09 +0000

    libaio is in

---
+++ hardware/misc/libaio/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def build():
+    autotools.make()
+
+def check():
+    shelltools.cd("harness")
+    pisitools.dodir("testdir")
+
+    autotools.make("check prefix=../src libdir=../src")
+
+
+def install():
+    autotools.rawInstall("destdir=%s prefix=/ libdir=/lib \
+                          includedir=/usr/include usrlibdir=/usr/lib" % get.installDIR())
+
+    pisitools.remove("/usr/lib/libaio.a")
+
+    pisitools.dodoc("ChangeLog", "COPYING", "TODO")
