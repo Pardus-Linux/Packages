bb75a2e0a80f8483e7fbf5ae776ece3481c576a7
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-09-07 07:12:05 +0000

    Add indilib

---
+++ science/misc/indilib/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "libindi-%s" % get.srcVERSION()
+
+def setup():
+    cmaketools.configure("-DBUILD_ROOT=%s" % get.installDIR())
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING.LIB", "NEWS", "README*", "TODO")
