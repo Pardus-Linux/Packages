82049e7e82fdb32eed0e0ca2c5d0233ea921f111
Ozan Çağlayan <ozan@pardus.org.tr>
2010-09-09 11:00:42 +0000

    ipw2100-firmware is ready

---
+++ hardware/firmware/ipw2100-firmware/actions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+
+WorkDir = "."
+
+def install():
+    pisitools.insinto("/lib/firmware", "*.fw")
+    pisitools.dodoc("LICENSE")
