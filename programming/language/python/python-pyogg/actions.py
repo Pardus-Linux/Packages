cd82534f8e1904e81d174c02c749c72935cd6ce4
Onur Küçük <onur@pardus.org.tr>
2010-09-11 19:59:16 +0000

    pyogg is in

---
+++ programming/language/python/python-pyogg/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import get
+
+WorkDir = "pyogg-%s" % get.srcVERSION()
+examples = "%s/%s/examples" % (get.docDIR(), get.srcNAME())
+
+def setup():
+    shelltools.system("./config_unix.py")
+
+def build():
+    pythonmodules.compile()
+
+def install():
+    pythonmodules.install()
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "NEWS", "README")
+
+    shelltools.chmod("test/*", 0644)
+    pisitools.insinto(examples, "test/*")
+
