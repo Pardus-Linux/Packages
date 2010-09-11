20ac20b3b90818fc5ad207406f10174df5f3c3dd
Onur Küçük <onur@pardus.org.tr>
2010-09-11 18:55:41 +0000

    mpeg2vidcodec is in

---
+++ multimedia/converter/mpeg2vidcodec/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005,2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "mpeg2"
+
+def setup():
+     pisitools.dosed("Makefile", "-O2", get.CFLAGS())
+
+def build():
+    autotools.make('CC="%s"' % get.CC())
+
+def install():
+    pisitools.dobin("src/mpeg2dec/mpeg2decode")
+    pisitools.dobin("src/mpeg2enc/mpeg2encode")
+
+    pisitools.dodoc("README", "doc/*")
