7aff191a714a26e59d329f0a3811ed192fe271f1
Onur Küçük <onur@pardus.org.tr>
2010-09-11 19:28:33 +0000

    hydra is ready

---
+++ network/analyzer/hydra/actions.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "hydra-%s-src" % get.srcVERSION()
+
+moduleconfig = {"XDEFINES": "-DLIBOPENSSL -DLIBSSH",
+                "XLIBS": "-lcrypto -lssl -lssh -lm",
+                "XLIBPATHS": "",
+                "XIPATHS": "",
+                "CC": get.CC(),
+                "STRIP": "true"
+}
+
+
+def setup():
+    pisitools.dosed("Makefile.am", "-O2", get.CFLAGS())
+    autotools.configure("--disable-xhydra")
+
+    for i in moduleconfig:
+        pisitools.dosed("Makefile", "^%s=.*" % i, "%s=%s" % (i, moduleconfig[i]))
+
+def build():
+    autotools.make()
+
+def install():
+    for i in ["hydra", "pw-inspector"]:
+        pisitools.dobin(i)
+
+    pisitools.dodoc("CHANGES", "LICENSE*", "README*", "TODO")
