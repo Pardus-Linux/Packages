e67967865d4bdd7097fd0ca7441392cd24d1b3f7
Ozan Çağlayan <ozan@pardus.org.tr>
2010-09-14 08:47:23 +0000

    Bump to 4.0, switch to rsyslog

--- system/base/mudur/actions.py
+++ system/base/mudur/actions.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2009 TUBITAK/UEKAE
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def install():
