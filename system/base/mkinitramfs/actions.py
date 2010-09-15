d7da03ad086cd6bd557b72f5625d95b8d2f4c7eb
Onur Küçük <onur@pardus.org.tr>
2010-09-15 17:09:58 +0000

    Remove busybox parts as it is a seperate package now Adapt to new busybox path Change package style and version to be standalone mkinitramfs

--- system/base/mkinitramfs/actions.py
+++ system/base/mkinitramfs/actions.py
@@ -5,19 +5,16 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
-WorkDir = "busybox-%s" % get.srcVERSION()
 
-def build():
-    autotools.make()
-    autotools.make("busybox.links")
+WorkDir = "./"
+
+def setup():
+    shelltools.move("README.mkinitramfs", "README")
 
 def install():
-    pisitools.insinto("/lib/initramfs", "busybox.links")
-    pisitools.insinto("/lib/initramfs", "busybox")
-
     pisitools.dodir("/etc/initramfs.d")
+    pisitools.dodoc("README")
 
