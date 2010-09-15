e22713c1b8c8eb6b422a6302aaf8a632f2b36cf2
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-09-15 16:51:31 +0000

    Add liblazy

---
+++ programming/misc/liblazy/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--enable-static=no")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("README", "AUTHORS", "ChangeLog", "NEWS", "COPYING")
