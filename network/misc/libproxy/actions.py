37bb0170c0176649b13174161ed9da3f2f7dd4de
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-09-16 07:21:04 +0000

    Bump to 0.4.6, enable KDE module

--- network/misc/libproxy/actions.py
+++ network/misc/libproxy/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    cmaketools.configure()
+    cmaketools.configure("-DWITH_MOZJS=0")
 
 def build():
     cmaketools.make()
