c5c7f41c3fab67bb8b05b25859e86b510b9593f9
Gökhan Özbulak <gozbulak@pardus.org.tr>
2010-09-17 13:53:31 +0000

    Tomcat-servlet-api upgrade from v5.5.28 to v5.5.31

---
+++ programming/language/java/tomcat-servlet-api/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007,2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir="apache-tomcat-%s-src/servletapi" % get.srcVERSION()
+
+def setup():
+    shelltools.export("LC_ALL", "C")
+    shelltools.system("ant -f jsr154/build.xml dist")
+    shelltools.system("ant -f jsr152/build.xml dist")
+
+def install():
+    pisitools.insinto("/usr/share/java","jsr154/dist/lib/servlet-api*.jar","servlet-api.jar")
+    pisitools.insinto("/usr/share/java","jsr152/dist/lib/jsp-api*.jar","jsp-api.jar")
+
+    pisitools.dodoc("jsr154/dist/README.txt")
+    pisitools.dodoc("jsr154/dist/LICENSE")
