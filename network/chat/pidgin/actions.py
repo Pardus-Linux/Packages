af02b6dd430f980c9585e09534ec5bd585efd6b8
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-09-17 13:12:12 +0000

    pidgin is ready

---
+++ network/chat/pidgin/actions.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import perlmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.export("CFLAGS","%s -fstack-protector-all" % get.CFLAGS())
+    autotools.autoreconf("-vfi")
+    autotools.configure("--prefix=/usr \
+                         --enable-dbus \
+                         --enable-cyrus-sasl \
+                         --enable-gnutls=yes \
+                         --enable-nss=no \
+                         --enable-cap \
+                         --disable-schemas-install \
+                         --disable-meanwhile \
+                         --enable-nm \
+                         --disable-tcl \
+                         --disable-tk \
+                         --x-includes=/usr/include/X11 \
+                         --with-gnutls-includes=/usr/include/gnutls \
+                         --with-gnutls-libs=/usr/lib")
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING", "HACKING", "NEWS", "README", "ChangeLog")
