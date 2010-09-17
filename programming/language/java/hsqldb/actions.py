01e84ad75a9bc935fdab9e53b2b7403f91b5c699
Gökhan Özbulak <gozbulak@pardus.org.tr>
2010-09-17 13:56:07 +0000

    Hsqldb upgrade from v1.8.1.1 to v1.8.1.3

---
+++ programming/language/java/hsqldb/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "hsqldb"
+
+def setup():
+    shelltools.unlink("lib/hsqldb.jar")
+
+def build():
+    shelltools.export("JAVA_HOME", "/opt/sun-jdk")
+    shelltools.cd("build")
+    shelltools.system("ant jar")
+
+def install():
+    pisitools.insinto("/usr/share/java", "lib/hsqldb.jar")
+
+    pisitools.dohtml("doc/*.html", "doc/guide/*", "docsrc/*")
+    pisitools.dodoc("readme.txt", "doc/*.txt")
