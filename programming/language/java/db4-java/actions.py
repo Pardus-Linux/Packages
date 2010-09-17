560939f81aa51c82700214fbde6623bdf8cc0e0f
Gökhan Özbulak <gozbulak@pardus.org.tr>
2010-09-17 13:55:13 +0000

    Db4-java upgrade from v4.7.25 to v4.8.30

---
+++ programming/language/java/db4-java/actions.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+import os
+
+WorkDir = "db-%s/build_unix" % get.srcVERSION()
+
+def setup():
+    shelltools.export("PATH", "%s:/opt/sun-jdk/bin" % os.environ.get("PATH"))
+
+    shelltools.system("../dist/configure \
+                       --prefix=/usr \
+                       --mandir=/usr/share/man \
+                       --infodir=/usr/share/info \
+                       --datadir=/usr/share \
+                       --sysconfdir=/etc \
+                       --localstatedir=/var/lib \
+                       --libdir=/usr/lib \
+                       --enable-compat185 \
+                       --with-uniquename \
+                       --enable-java \
+                       --disable-cxx \
+                       --disable-tcl \
+                       --disable-static")
+
+def build():
+    shelltools.export("PATH", "%s:/opt/sun-jdk/bin" % os.environ.get("PATH"))
+    autotools.make()
+
+def install():
+    pisitools.dolib(".libs/libdb_java*.so")
+    pisitools.insinto("/usr/share/java","db.jar")
