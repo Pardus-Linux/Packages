792f49580cc218f07908718ee6066759df05a34c
Mehmet Emre Atasever <memre@pardus.org.tr>
2010-09-20 21:26:29 +0000

    cscope is ready for 2011 BUG:COMMENT:13412

---
+++ programming/tool/cscope/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.dodir("/var/run/cs")
+    pisitools.dobin("contrib/xcscope/cscope-indexer")
+
+    # emacs integration
+    pisitools.insinto("/usr/share/emacs/site-lisp/xcscope/", "contrib/xcscope/xcscope.el")
+
+    pisitools.dodoc("AUTHORS", "TODO", "COPYING", "README", "ChangeLog", "NEWS")
+
