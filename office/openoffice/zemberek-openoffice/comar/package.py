25e01ea189a552cec82ca7a98eadc3b26d690544
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-09-21 08:11:07 +0000

    Add zemberek-openoffice

---
+++ office/openoffice/zemberek-openoffice/comar/package.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import os
+import glob
+
+unopkg = "/opt/OpenOffice.org/bin/unopkg"
+extPath = glob.glob("/usr/share/zemberek/zemberek*.oxt")[0]
+extID = "net.zemberek.ooo.spellchecker"
+
+def postInstall(fromVersion, fromRelease, toVersion, toRelease):
+    os.environ["JAVA_HOME"] = "/opt/sun-jre"
+
+    ret = os.system("%s add --shared --force %s" % (unopkg, extPath))
+
+    if ret != 0:
+        raise Exception("Could not install OO.org extension: %s" % extID)
+
+def preRemove():
+    os.environ["JAVA_HOME"] = "/opt/sun-jre"
+
+    ret = os.system("%s remove --shared %s" % (unopkg, extID))
+
+    if ret != 0:
+        raise Exception("Could not remove OO.org extension: %s" % extID)
