0b52ee6a2112d60eeadec7a99498329ceb534e5d
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-09-21 10:29:18 +0000

    Add oxygen-like theme for gtk2

---
+++ desktop/toolkit/gtk/gtk2-themes-kde4oxygen/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+import os
+
+WorkDir = "."
+
+def install():
+    shelltools.copytree("kde42-oxygen", "%s/usr/share/themes/Oxygen" % get.installDIR())
+    pisitools.removeDir("/usr/share/themes/Oxygen/metacity-1")
+
+    #fix permissions of directories and files
+    for dirpath, dirs, files in os.walk("%s/usr/share/themes/Oxygen" % get.installDIR()):
+        shelltools.chmod(dirpath, 0755)
+        for file in files:
+            shelltools.chmod(os.path.join(dirpath, file), 0644)
