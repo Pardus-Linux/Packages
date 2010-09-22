1bde2f2a8ebeabe40c06a357aa3f24e93a3c71dc
Ozan Çağlayan <ozan@pardus.org.tr>
2010-09-22 06:40:56 +0000

    poster is ready

---
+++ hardware/printer/poster/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
+WorkDir = "poster-20060221"
+
+def build():
+    shelltools.system("%s %s %s poster.c -lm -o poster" % (get.CC(), get.CFLAGS(), get.LDFLAGS()))
+
+def install():
+    pisitools.dobin("poster")
+
+    pisitools.dodoc("README")
+    pisitools.doman("poster.1")
