9878b671315db7d8a1722371dc64e081d7de81e8
Serdar Dalgıç <serdar@pardus.org.tr>
2010-09-23 13:47:32 +0000

    qtscriptgenerator is ready for amarok in 2011.

---
+++ desktop/toolkit/qt/qtscriptgenerator/actions.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir="%s-src-%s" % (get.srcNAME(), get.srcVERSION())
+
+def setup():
+    shelltools.cd("generator")
+    shelltools.system("qmake generator.pro")
+    shelltools.cd("../qtbindings")
+    shelltools.system("qmake qtbindings.pro")
+
+def build():
+    shelltools.cd("generator")
+    autotools.make("-j1")
+    shelltools.system("QTDIR=/usr/share/qt4 ./generator --include-paths='/usr/include'")
+
+    shelltools.cd("../qtbindings")
+    autotools.make("-j1")
+
+def install():
+    pisitools.insinto("/usr/lib/qt4/plugins/script", "plugins/script/*")
+    pisitools.insinto("%s/qtscriptgenerator" % get.docDIR(), "doc/*")
+
+    pisitools.dodoc("LICENSE.GPL")
