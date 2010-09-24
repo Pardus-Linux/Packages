53e5ab700116d1ab267fe719e15330ee4a29bd41
Ozan Çağlayan <ozan@pardus.org.tr>
2010-09-24 07:25:56 +0000

    Bump

--- system/base/audit/actions.py
+++ system/base/audit/actions.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007-2009 TUBITAK/UEKAE
+# Copyright 2007-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import get
 
 def setup():
@@ -28,8 +28,6 @@ def install():
     # remove RH specific bits
     pisitools.removeDir("/etc/sysconfig/")
     pisitools.removeDir("/etc/rc.d/")
-
-    pythonmodules.fixCompiledPy()
 
     pisitools.dodir("/var/log/audit/")
 
