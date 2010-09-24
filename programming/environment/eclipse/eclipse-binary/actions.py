30a89e22c10d6a3d1197af9facc36900c00ae482
Gökhan Özbulak <gozbulak@pardus.org.tr>
2010-09-24 06:52:48 +0000

    Adding eclipse-binary into 2011 repo

---
+++ programming/environment/eclipse/eclipse-binary/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "."
+
+def install():
+    pisitools.dodir("/opt")
+    shelltools.copytree("eclipse", "%s/opt/eclipse" % get.installDIR())
+
