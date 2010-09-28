9bce6e9cf6cdb9c0674bef5e1eab4130da64dc48
Burak Çalışkan <burakclskn@gmail.com>
2010-09-28 14:57:08 +0000

    ready for 2011

--- desktop/toolkit/gtk/gtksourceview/actions.py
+++ desktop/toolkit/gtk/gtksourceview/actions.py
@@ -7,6 +7,10 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
+shelltools.export("HOME", get.workDIR())
 
 def setup():
     autotools.configure("--disable-static")
