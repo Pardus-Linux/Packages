a23e7297c053ac32e7cc5cfabfa2ce933ac825d2
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-09-28 12:04:54 +0000

    Update to latest sip

--- programming/language/python/sip/actions.py
+++ programming/language/python/sip/actions.py
@@ -10,6 +10,9 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import get
 
+WorkDir = "%s-4.11.2-snapshot-c93f5da3d4e4" % get.srcNAME()
+NoStrip=["/"]
+
 def setup():
     pythonmodules.run('configure.py \
                        -b /usr/bin \
