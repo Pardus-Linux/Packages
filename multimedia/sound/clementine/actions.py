6a66ec72bff8b9a1be073732f8443f57ac96e1c7
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-09-29 13:42:32 +0000

    Move clementine to 2011 repository. Thanks for reviews.
    BUG:FIXED:14488

---
+++ multimedia/sound/clementine/actions.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    for i in ["libprojectm", "qxt", "qtiocompressor"]:
+        shelltools.unlinkDir("3rdparty/%s" % (i))
+
+    # Upstream supports only gstreamer engine, other engines are unstable and lacking features.
+    # QTSINGLEAPPLICATION is builtin since we need to patch Qt just for this package and  Gökçen has given OK
+    # for using builtin qtsingleapplication.
+    cmaketools.configure("-DHAVE_LIBNOTIFY=ON \
+                          -DENGINE_GSTREAMER_ENABLED=ON \
+                          -DENGINE_QT_PHONON_ENABLED=OFF \
+                          -DENGINE_LIBVLC_ENABLED=OFF \
+                          -DENGINE_LIBXINE_ENABLED=OFF \
+                          -DSTATIC_SQLITE=OFF \
+                          -DUSE_SYSTEM_QTSINGLEAPPLICATION=OFF \
+                          -DUSE_SYSTEM_QXT=ON \
+                          -DUSE_SYSTEM_PROJECTM=ON \
+                          -DENABLE_LIBMTP=ON \
+                          -DENABLE_VISUALISATIONS=ON \
+                          -DENABLE_GIO=ON \
+                          -DBUNDLE_PROJECTM_PRESETS=OFF", sourceDir=".")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    for i in ("16","32","64"):
+        pisitools.insinto("/usr/share/icons/hicolor/%sx%s/apps" % (i,i), "dist/clementine_%s.png" % i, "clementine.png")
+
+    pisitools.insinto("/usr/share/clementine/locale", "src/translations/*.qm")
+    pisitools.dosym("/usr/share/icons/hicolor/64x64/apps/clementine.png", "/usr/share/pixmaps/clementine.png")
+
+    pisitools.dodoc("Changelog", "COPYING", "TODO")
