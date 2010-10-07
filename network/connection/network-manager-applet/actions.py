fb85ff0ddc058b334972fd72db73a64b232d76fa
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-10-07 13:06:55 +0000

    Merge NM-applet, maybe we can use that as NM GUI. My hopes about KNM is getting less and less every minute.

---
+++ network/connection/network-manager-applet/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+def setup():
+    #shelltools.system("./autogen.sh")
+    autotools.configure("--disable-dependency-tracking \
+                         --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    # Remove empty gnome-bluetooth directory
+    pisitools.removeDir("/usr/lib")
+
+    pisitools.dodoc("README", "COPYING")
