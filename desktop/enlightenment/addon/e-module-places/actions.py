5450dd5b74b71f79a78cfdaa88271b120ee97921
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-10-08 07:28:15 +0000

    Version bump.

--- desktop/enlightenment/addon/e-module-places/actions.py
+++ desktop/enlightenment/addon/e-module-places/actions.py
@@ -9,8 +9,6 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "places-%s" % get.srcVERSION()
-
 def setup():
     shelltools.export("AUTOPOINT", "/bin/true")
 
