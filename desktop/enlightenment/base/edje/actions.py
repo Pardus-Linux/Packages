732ad144df3c90cd9b8c7619be18db7c30345a08
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-10-08 07:27:49 +0000

    Version bump.

--- desktop/enlightenment/base/edje/actions.py
+++ desktop/enlightenment/base/edje/actions.py
@@ -8,6 +8,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+WorkDir = "edje-1.0.0.beta"
+
 def setup():
     autotools.autoreconf("-fi")
     autotools.configure("--disable-static")
@@ -18,4 +20,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "COPYING*", "README")
+    pisitools.dodoc("AUTHORS", "COPYING", "README")
