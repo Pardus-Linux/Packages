d1a7476714c3e6cc958584e15a179020540eb7db
Fatih Arslan <farslan@pardus.org.tr>
2010-10-11 09:23:22 +0000

    Cosmetic change

--- programming/language/perl/perl-DateTime-Format-DateParse/actions.py
+++ programming/language/perl/perl-DateTime-Format-DateParse/actions.py
@@ -7,6 +7,7 @@
 from pisi.actionsapi import perlmodules
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+
 WorkDir = "%s-%s" % (get.srcNAME()[5:], get.srcVERSION())
 
 def setup():
