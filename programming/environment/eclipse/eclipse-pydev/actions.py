c0fabd33dec6137b2f8cc29de8b78ea951834c9f
Gökhan Özbulak <gozbulak@pardus.org.tr>
2010-10-12 17:56:39 +0000

    eclipse-pydev package is ready for 2011

---
+++ programming/environment/eclipse/eclipse-pydev/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "."
+
+def fixPermissions(dest):
+    import os
+    for root, dirs, files in os.walk(dest):
+        for name in dirs:
+            shelltools.chmod(os.path.join(root, name), 0755)
+        for name in files:
+            shelltools.chmod(os.path.join(root, name), 0644)
+
+def install():
+    pisitools.dodir("/opt/eclipse")
+
+    pisitools.insinto("/opt/eclipse", "plugins")
+    pisitools.insinto("/opt/eclipse", "features")
+
+    # Fix permissions
+    fixPermissions("%s/opt" % get.installDIR())
