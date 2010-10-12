d501a9f723a0ac16f92707bc4edf9690278bcab0
Burak Çalışkan <burakclskn@gmail.com>
2010-10-12 17:13:57 +0000

    new package - gdk-pixbuf BUG:FIXED:14467

---
+++ desktop/toolkit/gtk/gdk-pixbuf/actions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    autotools.autoreconf("-fiv")
+    autotools.configure("--disable-static \
+                         --enable-silent-rules \
+                         --enable-introspection \
+                         --with-libjasper \
+                         --with-included-loaders=png")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.remove("/usr/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache")
+
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
