690cb1f0f2a528ecaa377c542f1487bcf125f139
Gökhan Özbulak <gozbulak@pardus.org.tr>
2010-10-12 18:11:50 +0000

    eclipse-binary package is ready for 2011 -> Merged the package in my playground with that in 2011 repo

--- programming/environment/eclipse/eclipse-binary/actions.py
+++ programming/environment/eclipse/eclipse-binary/actions.py
@@ -5,13 +5,11 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "."
+WorkDir = get.ARCH()
 
 def install():
     pisitools.dodir("/opt")
-    shelltools.copytree("eclipse", "%s/opt/eclipse" % get.installDIR())
-    pisitools.dosym("/opt/eclipse/eclipse", "/usr/bin/eclipse")
+    pisitools.insinto("/opt", "eclipse")
