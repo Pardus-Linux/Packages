77cc3265ccd642ee7923e5b80af9187f31fbc460
Uğur Çetin <ugur.jnmbk@gmail.com>
2010-10-13 08:58:54 +0000

    ready for 2011

---
+++ desktop/kde/addon/knazar/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=release")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc('COPYING', 'AUTHORS', 'ChangeLog', 'README')
