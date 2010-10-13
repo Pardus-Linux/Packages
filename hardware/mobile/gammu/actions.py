fc5eaeaf3c5afb4783606c155bfa0a24afd421cc
Necdet Yücel <necdetyucel@gmail.com>
2010-10-13 13:16:03 +0000

    ready for 2011

---
+++ hardware/mobile/gammu/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    cmaketools.configure("-DENABLE_SHARED=ON\
+    -DINSTALL_DOC=ON\
+    -DINSTALL_LOC=ON\
+    -DINSTALL_BASH_COMPLETION=OFF", installPrefix="%s/usr" % get.installDIR())
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall()
+
+    pisitools.dodoc("ChangeLog", "COPYING", "README")
+
+    pisitools.remove("/usr/lib/*.a")
