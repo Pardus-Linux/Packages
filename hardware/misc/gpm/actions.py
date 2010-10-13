12fe2b0ea0b9f47ebe27bf6180e860b204bd5b57
Ozan Çağlayan <ozan@pardus.org.tr>
2010-10-13 12:22:22 +0000

    Add some fedora patches and systemd support

--- hardware/misc/gpm/actions.py
+++ hardware/misc/gpm/actions.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import libtools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.export("CC", get.CC())
+    autotools.autoreconf("-fi")
     autotools.configure("--libdir=/usr/lib \
                          --sysconfdir=/etc/gpm")
 
 def build():
-    autotools.make("clean")
-    autotools.make("-j1 -C doc")
-    autotools.make("-j1 EMACS=:")
+    autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
@@ -29,6 +26,8 @@ def install():
 
     #remove static link
     pisitools.remove("/usr/lib/libgpm.a")
+
+    pisitools.removeDir("/usr/share/emacs")
 
     pisitools.insinto("/etc/gpm", "conf/gpm-*.conf")
 
