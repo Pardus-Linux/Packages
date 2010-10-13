24efd65b2f69961efa4bcdaaf5cef0e291156474
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-10-13 08:13:18 +0000

    Version bump.

--- network/p2p/libktorrent/actions.py
+++ network/p2p/libktorrent/actions.py
@@ -6,7 +6,6 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 from pisi.actionsapi import kde4
