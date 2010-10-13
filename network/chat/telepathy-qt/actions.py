83a33f14501a258a38c1b75ea406478b405f12db
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-10-13 09:00:14 +0000

    Version bump, fix build.

--- network/chat/telepathy-qt/actions.py
+++ network/chat/telepathy-qt/actions.py
@@ -5,13 +5,11 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
 WorkDir = "telepathy-qt4-%s" % get.srcVERSION()
-
-QTDIR = {"qt": get.qtDIR()}
 
 def setup():
     autotools.autoreconf("-vfi")
@@ -22,3 +20,5 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
