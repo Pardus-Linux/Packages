faff1bd70bcb7e3e9db3b7a38e28dae8e66ef798
Ozan Çağlayan <ozan@pardus.org.tr>
2010-10-14 09:44:07 +0000

    ndiswrapper is ready

---
+++ kernel/default/drivers/module-ndiswrapper/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kerneltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+KDIR = kerneltools.getKernelVersion()
+
+def build():
+    for i in ["driver/Makefile", "Makefile"]:
+        pisitools.dosed(i, "^KVERS \?=.*$", "KVERS ?= %s" % KDIR)
+
+    autotools.make("-C /lib/modules/%s/build M=`pwd`" % KDIR)
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodir("/etc/ndiswrapper")
+
+    pisitools.dodoc("README", "AUTHORS", "ChangeLog")
