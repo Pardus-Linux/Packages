c7f6b0b46ac21aacb08b360868309c234282859d
Ozan Çağlayan <ozan@pardus.org.tr>
2010-10-15 10:59:21 +0000

    Version bump

--- hardware/cpu/microcode-data/actions.py
+++ hardware/cpu/microcode-data/actions.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2008-2009 TUBITAK/UEKAE
+# Copyright 2008-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 NoStrip = ["/"]
 WorkDir = "."
 
-datafile_intel = "microcode-20100826.dat"
+datafile_intel = "microcode-20100914.dat"
 datafile_amd = "amd-ucode-2009-10-09/microcode_amd.bin"
 
 def setup():
