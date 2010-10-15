8b55f2bd3c38233583494bf78f25d27b16beebc9
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-10-15 13:11:29 +0000

    Version bump.

--- network/web/arora/actions.py
+++ network/web/arora/actions.py
@@ -4,13 +4,8 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 from pisi.actionsapi import qt4
-
-WorkDir="arora-0.10.2"
 
 def setup():
     qt4.configure()
