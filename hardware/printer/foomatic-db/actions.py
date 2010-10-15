149e5ea4c7a6532f4a4a9fcc15ad5380ce952c9c
Ozan Çağlayan <ozan@pardus.org.tr>
2010-10-15 11:41:23 +0000

    Ready

---
+++ hardware/printer/foomatic-db/actions.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+import os
+
+WorkDir = "foomatic-db-%s" % get.srcVERSION().split("_", 1)[1]
+NoStrip = "/"
+
+def setup():
+    # For gutenprint printers, use gutenprint-ijs-simplified.5.2
+    pisitools.dosed("db/source/printer/*.xml", ">gutenprint<", ">gutenprint-ijs-simplified.5.2<")
+
+    # Disable gzip compression for better lzma compression
+    autotools.configure("--disable-gzip-ppds")
+
+    # Cleanup conflicts
+    shelltools.cd("db/source")
+    shelltools.system("../../cleanup-conflicts")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    # Fix permissions
+    for root, dirs, files in os.walk("%s/usr/share/foomatic/db" % get.installDIR()):
+        for name in dirs:
+            shelltools.chmod(os.path.join(root, name), 0755)
+        for name in files:
+            shelltools.chmod(os.path.join(root, name), 0644)
