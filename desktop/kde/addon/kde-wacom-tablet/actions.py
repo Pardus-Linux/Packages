a7e6e13e5e8203b6f4ca48317967b0e92d898e52
Metin Akdere <metin@pardus.org.tr>
2010-10-19 11:08:59 +0000

    kde-wacom-tablet is ready for 2011.

---
+++ desktop/kde/addon/kde-wacom-tablet/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+from pisi.actionsapi import kde4
+
+WorkDir = "wacomtablet-%s" % get.srcVERSION()
+shelltools.export("HOME",get.workDIR())
+
+def setup():
+    kde4.configure()
+
+def build():
+    kde4.make()
+
+def install():
+    kde4.install()
