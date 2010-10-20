7e65e88bc8c7a649e91116ddae718c63a0d73007
Necdet Yücel <necdetyucel@gmail.com>
2010-10-20 11:25:21 +0000

    ready for 2011

---
+++ programming/language/python/python-pyRXP/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+WorkDir="pyRXP-%s" % get.srcVERSION()
+
+def build():
+    pythonmodules.compile()
+
+def install():
+    pythonmodules.install()
+
+    pisitools.insinto("%s/%s" % (get.docDIR(), get.srcNAME()), "examples")
+    pisitools.insinto("%s/%s" % (get.docDIR(), get.srcNAME()), "docs/*")
