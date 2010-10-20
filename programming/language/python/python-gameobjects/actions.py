b28920faf2a806b00c2e40eb02a35868614ac286
Necdet Yücel <necdetyucel@gmail.com>
2010-10-20 10:56:00 +0000

    ready for 2011

---
+++ programming/language/python/python-gameobjects/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import pythonmodules
+
+WorkDir = "gameobjects-%s" % get.srcVERSION()
+
+def build():
+    pythonmodules.compile()
+
+def install():
+    pythonmodules.install()
