e3c57a9b4330d1d5720b89c483b26b5a79965033
Fatih Arslan <farslan@pardus.org.tr>
2010-10-27 11:39:42 +0000

    * Take latest snapshot from CVS repo * Fixes complication with KDE4 * xfce-detection.patch has changed (one patch to rule them all) * Added other xdg-* scripts

--- desktop/misc/xdg-utils/actions.py
+++ desktop/misc/xdg-utils/actions.py
@@ -5,14 +5,18 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
     autotools.configure()
 
 def build():
-    autotools.make()
+    # For fixing links' sandbox violations
+    shelltools.export("HOME", get.workDIR())
+    autotools.make("-C scripts scripts")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
