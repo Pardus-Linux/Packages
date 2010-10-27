b5226b7784429ce28c46a774aa892af4c664055a
Ozan Çağlayan <ozan@pardus.org.tr>
2010-10-27 07:40:31 +0000

    minicom is ready
    BUG:FIXED:14911

---
+++ network/connection/minicom/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import libtools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    #libtools.libtoolize("--copy --force")
+    autotools.configure("--sysconfdir=/etc/minicom")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.insinto("/etc/minicom", "doc/minirc.dfl")
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "README", "TODO", "NEWS", "doc/minicom.FAQ")
