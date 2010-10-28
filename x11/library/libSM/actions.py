94b66924a04984680a8aca0f9945314e073dbcbf
Fatih Aşıcı <fatih@pardus.org.tr>
2010-10-28 14:24:14 +0000

    New stable release (1.2.0)

--- x11/library/libSM/actions.py
+++ x11/library/libSM/actions.py
@@ -1,15 +1,16 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-static")
+    autotools.configure("--disable-static \
+                         --without-xmlto \
+                         --without-fop")
 
 def build():
     autotools.make()
@@ -17,3 +18,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
