57dd641faec9109f6201e96e8131ec20a45e702a
Fatih Aşıcı <fatih@pardus.org.tr>
2010-10-28 14:40:55 +0000

    New stable release (1.2.0)

--- x11/library/libXext/actions.py
+++ x11/library/libXext/actions.py
@@ -4,13 +4,17 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 
 def setup():
     autotools.autoreconf("-vif")
-    autotools.configure("--disable-static")
+    autotools.configure("--disable-static \
+                         --disable-specs")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
