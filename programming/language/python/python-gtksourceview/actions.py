6eb167e2a6fb0e4d36bebea0c648acc249a93783
Burak Çalışkan <burakclskn@gmail.com>
2010-10-30 18:27:11 +0000

    copy and rename package

---
+++ programming/language/python/python-gtksourceview/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "pygtksourceview-%s" % get.srcVERSION()
+
+def setup():
+    autotools.autoreconf("-fiv")
+    autotools.configure("--disable-static\
+                         --disable-docs")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("NEWS", "README", "AUTHORS", "ChangeLog", "COPYING")
