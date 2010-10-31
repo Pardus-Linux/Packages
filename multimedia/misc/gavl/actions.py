25f922bbf0066eae665f688f096aba8733574caa
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-10-31 19:50:24 +0000

    Disable build-time CPU detection, as in all other distros. Maybe related to #14984.
    BUG:COMMENT:14984

--- multimedia/misc/gavl/actions.py
+++ multimedia/misc/gavl/actions.py
@@ -11,12 +11,18 @@ from pisi.actionsapi import get
 
 def setup():
     pisitools.dosed("configure.ac", "-mfpmath=387", "")
+
+    #disable CPU detection
+    pisitools.dosed("configure.ac", "LQT_TRY_CFLAGS", "dnl LQT_TRY_CFLAGS")
+    pisitools.dosed("configure.ac", "LQT_OPT_CFLAGS", "dnl LQT_OPT_CFLAGS")
+
     autotools.autoreconf("-fi")
 
     # disable libpng because it's only used for tests
     autotools.configure("--disable-libpng \
                          --disable-dependency-tracking \
                          --without-cpuflags \
+                         --disable-cpu-clip \
                          --without-doxygen")
 
 def build():
