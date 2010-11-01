968740bf9abe3c53eb412383ce4d3f95f5a23c8c
Fatih Aşıcı <fatih@pardus.org.tr>
2010-11-01 10:37:37 +0000

    New stable release (1.1.0)

--- system/base/libXdmcp/actions.py
+++ system/base/libXdmcp/actions.py
@@ -4,12 +4,16 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--disable-static")
+    autotools.configure("--disable-static \
+                         --without-xmlto")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
