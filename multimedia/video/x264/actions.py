f68c318e2eba2175277881159472f5a4dc524079
Onur Küçük <onur@pardus.org.tr>
2010-11-03 12:13:29 +0000

    update snapshot for X264_TYPE_KEYFRAME

--- multimedia/video/x264/actions.py
+++ multimedia/video/x264/actions.py
@@ -25,11 +25,17 @@ def getMinorVersion():
 
 def setup():
     shelltools.export("CFLAGS", "%s -O3" % get.CFLAGS())
+
+    # force using shared gpac
+    pisitools.dosed("configure", "-lgpac_static", "-lgpac")
+
+    # these disables are here to prevent circular deps, especially with ffmpeg
     autotools.rawConfigure("--prefix=/usr \
                             --enable-pic \
-                            --disable-avs-input \
-                            --disable-ffms-input \
-                            --disable-lavf-input \
+                            --disable-avs \
+                            --disable-ffms \
+                            --disable-swscale \
+                            --disable-lavf \
                             --enable-shared")
 
 def build():
