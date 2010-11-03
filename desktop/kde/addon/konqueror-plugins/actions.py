efd5ec9d76e530e7c15987307e791f1504b573f3
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-11-03 12:36:05 +0000

    Add kwebkitpart and konqueror-plugins

---
+++ desktop/kde/addon/konqueror-plugins/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kde4
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+#WorkDir = "konq-plugins-%s" % get.srcVERSION()
+WorkDir = "konq-plugins"
+NoStrip=["/usr/share"]
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    kde4.configure()
+
+def build():
+    kde4.make()
+
+def install():
+    kde4.install()
+
