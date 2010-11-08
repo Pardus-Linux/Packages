27ec342ce8403168b9b11285bf3b5a8a5eba1273
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-11-08 14:59:01 +0000

    Split juk, dragonplayer, libraries, kmix and kscd as seperate binary packages, so we can get rid of juk and dragon player in 2011 images.

--- desktop/kde/base/kdemultimedia/actions.py
+++ desktop/kde/base/kdemultimedia/actions.py
@@ -5,7 +5,7 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 from pisi.actionsapi import kde4
@@ -21,3 +21,5 @@ def build():
 
 def install():
     kde4.install()
+
+    pisitools.dodoc("COPYING*", "README", "AUTHORS")
