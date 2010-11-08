0b2bf25e4cf68aef49f472f0a729440e748e5659
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-11-08 09:04:49 +0000

    Version bump, fix CVE-2010-3611.

--- server/dhcp/actions.py
+++ server/dhcp/actions.py
@@ -9,6 +9,8 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
+WorkDir = "dhcp-4.2.0-P1"
+
 def setup():
     shelltools.export("CFLAGS", "%s -D_GNU_SOURCE -fPIC" % get.CFLAGS())
 
