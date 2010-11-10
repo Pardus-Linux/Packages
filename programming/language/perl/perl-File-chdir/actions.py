c55be86bc0a9a2a9c9f2f47cfa1eb930118d79fd
Serdar Dalgıç <serdar@pardus.org.tr>
2010-11-10 10:58:19 +0000

    perl-File-chdir is ready for Pardus 2011.

---
+++ programming/language/perl/perl-File-chdir/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# (C) TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import perlmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "%s-%s" % (get.srcNAME()[5:], get.srcVERSION())
+
+def setup():
+    perlmodules.configure()
+
+def build():
+    perlmodules.make()
+
+def check():
+    perlmodules.make("test")
+
+def install():
+    perlmodules.install()
+
+    pisitools.remove("/usr/lib/perl5/vendor_perl/%s/File/chdir.pod" % get.curPERL())
+
