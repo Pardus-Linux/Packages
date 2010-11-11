829bccc1bc5b4836a437a30f6e865dc588531b1b
Renan Çakırerk <renan@pardus.org.tr>
2010-11-11 09:57:25 +0000

    add mysql-python to 2011 repo. Update archive links

---
+++ programming/language/python/mysql-python/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pythonmodules
+
+WorkDir = "MySQL-python-1.2.3"
+
+def build():
+    pythonmodules.compile()
+
+def install():
+    pythonmodules.install()
