4b933af10737f8d825d758f8f6b5403ab04399a1
Necdet Yücel <necdetyucel@gmail.com>
2010-11-12 09:47:46 +0000

    clamtk is ready for 2011

---
+++ util/antivirus/clamtk/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+import os
+import fnmatch
+from pisi.actionsapi import pisitools
+
+
+def setup():
+    pisitools.dosed("clamtk", "use ClamTk::Prefs", "use lib \"/usr/lib/\";\nuse ClamTk::Prefs")
+    pisitools.dosed("clamtk", "use ClamTk::GUI", "use lib \"/usr/lib/\";\nuse ClamTk::GUI")
+    pisitools.dosed("clamtk.desktop", "(?<=Name\=).*", "ClamTK")
+    pisitools.dosed("clamtk.desktop", "(?<=Name\[tr\]\=).*", "ClamTK")
+    pisitools.dosed("clamtk.desktop", "(?<=GenericName\=).*", "Virus Scanner\nGenericName[tr]=Virüs Tarayıcısı")
+
+def install():
+    pisitools.dobin("clamtk")
+    pisitools.insinto("/usr/lib", "lib", "ClamTk")
+    pisitools.doman("clamtk.1.gz")
+    pisitools.insinto("/usr/share/applications", "clamtk.desktop")
+    pisitools.insinto("/usr/share/pixmaps", "clamtk.png")
+    pisitools.dodoc("CHANGES", "DISCLAIMER", "LICENSE", "README")
+
+    #Locales
+    for i in os.listdir("po"):
+        if fnmatch.fnmatch(i, '*.po'):
+            pisitools.domo("po/" + i, i.replace(".po", ""), "clamtk.mo")
