33e45df71dab7ddcc8d1fa11e7b650aa10837c67
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-11-13 10:50:16 +0000

    Bump to latest snapshot, disable eweather.

--- desktop/enlightenment/addon/elementary/actions.py
+++ desktop/enlightenment/addon/elementary/actions.py
@@ -10,7 +10,8 @@ from pisi.actionsapi import get
 
 def setup():
     autotools.autoreconf("-vfi")
-    autotools.configure("--disable-static")
+    autotools.configure("--disable-static \
+                         --disable-eweather")
 
 def build():
     autotools.make()
