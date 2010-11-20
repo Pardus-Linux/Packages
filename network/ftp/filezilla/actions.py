9af1fd9991306aee261b26431c5397ee526364fb
Renan Çakırerk <renan@pardus.org.tr>
2010-11-20 02:01:32 +0000

    Add Filezilla to 2011 repo.

---
+++ network/ftp/filezilla/actions.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.export("WXRC", "/usr/bin/wxrc-2.8")
+    autotools.configure("--disable-static \
+                         --with-wx-config=/usr/bin/wx-config \
+                         --disable-manualupdatecheck \
+                         --disable-autoupdatecheck \
+                         --with-tinyxml=builtin")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    # move fzdefaults.xml.example to /usr/share/filezilla
+    pisitools.domove("/usr/share/filezilla/docs/fzdefaults.xml.example", "/usr/share/filezilla")
+    pisitools.removeDir("/usr/share/filezilla/docs")
+
+    pisitools.dodoc("ChangeLog", "README", "AUTHORS")
+
