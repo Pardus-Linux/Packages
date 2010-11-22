64cea3e2fecba533a109f7100212c9d29f7118b9
Onur Küçük <onur@pardus.org.tr>
2010-11-22 13:57:09 +0000

    Enable static libs Build twice on multiarch and give static libs only Tidy up actions.py Update patch level to 20101120

--- system/base/ncurses/actions.py
+++ system/base/ncurses/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -11,52 +11,77 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 WorkDir = "ncurses-%s" % get.srcVERSION().split("_", 1)[0]
+multilib = (get.ARCH() == "x86_64")
+
+configparams = "--without-debug \
+               --without-profile \
+               --disable-rpath \
+               --enable-const \
+               --enable-largefile \
+               --enable-widec \
+               --with-terminfo-dirs='/etc/terminfo:/usr/share/terminfo' \
+               --disable-termcap \
+               --with-shared \
+               --with-rcs-ids \
+               --with-chtype='long' \
+               --with-mmask-t='long' \
+               --without-ada \
+               --enable-symlinks"
+
+
+def linknonwide(targetDir):
+    # symlink normal objects to widechar ones, to force widechar enabling
+    for f in shelltools.ls("%s/%s/*w.*" % (get.installDIR(), targetDir)):
+        source = shelltools.baseName(f)
+        destination = source.replace("w.", ".")
+        pisitools.dosym(source, "%s/%s" % (targetDir, destination))
 
 def setup():
-    autotools.configure("--without-debug \
-                         --without-profile \
-                         --disable-rpath \
-                         --enable-const \
-                         --enable-largefile \
-                         --enable-widec \
-                         --with-terminfo-dirs='/etc/terminfo:/usr/share/terminfo' \
-                         --disable-termcap \
-                         --with-shared \
-                         --with-rcs-ids \
-                         --with-chtype='long' \
-                         --with-mmask-t='long' \
-                         --without-ada \
-                         --enable-symlinks")
+    autotools.configure(configparams)
+
+    if multilib:
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+        shelltools.makedirs("build32")
+        shelltools.cd("build32")
+        shelltools.system("../configure %s --without-gpm" % configparams)
 
 def build():
     autotools.make()
 
+    if multilib:
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+        shelltools.cd("build32")
+        autotools.make()
+
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    # No static libs
-    pisitools.remove("/usr/lib/*.a")
+    # Handle static libs in /usr/%libdir/static
+    pisitools.dodir("/usr/lib/static")
+    for i in shelltools.ls("%s/usr/lib/*.a" % get.installDIR()):
+        pisitools.domove("/usr/lib/%s" % shelltools.baseName(i), "/usr/lib/static/")
 
-    for file in shelltools.ls(get.installDIR() + "/usr/lib/*w.*"):
-        source = file.replace(get.installDIR(), "")
-        destination = source.replace("w.", ".")
-        pisitools.dosym(source, destination)
+    linknonwide("/usr/lib/static")
+    linknonwide("/usr/lib")
 
-    for file in shelltools.ls(get.installDIR() + "/lib/libncursesw.so*"):
-        source = file.replace(get.installDIR(), "")
-        destination = source.replace("w.", ".")
-        pisitools.dosym(source, destination)
+    if multilib:
+        pisitools.dodir("/usr/lib32/static")
+        for i in shelltools.ls("build32/lib/*.a"):
+            pisitools.insinto("/usr/lib32/static/", "build32/lib/%s" % shelltools.baseName(i))
+
+        linknonwide("/usr/lib32/static")
+
 
     # We need the basic terminfo files in /etc
     terminfo = ["ansi", "console", "dumb", "linux", "rxvt", "screen", "sun", \
                 "vt52", "vt100", "vt102", "vt200", "vt220", "xterm", "xterm-color", "xterm-xfree86"]
 
-    for file in terminfo:
-        termfile = file[0] + "/" + file
+    for f in terminfo:
+        termfile = f[0] + "/" + f
         if shelltools.can_access_file("/usr/share/terminfo/%s" % termfile):
-            pisitools.dodir("/etc/terminfo/%s" % file[0])
-            pisitools.domove("/usr/share/terminfo/%s" % termfile, "/etc/terminfo/%s" % file[0])
-            pisitools.dosym("../../../../etc/terminfo/%s/%s" % (file[0], file ), "/usr/share/terminfo/%s/%s" % (file[0], file ))
+            pisitools.dodir("/etc/terminfo/%s" % f[0])
+            pisitools.domove("/usr/share/terminfo/%s" % termfile, "/etc/terminfo/%s" % f[0])
+            pisitools.dosym("../../../../etc/terminfo/%s/%s" % (f[0], f), "/usr/share/terminfo/%s/%s" % (f[0], f))
 
     pisitools.dodoc("ANNOUNCE", "NEWS", "README*", "TO-DO")
 
