a0ddfcae335e4243f9da99451fff2aeb368462f1
Onur Küçük <onur@pardus.org.tr>
2010-11-23 14:40:58 +0000

    ready for 2011

---
+++ game/action/abuse-data/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+import os
+
+WorkDir = "./"
+datadir = "/usr/share/abuse/"
+
+def fixperms(d):
+    for root, dirs, files in os.walk(d):
+        for name in dirs:
+            shelltools.chmod(os.path.join(root, name), 0755)
+        for name in files:
+            shelltools.chmod(os.path.join(root, name), 0644)
+
+def install():
+    for f in shelltools.ls("./"):
+        if f != "pisiBuildState":
+            fixperms(f)
+            pisitools.insinto(datadir, f)
+
