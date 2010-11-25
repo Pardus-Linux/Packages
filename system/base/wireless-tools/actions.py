3aff103ec917a7a8c98111e9da57a3e673d729d7
Ozan Çağlayan <ozan@pardus.org.tr>
2010-11-25 10:50:01 +0000

    Split devel package, drop COMAR stuff

--- system/base/wireless-tools/actions.py
+++ system/base/wireless-tools/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,17 +10,18 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir="wireless_tools.29"
-
 def setup():
     pisitools.dosed("Makefile", "CC = .*", "CC = %s" % get.CC())
     pisitools.dosed("Makefile", "^CFLAGS=", "CFLAGS=%s " % get.CFLAGS())
 
 def build():
-    autotools.make()
+    autotools.make("BUILD_SHARED=1")
 
 def install():
-    autotools.rawInstall("PREFIX=%s/usr INSTALL_INC=%s/usr/include INSTALL_MAN=%s/usr/share/man" % (get.installDIR(), get.installDIR(), get.installDIR()))
-    pisitools.dosym("/usr/sbin/iwlist", "/usr/bin/iwlist")
+    autotools.rawInstall("PREFIX=%s/usr \
+                          INSTALL_DIR=%s/sbin \
+                          INSTALL_LIB=%s/usr/lib \
+                          INSTALL_INC=%s/usr/include \
+                          INSTALL_MAN=%s/usr/share/man" % ((get.installDIR(),)*5))
 
-    pisitools.dodoc("CHANGELOG.h", "COPYING", "HOTPLUG.txt", "IFRENAME-VS-XXX.txt", "PCMCIA.txt", "README")
+    pisitools.dodoc("COPYING", "README")
