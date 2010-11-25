d456fd37816f66843dba4f80aeec9e1f166f4a2a
Serdar Dalgıç <serdar@pardus.org.tr>
2010-11-25 12:18:08 +0000

    python-imdby is ready for Pardus 2011.

---
+++ programming/language/python/python-imdbpy/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import get
+
+WorkDir = "IMDbPY-%s" % get.srcVERSION()
+
+def setup():
+    pisitools.dosed("setup.py","doc'","share/doc/%s'" % get.srcNAME())
+
+def install():
+    pythonmodules.install()
+
+    pisitools.domove("/usr/imdb/locale", "/usr/share/locale")
+    pisitools.removeDir("/usr/imdb")
+    pisitools.remove("%s/%s/INSTALL.txt" % (get.docDIR(), get.srcNAME()) )
+
