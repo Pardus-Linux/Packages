5739d1f73e9144c5c2d96c9165953ffc74b61b59
Serdar Dalgıç <serdar@pardus.org.tr>
2010-11-26 12:01:06 +0000

    Version bump, add libpng patch and make wesnoth ready for Pardus 2011.

---
+++ game/strategy/wesnoth/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-vif")
+    autotools.configure('--with-fifodir="/var/run/wesnothd" \
+                         --enable-editor \
+                         --enable-server \
+                         --enable-tools \
+                         --disable-rpath \
+                         --disable-dependency-tracking')
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "README", "players_changelog", "copyright")
