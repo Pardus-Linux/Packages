8bdf02e70f76af5ba3689da4ef4e5734cfd423ee
Metin Akdere <metin@pardus.org.tr>
2010-11-26 07:39:24 +0000

    Added metis to 2011.

---
+++ science/mathematics/metis/actions.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+WorkDir="metis-4.0"
+
+def setup():
+    autotools.autoreconf("-vif")
+    autotools.configure("--disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("CHANGES")
