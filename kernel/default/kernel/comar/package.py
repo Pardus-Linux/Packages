ebb69956511ba09cbb1014f64d90063374ea11e3
Ozan Çağlayan <ozan@pardus.org.tr>
2010-11-26 06:31:33 +0000

    Bump to 2.6.36.1
     - Version bump to 2.6.36.1,
     - Depend on new mkinitramfs to create the mkinitramfs,
     - tty: restore tty_ldisc_wait_idle,
     - print tty device name to find out the WARNING in tty_io.c,
     - fix gcc miscompilation issue,
     - fix zero struct memory in ipc compat (CVE-2010-4073),
     - fix zero struct memory in ipc shm (CVE-2010-4072),
     - fix logic error in INET_DIAG bytecode auditing (CVE-2010-3880),
     - posix-cpu-timers: workaround to suppress the problems with mt exec,
     - Fix graphics on HP 2530p.

--- kernel/default/kernel/comar/package.py
+++ kernel/default/kernel/comar/package.py
@@ -6,9 +6,6 @@ import subprocess
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     KVER = open("/etc/kernel/kernel").read().strip()
 
-    # Create initramfs image
-    subprocess.call(["/sbin/mkinitramfs", "--type", "kernel"])
-
     # Update GRUB entry
     if os.path.exists("/boot/grub/grub.conf"):
         call("grub", "Boot.Loader", "updateKernelEntry", (KVER, ""))
