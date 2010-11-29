60b586c10ab9d11855c1503d1c8e15405e045617
Necdet Yücel <necdetyucel@gmail.com>
2010-11-29 13:46:47 +0000

    ready for 2011, thanks for reviews BUG:FIXED:14299

---
+++ programming/language/objectivec/gnustep-base/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-libffi \
+                        --enable-ffcall \
+                        --enable-gnutls \
+                        --enable-tls \
+                        --enable-zeroconf \
+                        --with-default-config=/etc/GNUstep/GNUstep.conf")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "NEWS")
