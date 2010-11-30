cc78eb68e702a1871bbbbead330011f57dd99616
İşbaran Akçayır <isbaran@gmail.com>
2010-11-30 08:54:00 +0000

    ready

---
+++ science/gis/geos/actions.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-static \
+                         --enable-python \
+                         --enable-ruby")
+
+def build():
+    autotools.make()
+    autotools.make("check")
+
+    shelltools.cd("doc")
+    autotools.make("doxygen-html")
+
+def install():
+    autotools.rawInstall("DESTDIR=%(D)s \
+                          PYTHON_PREFIX=%(D)s/usr/lib/%(V)s \
+                          PYTHON_EXEC_PREFIX=%(D)s/usr/lib/%(V)s" % {"D": get.installDIR(),
+                                                                     "V": get.curPYTHON()})
+
+    pisitools.insinto("/usr/share/doc/geos", "doc/doxygen_docs/html")
+    pisitools.dodoc("README", "ChangeLog", "AUTHORS", "NEWS", "COPYING")
