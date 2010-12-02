1276c6e7cc1fc5c1cfcc1c88780837534345c1b2
Serdar Dalgıç <serdar@pardus.org.tr>
2010-12-02 09:12:38 +0000

    Orkhun-fonts ready for Pardus 2011.

---
+++ desktop/font/Orkhun-fonts/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+WorkDir = "orkhun"
+
+def install():
+    shelltools.chmod("*.ttf", 0644)
+    pisitools.insinto("/usr/share/fonts/orkhun/", "*.ttf")
+
+    pisitools.dodoc("LICENSE", "README")
+
