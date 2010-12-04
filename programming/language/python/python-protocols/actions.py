66f4d0904bb90f4a823fbd5655d015c74d2431c7
Fethican Coşkuner <fethicanc@gmail.com>
2010-12-04 16:16:49 +0000

    ready for 2011

---
+++ programming/language/python/python-protocols/actions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def install():
+    pythonmodules.install()
+    pisitools.insinto("/usr/share/doc/%s" % get.srcNAME(), "docs/ref/*.tex")
+    pisitools.insinto("/usr/share/doc/%s" % get.srcNAME(), "README.txt")
