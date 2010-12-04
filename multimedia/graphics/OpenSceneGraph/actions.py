2ea0e28115b6152f57f4eddd0454419e8eec7d7d
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-12-04 11:35:08 +0000

    I love fixing packages that I dont maintain.

--- multimedia/graphics/OpenSceneGraph/actions.py
+++ multimedia/graphics/OpenSceneGraph/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    cmaketools.configure()
+    cmaketools.configure("-DLIB_POSTFIX=''")
 
 def build():
     cmaketools.make("-j1")
