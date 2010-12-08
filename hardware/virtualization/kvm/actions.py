4429a9fc4d923e024ccba403ad904a1fdc3cf4f7
Onur Küçük <onur@pardus.org.tr>
2010-12-08 22:21:28 +0000

    version bump

--- hardware/virtualization/kvm/actions.py
+++ hardware/virtualization/kvm/actions.py
@@ -15,7 +15,7 @@ WorkDir="qemu-kvm-%s" % get.srcVERSION()
 
 cflags = get.CFLAGS().replace("-fpie", "").replace("-fstack-protector", "")
 
-targetList = "i386-softmmu i386-linux-user"
+targetList = "i386-softmmu x86_64-softmmu i386-linux-user x86_64-linux-user"
 
 def setup():
     # disable fdt until dtc is in repo
@@ -23,23 +23,27 @@ def setup():
 
     shelltools.export("CFLAGS", cflags)
     autotools.rawConfigure('--prefix=/usr \
+                            --mandir=/usr/share/man \
+                            --sysconfdir=/etc \
                             --audio-drv-list="alsa pa sdl oss" \
                             --cc="%s" \
                             --host-cc="%s" \
                             --enable-system \
                             --enable-linux-user \
                             --disable-strip \
+                            --disable-xen \
+                            --disable-werror \
                             --disable-bsd-user \
                             --disable-darwin-user' % (get.CC(), get.CC()))
 
-    shelltools.cd("kvm/user")
+    shelltools.cd("kvm/test")
     autotools.rawConfigure("--prefix=/usr \
                             --kerneldir=../../kernel")
 
 
 def build():
     autotools.make("V=1")
-    autotools.make("-C kvm/user kvmtrace")
+    autotools.make("-C kvm/test kvmtrace")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
@@ -48,14 +52,16 @@ def install():
     pisitools.domove("/usr/share/man/man1/qemu.1", "/usr/share/man/man1/", "qemu-kvm.1")
 
     # Install kvm-tools
-    pisitools.dobin("kvm/user/kvmtrace")
-    pisitools.dobin("kvm/user/kvmtrace_format")
+    pisitools.dobin("kvm/test/kvmtrace")
+    pisitools.dobin("kvm/test/kvmtrace_format")
     pisitools.dobin("kvm/kvm_stat")
 
     # Use the one qemu provides
     pisitools.remove("/usr/bin/qemu-img")
     pisitools.remove("/usr/share/man/man1/qemu-img.1")
     pisitools.remove("/usr/bin/qemu-nbd")
+    pisitools.remove("/etc/qemu/target-*")
 
     pisitools.removeDir("/usr/share/man/man8/")
     pisitools.removeDir("/usr/share/doc")
+
