08c2a1b4d706be4426ce4c7106cd038fa88bd4ca
Fatih Arslan <farslan@pardus.org.tr>
2010-12-08 17:50:02 +0000

    Make file paths compatible with KDE4, cleanup actions.py

--- tex/tool/kile/actions.py
+++ tex/tool/kile/actions.py
@@ -5,29 +5,16 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import kde
+from pisi.actionsapi import kde4
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import cmaketools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
-import os
-
-WorkDir = "kile-2.1b4"
-shelltools.export("HOME", get.workDIR())
 
 def setup():
-    shelltools.makedirs("build")
-    shelltools.cd("build")
-
-    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr/kde/4", sourceDir="..")
+    kde4.configure()
 
 def build():
-    shelltools.cd("build")
-    cmaketools.make()
+    kde4.make()
 
 def install():
-    shelltools.cd("build")
-    cmaketools.install("DESTDIR=%s" % get.installDIR())
-
-    shelltools.cd("..")
+    kde4.install()
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
