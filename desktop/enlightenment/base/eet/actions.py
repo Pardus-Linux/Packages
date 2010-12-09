1b0ad5170e9b108a4d27c02a226cb8ba0acc0d83
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-12-09 11:17:31 +0000

    Version bump.

--- desktop/enlightenment/base/eet/actions.py
+++ desktop/enlightenment/base/eet/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "eet-1.4.0.beta2"
+WorkDir = "eet-1.4.0.beta3"
 
 def setup():
     autotools.autoreconf("-fi")
