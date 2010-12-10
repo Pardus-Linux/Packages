fabe6d6fd8432b7dbb12b77fa5947ad092be0ca4
Ozan Çağlayan <ozan@pardus.org.tr>
2010-12-10 10:22:39 +0000

    Copy to 2011
    BUG:FIXED:14775

---
+++ hardware/powermanagement/upower/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+# For g-i-r
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-static \
+                         --disable-gtk-doc \
+                         --enable-introspection")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("ChangeLog", "COPYING", "README")
