54a4c12fe684687acb1540b6db3e9310f647e2af
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-12-13 09:05:16 +0000

    Silently switch to stable release of new gtk-oxygen theme written in pure gtk/cairo.

--- desktop/toolkit/gtk/gtk-style-oxygen/actions.py
+++ desktop/toolkit/gtk/gtk-style-oxygen/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,7 +9,7 @@ from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "gtk-oxygen-engine-%s" % get.srcVERSION()
+WorkDir = "oxygen-gtk-%s" % get.srcVERSION()
 
 def setup():
     cmaketools.configure()
