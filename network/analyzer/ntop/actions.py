ea2e82069bdf7485e920fc77bafb232908361886
Erdem Bayer <ebayer@bayer.gen.tr>
2010-12-13 14:24:03 +0000

    Merge from corporate2/devel/network/analyzer/ntop@107103

---
+++ network/analyzer/ntop/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+#WorkDir = ""
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.system("./autogen.sh --noconfig")
+    autotools.configure("--enable-mysql --enable-fc --enable-efficiency \
+                        --enable-snmp --disable-static \
+                        --localstatedir=/var/lib")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    # remove empty dir
+    pisitools.removeDir("/usr/lib/plugins")
