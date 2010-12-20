95c1b922b0904ced07583a5af8360a035db2fb48
Beyza Ermiş <beyza@pardus.org.tr>
2010-12-20 08:53:26 +0000

    Supertuxkart is ready for 2011

---
+++ game/sports/supertuxkart/actions.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+import os
+
+#WorkDir = "supertuxkart-%sa" % get.srcVERSION()
+
+def removegames(dest):
+    for root, dirs, files in os.walk(dest):
+        for name in files:
+            if name.startswith("Makefile"):
+                pisitools.dosed(os.path.join(root, name), "games/")
+
+def setup():
+    # FIXME: do it by hand until pisi actions api change is packaged
+    # for i in `find -name "Makefile*"|xargs grep games|sed 's/:.*//g' |cut -c3-|sort -u`;do sed -i -e 's/\/games//g' $i;done
+    # removegames("./")
+
+    autotools.autoreconf("-vfi")
+    autotools.configure()
+
+def build():
+    autotools.make("SUPERTUXKART_DATADIR=/usr/share/supertuxkart")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    # pisitools.dobin("src/supertuxkart")
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "COPYING", "TODO")
+
