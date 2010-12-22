8e5e1edd9911c01c1176a8456204545083b3e66a
Ozan Çağlayan <ozan@pardus.org.tr>
2010-12-22 08:42:34 +0000

    Version bump

--- hardware/powermanagement/acpica/actions.py
+++ hardware/powermanagement/acpica/actions.py
@@ -5,20 +5,17 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
-
-WorkDir = "acpica-unix-%s" % get.srcVERSION().split('_')[-1]
 
 def build():
     shelltools.export("CFLAGS", "%s -fno-strict-aliasing" % get.CFLAGS())
     pisitools.dosed("tools/acpiexec/Makefile", "CFLAGS\+= -Wall -g", "CFLAGS+= -Wall")
 
     for i in ["tools/acpisrc", "tools/acpixtract", "tools/acpiexec", "compiler/ clean", "compiler/"]:
-        autotools.make("-C %s" % i)
-
+        autotools.make("-j1 -C %s" % i)
 
 def install():
     pisitools.dobin("compiler/iasl")
@@ -27,4 +24,3 @@ def install():
         pisitools.dosbin("tools/%s/%s" % (i, i))
 
     pisitools.dodoc("changes.txt", "README")
-
