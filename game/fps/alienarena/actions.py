b6e662149c6afcd387849bb1df135c9acd30cef4
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2010-12-22 13:12:36 +0000

    Version bump (#15034).
    BUG:COMMENT:15034

--- game/fps/alienarena/actions.py
+++ game/fps/alienarena/actions.py
@@ -9,32 +9,20 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-datadir = "/usr/share/alienarena"
-libdir = "/usr/lib/alienarena"
+shelltools.export("PTHREAD_LIBS", "-lpthread")
+shelltools.export("PTHREAD_CFLAGS", "-pthread")
 
 def setup():
-    # Use system GL header
-    pisitools.dosed("source/ref_gl/r_local.h", "glext.h", "GL/glext.h")
+    # Use system libopenal
+    pisitools.dosed("source/unix/qal_unix.c", "libopenal.so", "$LIBOPENAL")
+    autotools.configure()
 
 def build():
-    shelltools.cd("source")
-    autotools.make("CC=%s \
-                    PREFIX=/usr \
-                    BUILD=ALL \
-                    OPTIM_LVL=2 \
-                    SDLSOUND=yes \
-                    WITH_DATADIR=yes \
-                    WITH_LIBDIR=yes \
-                    DATADIR=%s \
-                    LIBDIR=%s \
-                    OPTIMIZED_CFLAGS=no" % (get.CC(), datadir, libdir))
+    autotools.make()
 
 def install():
-    shelltools.cd("source")
-    pisitools.dobin("release/crded")
-    pisitools.dobin("release/crx")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.rename("/usr/bin/crded", "alienarena-server")
     pisitools.rename("/usr/bin/crx", "alienarena")
+    pisitools.rename("/usr/bin/crx-ded", "alienarena-server")
 
-    pisitools.doexe("release/game.so", "%s/arena" % libdir)
