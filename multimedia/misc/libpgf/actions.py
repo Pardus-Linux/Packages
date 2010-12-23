ac82b1f9da47cb83fc7335e04a562d5832f49cb0
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-12-23 06:36:19 +0000

    Add libpgf for digikam

---
+++ multimedia/misc/libpgf/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009,2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    pisitools.dosed("doc/Makefile.in", "^DOC_MODULE = .*$", "DOC_MODULE = ${PACKAGE}")
+    autotools.configure("--disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "README", "NEWS", "FAQ")
