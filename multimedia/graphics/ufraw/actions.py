3ed879c06fdf4e7159f5f608607691484444e831
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-12-25 20:59:28 +0000

    Add ufraw to 2011.
    BUG:FIXED:15828

---
+++ multimedia/graphics/ufraw/actions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.system("./autogen.sh")
+
+    autotools.configure("--enable-mime \
+                         --enable-extras \
+                         --enable-contrast \
+                         --with-lensfun")
+
+    pisitools.dosed("Makefile", "/usr/lib/gimp/", "%s/usr/lib/gimp/" % get.installDIR())
+
+def build():
+    autotools.make("schemasdir=/etc/gconf/schemas")
+
+def install():
+    autotools.install("schemasdir=%s/etc/gconf/schemas" % get.installDIR())
+
+    # Do not conflict with dcraw package
+    pisitools.remove("/usr/bin/dcraw")
+
+    pisitools.insinto("/usr/share/mime/packages", "ufraw-mime.xml")
+
+    pisitools.dodoc("COPYING", "MANIFEST", "README")
