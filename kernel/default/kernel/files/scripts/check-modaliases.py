46febfea10280a584a98411944d3b674ea2487b6
Ozan Çağlayan <ozan@pardus.org.tr>
2010-12-26 15:12:57 +0000

    Sync scripts

---
+++ kernel/default/kernel/files/scripts/check-modaliases.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Analyze the given kernel for drivers racing for
+# the same devices.
+
+import os
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print "Usage: %s <kernel version>" % sys.argv[0]
+        sys.exit(1)
+
+    kver = sys.argv[1]
+    alias_file = "/lib/modules/%s/modules.alias" % kver
+    aliases = {}
+
+    if os.path.exists(alias_file):
+        for alias in open(alias_file, "r").readlines():
+            if alias and not alias.startswith("#"):
+                modalias, driver = alias.strip().split(" ", 2)[1:]
+                if modalias.startswith("char-"):
+                    continue
+                try:
+                    aliases[modalias].add(driver)
+                except KeyError:
+                    aliases[modalias] = set([driver])
+
+    # Dump results
+    for modalias, drivers in aliases.items():
+        if len(drivers) > 1:
+            # More than 1 driver supports this alias
+            print "%s: " % modalias,
+            print ",".join(drivers)
+
+
--- kernel/default/kernel/files/scripts/fedora-list-patches.py
+++ kernel/default/kernel/files/scripts/fedora-list-patches.py
@@ -4,23 +4,54 @@
 import os
 import sys
 
-patches = []
-all_patches = os.popen("find files/ -name '*.patch'").read().strip().split("\n")
+from hashlib import md5
 
-def is_avail(patch):
-    for p in all_patches:
-        if patch in p:
-            return p
+(SIZE, ALREADYINSPEC, DIFF) = range(3)
 
-for line in open(sys.argv[1], "r").readlines():
-    if not line.strip().startswith("#") and ("ApplyPatch" in line or "ApplyOptionalPatch" in line):
-        try:
-            patch = line.strip().split()[1]
-            p = os.path.join(os.path.dirname(sys.argv[1]), patch)
-            if os.path.exists(p) and not is_avail(patch):
-                patches.append("%s %s" % (str(os.stat(p).st_size), patch ))
-        except:
-            pass
+def files_are_same(filea, fileb):
+    md5a = md5(open(filea, "r").read())
+    md5b = md5(open(fileb, "r").read())
 
-patches.sort(cmp=lambda x,y: int(x.split()[0])-int(y.split()[0]))
-print "\n".join(patches)
+    return md5a.hexdigest() == md5b.hexdigest()
+
+def usage():
+    print "%s <path to RPM spec> [--different|--new]" % sys.argv[0]
+    return 1
+
+if __name__ == "__main__":
+
+    patches = {}
+    rpm = None
+    try:
+        rpm = sys.argv[1]
+    except IndexError:
+        sys.exit(usage())
+
+    dirname = os.path.dirname(rpm)
+
+    for line in open(rpm, "r").readlines():
+        line = line.strip()
+        if not line.startswith("#") and \
+            line.startswith(("ApplyPatch", "ApplyOptionalPatch")) and \
+            "()" not in line:
+            fedora_patch_name = line.split()[1]
+            fedora_patch_file = os.path.join(dirname, fedora_patch_name)
+            if os.path.exists(fedora_patch_file):
+                pspec_patch_file = os.path.join("files/fedora", fedora_patch_name)
+                if not os.path.exists(pspec_patch_file):
+                    patches[fedora_patch_name] = (str(os.stat(fedora_patch_file).st_size),
+                                                  False,
+                                                  False)
+                else:
+                    patches[fedora_patch_name] = (str(os.stat(fedora_patch_file).st_size),
+                                                  os.path.exists(pspec_patch_file),
+                                                  not files_are_same(fedora_patch_file, pspec_patch_file))
+
+
+    for patch, stats in patches.items():
+        if stats[DIFF]:
+            print "M %10s  %s" % (stats[SIZE], patch)
+        elif not stats[ALREADYINSPEC]:
+            # New patches
+            print "? %10s  %s" % (stats[SIZE], patch)
+
