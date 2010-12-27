6c224959a6ce4c6dc2ce06ee5a70f46f59722709
Fethican Coşkuner <fethicanc@gmail.com>
2010-12-27 21:51:24 +0000

    Version bump

--- programming/language/python/python-decorator/actions.py
+++ programming/language/python/python-decorator/actions.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
-WorkDir = "decorator-%s" % get.srcVERSION()
+def check():
+    shelltools.system("nosetests --with-doctest -e documentation")
 
 def install():
     pythonmodules.install()
 
-    pisitools.dodoc("CHANGES.txt", "documentation.html", "documentation.py")
+    pisitools.dodoc("README.txt", "documentation.py")
