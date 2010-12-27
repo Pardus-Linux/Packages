3c87e21494e9f982fd9de133abf8e244e151d51e
Onur Küçük <onur@pardus.org.tr>
2010-12-27 22:26:20 +0000

    Bump to latest pre Use fixed fonts for xmode2 Install irman2lirc

--- hardware/irda/lirc/actions.py
+++ hardware/irda/lirc/actions.py
@@ -10,23 +10,26 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "lirc-%s" % get.srcVERSION().replace("_", "")
+WorkDir = "lirc-%s" % get.srcVERSION().replace("_", "-")
 ldflags = get.LDFLAGS().replace("-Wl,-O1", "")
 
 def setup():
     shelltools.export("LDFLAGS", ldflags)
     autotools.autoreconf("-vfi")
     pisitools.dosed("configure*", "portaudio.h", "PORTAUDIO_DISABLED")
+    pisitools.dosed("configure*", "vga.h", "SVGALIB_DISABLED")
+
+    # we will use it
+    pisitools.dosed("contrib/irman2lirc", "/usr/local/etc/", "/etc/")
 
     autotools.configure("--localstatedir=/var \
                          --enable-sandboxed \
                          --enable-shared \
                          --disable-static \
                          --disable-debug \
+                         --disable-dependency-tracking \
                          --with-transmitter \
                          --with-x \
-                         --with-port=0x3f8 \
-                         --with-irq=4 \
                          --with-driver=userspace \
                          --with-syslog=LOG_DAEMON")
 
@@ -35,6 +38,7 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dobin("contrib/irman2lirc")
 
     # needed for lircd pid
     pisitools.dodir("/var/run/lirc")
