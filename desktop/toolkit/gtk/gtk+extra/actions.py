aab245b6a33d37bb5d3f355ea13584e8bb726013
Gökhan Özbulak <gozbulak@pardus.org.tr>
2010-12-27 12:56:57 +0000

    gtk+extra package for 2011

---
+++ desktop/toolkit/gtk/gtk+extra/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.autoreconf("-vif")
+    autotools.configure("--disable-static \
+                         --enable-gtk-doc")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "README")
