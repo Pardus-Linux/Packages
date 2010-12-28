5b2e746980f303c988133709247c34ca8714801b
Ozan Çağlayan <ozan@pardus.org.tr>
2010-12-28 13:02:36 +0000

    drop these bloatware

--- kernel/default/drivers/module-slmodem/comar/package.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    os.system("/bin/chown -R root:dialout /var/lib/slmodem")
--- kernel/default/drivers/module-slmodem/files/slamr.modprobe.d
+++
@@ -1,7 +0,0 @@
-# modprobe.d configuration file for SLAMR
-
-
-# Configurable module parameters
-# ------------------------------
-# debug:	debug level: 0-3 (default=0)
-
