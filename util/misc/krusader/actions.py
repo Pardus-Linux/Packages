d76a7df50d340d443b0d28ee0146904b1bbeec32
David Stegbauer <daaste@gmail.com>
2010-12-28 21:29:01 +0000

    krusader-2.3.0_beta1

--- util/misc/krusader/actions.py
+++ util/misc/krusader/actions.py
@@ -4,27 +4,22 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import cmaketools
-from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import kde4
+from pisi.actionsapi import get
 
 NoStrip=["/usr/share", "/usr/man"]
 WorkDir="krusader-%s" % get.srcVERSION().replace("_","-")
 shelltools.export("HOME", get.workDIR())
 
 def setup():
-    shelltools.makedirs("build")
-    shelltools.cd("build")
-    cmaketools.configure(installPrefix="/usr", sourceDir="..")
+    kde4.configure()
 
 def build():
-    shelltools.cd("build")
-    cmaketools.make()
+    kde4.make()
 
 def install():
-    shelltools.cd("build")
-    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-    shelltools.cd("..")
+    kde4.install()
     pisitools.dosym("/usr/share/icons/hicolor/48x48/apps/krusader_user.png", "/usr/share/icons/hicolor/48x48/apps/krusader.png")
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README", "FAQ")
