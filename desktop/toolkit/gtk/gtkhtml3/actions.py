9170f5ccdf01737262f4224c57f13f178bdb8893
Gökhan Özbulak <gozbulak@pardus.org.tr>
2010-12-28 14:33:36 +0000

    gtkhtml3 package for 2011

---
+++ desktop/toolkit/gtk/gtkhtml3/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-vfi")
+    autotools.configure("--disable-static \
+                         --disable-deprecated-warning-flags")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("TODO", "README", "NEWS", "ChangeLog", "BUGS", "AUTHORS")
