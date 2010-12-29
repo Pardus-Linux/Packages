a3e2981c11e2e748b98e090596a890529945b914
Onur Küçük <onur@pardus.org.tr>
2010-12-29 21:18:06 +0000

    port to kde4 actionsapi

--- hardware/powermanagement/knutclient/actions.py
+++ hardware/powermanagement/knutclient/actions.py
@@ -6,17 +6,18 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import cmaketools
+from pisi.actionsapi import kde4
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 shelltools.export("HOME", "%s" % get.workDIR())
 
 def setup():
-    cmaketools.configure(installPrefix="/usr/kde/4", sourceDir=".")
+    kde4.configure()
 
 def build():
-    cmaketools.make()
+    kde4.make()
 
 def install():
-    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    #cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    kde4.install()
