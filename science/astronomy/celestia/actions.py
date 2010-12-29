d82c43eb3323a289015ea6e3a9b28ebb72bc3ad2
Gökçen Eraslan <gokcen@pardus.org.tr>
2010-12-29 09:21:57 +0000

    Add celestia package from 2008, KDE3 is not available but this application also has a GTK interface.
    BUG:FIXED:15868

---
+++ science/astronomy/celestia/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--with-gtk \
+                   --with-pic \
+                   --enable-pch \
+                   --disable-rpath \
+                   --enable-theora \
+                   --without-arts \
+                   --with-lua")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
