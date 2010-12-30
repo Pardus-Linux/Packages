faf5f7fd29f4bf7589c2b992977d70d5fcbc613b
Çağlar Kilimci <ckilimci@pardus.org.tr>
2010-12-30 13:20:33 +0000

    wxMaxima for 2011. Also, version bump.

---
+++ science/mathematics/wxMaxima/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--enable-printing \
+                         --enable-unicode-glyphs")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.insinto("/usr/share/applications", "wxmaxima.desktop")
+    pisitools.dosym("/usr/share/wxMaxima/wxmaxima.png", "/usr/share/pixmaps/wxmaxima.png")
+
+    pisitools.remove("/usr/share/wxMaxima/COPYING")
+    pisitools.remove("/usr/share/wxMaxima/README")
+
+    pisitools.dodoc("NEWS", "AUTHORS", "ChangeLog", "README", "COPYING")
