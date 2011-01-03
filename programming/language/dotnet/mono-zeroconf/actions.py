45f6af6817f4c160c0cb96426bd829a63cb9a9fa
Gökmen Göksel <gokmen@pardus.org.tr>
2011-01-03 12:29:56 +0000

    - Mono-zeroconf is ready for 2011

---
+++ programming/language/dotnet/mono-zeroconf/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009, 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+shelltools.export("MONO_SHARED_DIR", get.workDIR())
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make("-j1")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
+
