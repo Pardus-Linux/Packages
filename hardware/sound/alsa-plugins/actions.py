9fda449526fb517ba6d752be11a85b208b5c5e03
Ozan Çağlayan <ozan@pardus.org.tr>
2011-01-05 14:51:20 +0000

    Bump

--- hardware/sound/alsa-plugins/actions.py
+++ hardware/sound/alsa-plugins/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2009-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -19,12 +19,7 @@ def setup():
     libtools.libtoolize()
 
     autotools.configure("--disable-static \
-                         --enable-avcodec \
-                         --enable-jack \
-                         --enable-samplerate \
-                         --enable-pulseaudio \
-                         --with-speex=lib \
-                         --disable-dependency-tracking")
+                         --with-speex=lib")
 
 def build():
     autotools.make()
