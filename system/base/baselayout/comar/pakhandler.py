a986cd1258cbdccfa135ae7371e3bd760211ed81
Fatih Aşıcı <fatih@pardus.org.tr>
2011-01-06 13:21:13 +0000

    Change package handler to look all paths written in ldconfig files and update cache when needed

--- system/base/baselayout/comar/pakhandler.py
+++ system/base/baselayout/comar/pakhandler.py
@@ -1,13 +1,25 @@
 # -*- coding: utf-8 -*-
 
-import piksemel
-import subprocess
 
 def update_ld_so_cache(filepath):
+    import glob
+    import piksemel
+    import subprocess
+
+    libdirs = []
+
+    for config_file in glob.glob("/etc/ld.so.conf.d/*.conf"):
+        for line in open(config_file):
+            line = line.strip()
+            if line.startswith("/"):
+                libdirs.append(line[1:])
+
+    libdirs = tuple(libdirs)
+
     doc = piksemel.parse(filepath)
     for item in doc.tags("File"):
         path = item.getTagData("Path")
-        if path.startswith("etc/ld.so.conf.d"):
+        if path.startswith(libdirs):
             subprocess.call(["/sbin/ldconfig", "-X"])
             return
 
