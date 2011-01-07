d8e764ff97ff40f278dc9622e9982863482278ff
Ozan Çağlayan <ozan@pardus.org.tr>
2011-01-07 11:27:50 +0000

    - Bump to latest, - Fix FAQ path (#15921), - Fix tvlist shortcut crash (#15296),
    BUG:FIXED:15921
    BUG:FIXED:15296

--- multimedia/video/smplayer/actions.py
+++ multimedia/video/smplayer/actions.py
@@ -4,14 +4,20 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    pisitools.dosed("Makefile", "^DOC_PATH=.*$", "DOC_PATH=$(PREFIX)/share/doc/smplayer")
+
 def build():
-    autotools.make("PREFIX=/usr")
+    autotools.make("QMAKE=qmake-qt4 LRELEASE=lrelease-qt4 PREFIX=/usr")
 
 def install():
-    autotools.rawInstall("PREFIX=/usr DESTDIR=%s DOC_PATH=/usr/share/doc/%s" % (get.installDIR(),get.srcNAME()))
+    autotools.rawInstall("QMAKE=qmake-qt4 LRELEASE=lrelease-qt4 PREFIX=/usr DESTDIR=%s DOC_PATH=/usr/share/doc/%s" % (get.installDIR(),get.srcNAME()))
 
-    pisitools.copytree("Oxygen-Air", "%s/usr/share/smplayer/themes/Oxygen-Air" % get.installDIR())
+    pisitools.copytree("../smplayer-oxygen-air-theme", "%s/usr/share/smplayer/themes/Oxygen-Air" % get.installDIR())
