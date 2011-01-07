1abcb908988144c5dc4fbed0fcc52674a291bb4f
Serdar Dalgıç <serdar@pardus.org.tr>
2011-01-07 14:15:53 +0000

    Add id3lib dependency and enable id2to23 conversion.

--- multimedia/editor/easytag/actions.py
+++ multimedia/editor/easytag/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--disable-id3v23")
+    autotools.configure()
 
 def build():
     autotools.make()
