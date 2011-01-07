fb282c025d48b1fdd6c858beabd30877dc3512b0
Serdar Dalgıç <serdar@pardus.org.tr>
2011-01-07 13:34:25 +0000

    Make dia ready for Pardus 2011.

---
+++ office/misc/dia/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    autotools.autoreconf("-fiv")
+    autotools.configure("--with-python \
+                         --with-cairo")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.removeDir("/usr/share/oaf")
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README")
