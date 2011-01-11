0a9108c60efbbfe4630f2a8ab25f5b528922ca97
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2011-01-11 14:36:28 +0000

    Depend on NetworkManager and wait enough for the interfaces being up, refactor service scripts (#11735).

--- server/dhcp/comar/dhcpd.py
+++ server/dhcp/comar/dhcpd.py
@@ -4,21 +4,32 @@ from comar.service import *
 serviceType = "server"
 serviceDesc = _({"en": "DHCP Daemon",
                  "tr": "DHCP Servisi"})
+
 serviceConf = "dhcpd"
 
-pidfile = "/var/run/dhcpd.pid"
+MSG_NM_NOT_RUNNING = _({"en": "NetworkManager service is not running",
+                        "tr": "NetworkManager hizmeti çalışmıyor",
+                       })
+
+PIDFILE = "/var/run/dhcpd.pid"
+TIMEOUT = config.get("TIMEOUT", 5)
 
 @synchronized
 def start():
+    startDependencies("NetworkManager")
+    if run("/usr/bin/nm-online -q -t %s" % TIMEOUT) != 0:
+        # NM is not running
+        fail(MSG_NM_NOT_RUNNING)
+
     startService(command="/usr/sbin/dhcpd",
-                 args="%s %s" % (config.get("DHCPD_ARGS", ""), config.get("INTERFACES", "")),
+                 args="-cf %s %s %s" % (config.get("DHCPD_CONF", "/etc/dhcp/dhcpd.conf"), config.get("DHCPD_ARGS", ""), config.get("INTERFACES", "")),
+                 pidfile=PIDFILE,
                  donotify=True)
 
 @synchronized
 def stop():
-    stopService(command="/usr/sbin/dhcpd",
-                pidfile=pidfile,
+    stopService(pidfile=PIDFILE,
                 donotify=True)
 
 def status():
-    return isServiceRunning(pidfile)
+    return isServiceRunning(PIDFILE)
--- server/dhcp/comar/dhcpd6.py
+++ server/dhcp/comar/dhcpd6.py
@@ -4,21 +4,32 @@ from comar.service import *
 serviceType = "server"
 serviceDesc = _({"en": "DHCPv6 Daemon",
                  "tr": "DHCPv6 Servisi"})
+
 serviceConf = "dhcpd6"
 
-pidfile = "/var/run/dhcpd6.pid"
+MSG_NM_NOT_RUNNING = _({"en": "NetworkManager service is not running",
+                        "tr": "NetworkManager hizmeti çalışmıyor",
+                       })
+
+PIDFILE = "/var/run/dhcpd6.pid"
+TIMEOUT = config.get("TIMEOUT", 5)
 
 @synchronized
 def start():
+    startDependencies("NetworkManager")
+    if run("/usr/bin/nm-online -q -t %s" % TIMEOUT) != 0:
+        # NM is not running
+        fail(MSG_NM_NOT_RUNNING)
+
     startService(command="/usr/sbin/dhcpd",
-                 args="%s %s" % (config.get("DHCPD_ARGS", ""), config.get("INTERFACES", "")),
+                 args="-6 -cf %s %s %s" % (config.get("DHCPD_CONF", "/etc/dhcp/dhcpd.conf"), config.get("DHCPD_ARGS", ""), config.get("INTERFACES", "")),
+                 pidfile=PIDFILE,
                  donotify=True)
 
 @synchronized
 def stop():
-    stopService(command="/usr/sbin/dhcpd",
-                pidfile=pidfile,
+    stopService(pidfile=PIDFILE,
                 donotify=True)
 
 def status():
-    return isServiceRunning(pidfile)
+    return isServiceRunning(PIDFILE)
