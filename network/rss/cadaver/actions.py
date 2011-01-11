e97dc51df9d17ec19f749459f7e6daeed7c40c34
Metin Akdere <metin@pardus.org.tr>
2011-01-11 08:36:22 +0000

    Added cadaver to 2011

---
+++ network/rss/cadaver/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--with-ssl=openssl \
+                         --enable-threadsafe-ssl=posix \
+                         --enable-threads=posix \
+                         --with-neon \
+                         --with-libxml2 \
+                         --with-expat")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("ChangeLog", "FAQ", "COPYING", "NEWS", "README", "TODO", "THANKS")
