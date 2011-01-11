0b822bb498e163ba2ef68e0535fef211bbc99c97
Beyza Ermiş <beyza@pardus.org.tr>
2011-01-11 12:09:43 +0000

    Warzone is ready for 2011.

---
+++ game/strategy/warzone2100/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    autotools.configure('--with-icondir=/usr/share/pixmaps \
+                         --disable-rpath \
+                         --disable-motif \
+                         --with-distributor="Pardus" \
+                         --disable-debug')
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
