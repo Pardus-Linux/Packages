0753fcc969ca24a62eb7a1ef4048610e0699b350
Serdar Dalgıç <serdar@pardus.org.tr>
2011-01-12 14:37:23 +0000

    Rearrange the dependencies for Pardus 2011. Leave the package to İşbaran ;)

---
+++ game/simulation/flightgear/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "FlightGear-%s" % get.srcVERSION()
+
+def setup():
+    autotools.configure("--enable-sp-fdms \
+                         --with-threads \
+                         --enable-sdl \
+                         --with-x")
+
+def build():
+    autotools.make("-j1")
+
+def install():
+    autotools.install()
+    pisitools.dodoc("README*", "ChangeLog", "AUTHORS", "NEWS", "Thanks")
