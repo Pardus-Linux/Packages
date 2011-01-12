f246f19c022211d508d919f78b1a3032b4fd9480
Uğur Çetin <ugur.jnmbk@gmail.com>
2011-01-12 03:13:09 +0000

    kotaci is ready for 2011 BUG:FIXED:15370

---
+++ network/monitor/kotaci/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import qt4
+
+def setup():
+    qt4.configure()
+
+    pisitools.dosed("data/kotaci.desktop", "ok", "kotaci")
+
+def build():
+    qt4.make()
+
+def install():
+    pisitools.dobin("bin/kotaci")
+    pisitools.insinto("/usr/share/applications", "data/kotaci.desktop")
+
+    pisitools.insinto("/usr/share/pixmaps", "data/icons/ok.png")
+    pisitools.rename("/usr/share/pixmaps/ok.png", "kotaci.png")
+
+    pisitools.dodoc("AUTHORS", "COPYING", "README", "TODO")
