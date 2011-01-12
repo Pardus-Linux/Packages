78eaa6c342e8fedea4fc29edd7f6f1228262ad5b
Serdar Dalgıç <serdar@pardus.org.tr>
2011-01-12 10:38:17 +0000

    Add kchmviewer to Pardus 2011.
    BUG:FIXED:16172

---
+++ office/misc/kchmviewer/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import kde4
+
+WorkDir="build-%s" % get.srcVERSION()
+
+def setup():
+    kde4.configure()
+
+def build():
+    kde4.make()
+
+def install():
+    kde4.install()
+
+    pisitools.insinto("/usr/share/pixmaps/", "packages/kchmviewer.png")
