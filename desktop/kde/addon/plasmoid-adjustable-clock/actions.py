1000de01f69cb540f6f1d1cb2339db4ee046e250
Çağlar Kilimci <ckilimci@pardus.org.tr>
2011-01-13 09:20:09 +0000

    Build for 2011

---
+++ desktop/kde/addon/plasmoid-adjustable-clock/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="adjustableclock-%s" % get.srcVERSION()
+shelltools.export("HOME", "%s" % get.workDIR())
+
+def setup():
+    cmaketools.configure(installPrefix="/usr/kde/4", sourceDir=".")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("README", "COPYING", "CHANGELOG")
