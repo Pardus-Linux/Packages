8db558133df92560556b7c4b04cd0709036ea0c4
Çağlar Kilimci <ckilimci@pardus.org.tr>
2011-01-13 08:04:16 +0000

    Build for 2011

---
+++ desktop/font/Inconsolata-fonts/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+WorkDir="."
+
+def install():
+    shelltools.chmod("*.otf",0644)
+
+    pisitools.insinto("/usr/share/fonts/inconsolata","*.otf")
+
+    pisitools.dosym("../conf.avail/inconsolata-fonts-fontconfig.conf", "/etc/fonts/conf.d/inconsolata-fonts-fontconfig.conf")
