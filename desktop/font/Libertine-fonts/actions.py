e392c18300b19c86fc3dee12831d5eeb507076b7
Çağlar Kilimci <ckilimci@pardus.org.tr>
2011-01-13 08:20:01 +0000

    Build for 2011

---
+++ desktop/font/Libertine-fonts/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "LinLibertineSRC"
+shelltools.export("HOME", get.workDIR())
+
+def build():
+    shelltools.system("fontforge -lang=ff -script buildscript *.sfd")
+
+def install():
+    pisitools.insinto("/usr/share/fonts/libertine","*.ttf")
+
+    pisitools.dodoc("*.txt")
