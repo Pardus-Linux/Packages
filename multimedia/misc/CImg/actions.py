60f3ab8785608f665d971050f15e0274eb8acef8
Serdar Dalgıç <serdar@pardus.org.tr>
2011-01-13 13:06:22 +0000

    Add CImg to Pardus 2011.

---
+++ multimedia/misc/CImg/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+examples = "%s/%s/examples" % (get.docDIR(), get.srcNAME())
+plugins = "%s/%s/plugins" % (get.dataDIR(), get.srcNAME())
+
+def install():
+    pisitools.insinto("/usr/include", "CImg.h")
+
+    pisitools.insinto(examples, "examples/*")
+    pisitools.insinto(plugins, "plugins/*")
+    pisitools.dohtml("html/*")
+    pisitools.dodoc("*.txt")
