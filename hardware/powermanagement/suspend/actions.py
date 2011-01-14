ffc0231124bb29ed167b4638a7284920de3119ef
Ozan Çağlayan <ozan@pardus.org.tr>
2011-01-14 15:00:10 +0000

    Add plymouth support, cleanup package

--- hardware/powermanagement/suspend/actions.py
+++ hardware/powermanagement/suspend/actions.py
@@ -3,29 +3,28 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 WorkDir = "suspend-%s" % get.srcVERSION()
 
 def setup():
     autotools.autoreconf("-vif")
-    autotools.configure("--enable-static \
+    autotools.configure("--enable-threads \
                          --enable-compress \
+                         --enable-plymouth \
                          --disable-encrypt \
+                         --disable-resume-static \
                          --with-initramfsdir=/usr/sbin")
 
 def build():
-    autotools.make("-j1")
+    autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.removeDir("/dev")
-
-    # will be created by postInstall script
-    pisitools.remove("/etc/suspend.conf")
 
     shelltools.touch("%s/etc/suspend.key" % get.installDIR())
