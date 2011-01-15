6cf6614c234ed87e7356648d3e88c85a67ef5f46
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2011-01-15 20:20:47 +0000

    ready for 2011

---
+++ server/database/postgresql-odbc/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2008  TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "psqlodbc-0%s" % get.srcVERSION()
+
+def setup():
+    autotools.autoreconf("-vfi")
+    autotools.configure("--with-unixodbc")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.rename("/usr/lib/psqlodbcw.so", "psqlodbc.so")
+    pisitools.remove("/usr/lib/psqlodbcw.la")
+    pisitools.dodoc("license.txt", "readme.txt")
