785315c3ca296d593d0329141fcdd20e39eb0412
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2011-01-15 20:21:03 +0000

    ready for 2011

---
+++ server/auth/ypbind/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005,2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+#
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "ypbind-mt-"+"%s" % get.srcVERSION()
+
+def setup():
+    autotools.configure("--enable-dbus-nm")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=\"%s\"" % get.installDIR())
+
+    pisitools.insinto("/etc", "etc/yp.conf")
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "README", "THANKS", "TODO")
