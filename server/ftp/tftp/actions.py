8d656ee92c9e8045288ecf9b9f7399c29da22e01
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2011-01-15 20:21:22 +0000

    ready for 2011

---
+++ server/ftp/tftp/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005, 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "tftp-hpa-5.0"
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("INSTALLROOT=%s" % get.installDIR())
+    pisitools.dodir("/tftpboot")
+    pisitools.dodoc("README", "CHANGES")
