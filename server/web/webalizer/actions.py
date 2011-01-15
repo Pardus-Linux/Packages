ad01a24bfdaaf8c7543c7faa763f976900ec2dd4
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2011-01-15 20:19:37 +0000

    ready for 2011

---
+++ server/web/webalizer/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="webalizer-2.21-02"
+
+def setup():
+    shelltools.export("CFLAGS", "%s -D_GNU_SOURCE" % get.CFLAGS())
+    autotools.configure("--enable-bz2 \
+                         --enable-geoip \
+                         --with-dblib=/usr/lib/libdb.so")
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dobin("webalizer")
+    pisitools.dosym("/usr/bin/webalizer","/usr/bin/webazolver")
+    pisitools.dodir("/usr/share/GeoDB")
+    pisitools.doman("webalizer.1")
+    pisitools.dodoc("*README*","CHANGES","Copyright")
