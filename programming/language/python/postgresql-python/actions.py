ee07043725aa086a8f29c5d41c3124d97b33af3b
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2011-01-15 20:23:25 +0000

    ready for 2011.

---
+++ programming/language/python/postgresql-python/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-·
+#
+# Copyright 2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir ="PyGreSQL-%s" % get.srcVERSION()
+
+def install():
+     pythonmodules.install()
+     pisitools.rename("/usr/lib/%s/site-packages/_pg.so" % get.curPYTHON(), "_pgmodule.so")
+     pisitools.dodoc("docs/*")
+     pisitools.insinto("/usr/share/doc/%s" % get.srcNAME(), "tutorial")
