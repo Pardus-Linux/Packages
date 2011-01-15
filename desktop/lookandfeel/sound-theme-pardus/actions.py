2ddf1bde25077fe0613daeafdea213be415a4c51
Gökçen Eraslan <gokcen@pardus.org.tr>
2011-01-15 17:26:32 +0000

    Ready for 2011.

---
+++ desktop/lookandfeel/sound-theme-pardus/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import kde4
+
+import os
+
+WorkDir = "./"
+NoStrip = ["/"]
+
+def fixperms(d):
+    for root, dirs, files in os.walk(d):
+        for name in dirs:
+            shelltools.chmod(os.path.join(root, name), 0755)
+        for name in files:
+            shelltools.chmod(os.path.join(root, name), 0644)
+
+def setup():
+    fixperms("sounds")
+
+def install():
+    pisitools.insinto("%s/share/" % kde4.prefix, "sounds")
+
