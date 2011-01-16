e1595ca795fb34bb445e815637f7accf10e33531
Ozan Çağlayan <ozan@pardus.org.tr>
2011-01-16 12:13:49 +0000

    tn5250 is ready

---
+++ network/remoteshell/tn5250/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-static \
+                         --with-x \
+                         --with-ssl")
+
+def configure():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s TERMINFO=%s/usr/share/terminfo" % (get.installDIR(),get.installDIR()))
+
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "NEWS", "README*", "TODO", "linux/README")
