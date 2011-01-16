0d838e4121d396bf9bab4ebe62205d3ec3447b4b
Gökhan Özbulak <gozbulak@pardus.org.tr>
2011-01-16 16:28:48 +0000

    Make eclipse icon visible on start menu
    BUG:FIXED:15502

--- programming/environment/eclipse/eclipse/actions.py
+++ programming/environment/eclipse/eclipse/actions.py
@@ -13,3 +13,6 @@ WorkDir = get.ARCH()
 def install():
     pisitools.dodir("/opt")
     pisitools.insinto("/opt", "eclipse")
+
+    # Make eclipse icon visible on start menu
+    pisitools.insinto("/usr/share/pixmaps", "%s/opt/eclipse/plugins/org.eclipse.platform_3.6.1.v201009090800/eclipse48.png" % get.installDIR(), "eclipse.png")
