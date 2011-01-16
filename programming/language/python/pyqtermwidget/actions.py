a37c0f06c8f101f06d0021b1782bd72dd22d1de7
Gökmen Göksel <gokmen@pardus.org.tr>
2011-01-16 19:22:29 +0000

    - Update version  - Use same source with qtermwidget

--- programming/language/python/pyqtermwidget/actions.py
+++ programming/language/python/pyqtermwidget/actions.py
@@ -9,15 +9,16 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "qtermwidget"
-
 def setup():
+    shelltools.cd("pyqt4")
     shelltools.system("python config.py")
 
 def build():
+    shelltools.cd("pyqt4")
     autotools.make()
 
 def install():
+    shelltools.cd("pyqt4")
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("README")
