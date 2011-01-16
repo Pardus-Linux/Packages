ce90d96a87a61c79c17ba6f694cb432511dec326
Ozan Çağlayan <ozan@pardus.org.tr>
2011-01-16 11:30:25 +0000

    fet is ready

---
+++ office/misc/fet/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+def setup():
+    # Remove binary translations
+    shelltools.unlink("translations/*.qm")
+    shelltools.system("qmake %s.pro" % get.srcNAME())
+
+def build():
+    autotools.make()
+    shelltools.cd("translations")
+    shelltools.system("lrelease *.ts")
+
+def install():
+    pisitools.dobin("fet")
+    pisitools.doman("doc/fet.1")
+    pisitools.insinto("/usr/share/%s/translations" % get.srcNAME(), "translations/*.qm")
+
+    # Install examples
+    pisitools.insinto("/usr/share/%s/examples" % get.srcNAME(), "examples/*")
+
+    pisitools.dodoc("ChangeLog", "COPYING", "README", "REFERENCES")
