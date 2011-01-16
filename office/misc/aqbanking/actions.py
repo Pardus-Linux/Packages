6909ee01e1107139caecdc87f965b21b818bf575
Ozan Çağlayan <ozan@pardus.org.tr>
2011-01-16 11:32:43 +0000

    aqbanking is ready

---
+++ office/misc/aqbanking/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--enable-gwenhywfar")
+
+
+def build():
+    autotools.make("-j1")
+    autotools.make("-j1 qt4-port")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.removeDir("/usr/share/doc/aqhbci")
