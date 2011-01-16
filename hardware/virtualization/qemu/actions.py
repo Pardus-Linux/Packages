3ca138334f3227decfc5c34ef5fc9034eefbd592
Onur Küçük <onur@pardus.org.tr>
2011-01-16 17:46:35 +0000

    Merge kvm package with qemu Install sasl2 config Tidy up and enable ksm services Fix parallel build on buildfarms Add some fixes from Debian

--- hardware/virtualization/qemu/actions.py
+++ hardware/virtualization/qemu/actions.py
@@ -10,32 +10,26 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-NoStrip=["/usr/share/qemu"]
+WorkDir="qemu-kvm-%s" % get.srcVERSION()
+NoStrip = ["/usr/share/qemu"]
 
-# Disabled linux-user targets: m68k, ppc, ppc64, ppc64abi32, sh4, sh4eb
-# user_targets=["alpha","arm","armeb","cris","i386","mips","mipsel","sparc","sparc32plus","sparc64", "x86_64"]
+shelltools.export("LC_ALL", "en_US.UTF-8")
 
-# Disabled softmmu targets (in addition to above) : alpha, arm, armeb
-# soft_targets=["cris","i386","mips","mipsel","sparc","sparc32plus","sparc64", "x86_64"]
-
-# target_list=[]
-
-# for target in user_targets:
-#     target_list.append("%s-linux-user" % target)
-
-# for target in soft_targets:
-#     target_list.append("%s-softmmu" % target)
-
-
+# disable debug to prevent memory exhaustion by linker
+# cflags = get.CFLAGS().replace("-fpie", "").replace("-fstack-protector", "").replace("-ggdb3 -funwind-tables -fasynchronous-unwind-tables", "")
 cflags = get.CFLAGS().replace("-fpie", "").replace("-fstack-protector", "")
+
 #extraldflags="-Wl,--build-id"
 #buildldflags="VL_LDFLAGS=-Wl,--build-id"
 extraldflags=""
 buildldflags=""
 
-soundDrivers = "pa sdl alsa oss"
+soundDrivers = "pa,sdl,alsa,oss"
+builddirkvmtest = "kvm/test"
 
-targetlist = "i386-softmmu x86_64-softmmu arm-softmmu cris-softmmu m68k-softmmu \
+#targetKvmList = "i386-softmmu x86_64-softmmu i386-linux-user x86_64-linux-user"
+targetListKvm = "x86_64-softmmu"
+targetList = "i386-softmmu x86_64-softmmu arm-softmmu cris-softmmu m68k-softmmu \
               mips-softmmu mipsel-softmmu mips64-softmmu mips64el-softmmu ppc-softmmu \
               ppcemb-softmmu ppc64-softmmu sh4-softmmu sh4eb-softmmu sparc-softmmu \
               i386-linux-user x86_64-linux-user alpha-linux-user arm-linux-user \
@@ -45,47 +39,84 @@ targetlist = "i386-softmmu x86_64-softmmu arm-softmmu cris-softmmu m68k-softmmu 
               sparc32plus-linux-user"
 
 
+cfgParamsCommon = '--prefix=/usr \
+                  --sysconfdir=/etc \
+                  --mandir=/usr/share/man \
+                  --cc="%s" \
+                  --host-cc="%s" \
+                  --extra-cflags="%s" \
+                  --extra-ldflags="%s" \
+                  --audio-drv-list="%s" \
+                  --disable-xen \
+                  --disable-werror \
+                  --disable-strip' % (get.CC(), get.CC(), cflags, extraldflags, soundDrivers)
+
+
+def printfancy(msg):
+    print
+    print "===== %s =====" % msg
+    print
+
 def setup():
     # disable fdt until dtc is in repo
     # pisitools.dosed("configure", 'fdt="yes"', 'fdt="no"')
 
     shelltools.export("CFLAGS", cflags)
-    shelltools.export("LC_ALL", "en_US.UTF-8")
-    autotools.rawConfigure('--prefix=/usr \
-                            --mandir=/usr/share/man \
-                            --sysconfdir=/etc \
-                            --disable-werror \
-                            --disable-strip \
-                            --disable-kvm \
-                            --disable-bsd-user \
-                            --disable-darwin-user \
-                            --target-list="%s" \
-                            --audio-drv-list="%s" \
-                            --cc="%s" \
-                            --host-cc="%s" \
-                            --extra-ldflags="%s" \
-                            --extra-cflags="%s" '% (targetlist, soundDrivers, get.CC(), get.CC(), extraldflags, cflags))
+    shelltools.export("LDFLAGS", extraldflags)
 
+    # different build dir setups are not supported yet, so we build kvm by hand for now
+    printfancy("configuring kvm")
+    autotools.rawConfigure('%s \
+                            --target-list="%s" \
+                            ' % (cfgParamsCommon, targetListKvm))
+
+    printfancy("building kvm")
+    autotools.make("V=1 -j1 config-host.h %s" % buildldflags)
+    autotools.make("V=1 %s" % buildldflags)
+    shelltools.copy("x86_64-softmmu/qemu-system-x86_64", "qemu-kvm")
+    autotools.make("clean")
+
+
+    printfancy("configuring kvmtest")
+    shelltools.cd(builddirkvmtest)
+    autotools.rawConfigure("--prefix=/usr \
+                            --kerneldir=../../kernel")
+    shelltools.cd("../..")
+
+
+    printfancy("configuring qemu")
+    autotools.rawConfigure('%s \
+                            --target-list="%s" \
+                            --disable-kvm \
+                            ' % (cfgParamsCommon, targetList))
+                            #--interp-prefix=%{_prefix}/qemu-%%M \
 
                             #--enable-system \
                             #--enable-linux-user \
 
-                            # --audio-card-list="ac97 es1370 sb16 cs4231a adlib gus" \
-                            # --target-list="%s"' % " ".join(target_list))
-                            # --disable-bluez \
-                            # --kerneldir="/lib/modules/%s/build" \
 
 def build():
-    shelltools.export("LC_ALL", "en_US.UTF-8")
-    # parallel build fails on high end machines
-    autotools.make("V=1 -j1 %s" % buildldflags)
+    shelltools.export("CFLAGS", cflags)
+    shelltools.export("LDFLAGS", extraldflags)
+
+    printfancy("building qemu")
+    autotools.make("V=1 -j1 config-host.h %s" % buildldflags)
+    autotools.make("V=1 %s" % buildldflags)
+
+    printfancy("building kvmtest")
+    autotools.make("-C %s V=1 kvmtrace" % builddirkvmtest)
 
 def install():
     autotools.rawInstall('DESTDIR="%s"' % get.installDIR())
 
-    # comes with kvm
-    pisitools.remove("/usr/bin/qemu-io")
+    # Install kvm-tools
+    pisitools.dobin("kvm/test/kvmtrace")
+    pisitools.dobin("kvm/test/kvmtrace_format")
+    pisitools.dobin("kvm/kvm_stat")
+    pisitools.dobin("qemu-kvm")
 
-    for i in ["pc-bios/README", "LICENSE", "TODO", "README", "qemu-doc.html", "qemu-tech.html"]:
+    pisitools.insinto("/etc/sasl2/", "qemu.sasl", "qemu.conf")
+
+    for i in ["pc-bios/README", "LICENSE", "TODO", "README", "COPYING*", "qemu-doc.html", "qemu-tech.html"]:
         pisitools.dodoc(i)
 
