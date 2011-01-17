4b4f6fc9a7a15a502f5416287ef7f8a0dda623da
Onur Küçük <onur@pardus.org.tr>
2011-01-17 21:12:15 +0000

    liquidwar is ready

---
+++ game/strategy/liquidwar/actions.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "liquidwar6-0.0.9beta"
+#WorkDir = "liquidwar6-%ssnapshot" % get.srcVERSION().split("_", 1)[1]
+
+def setup():
+    autotools.autoreconf("-vfi")
+    autotools.configure("--disable-static \
+                         --enable-mod-ogg \
+                         --enable-mod-gl \
+                         --disable-nls \
+                         --disable-rpath")
+
+    # pisitools.dosed("Makefile", "^DOCDIR.*", "DOCDIR = /%s/%s" % (get.docDIR(), get.srcNAME()))
+
+def build():
+    # autotools.make("-j1")
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    # autotools.make("DESTDIR=%s install_nolink" % get.installDIR())
+    # pisitools.removeDir("/usr/share/applnk")
