5ba88459540c29ed9c403f9c980e5cebe7866045
Beyza Ermiş <beyza@pardus.org.tr>
2011-01-18 11:54:41 +0000

    CDK is ready for 2011.

---
+++ multimedia/graphics/cdk/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+WorkDir = "cdk-5.0-20060507"
+
+def setup():
+    autotools.configure("--with-ncurses \
+                         --with-libtool")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s DOCUMENT_DIR=%s" % ( get.installDIR(), get.docDIR() ) )
