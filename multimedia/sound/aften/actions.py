ed57266667ef0b288917fa4e63777ec3fd814346
Beyza Ermiş <beyza@pardus.org.tr>
2011-01-18 10:11:20 +0000

    Aften is ready for 2011.

---
+++ multimedia/sound/aften/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+
+    cmaketools.configure("-DSHARED=ON -DDOUBLE=ON -DBINDINGS_CXX=1", sourceDir="..")
+
+def build():
+    shelltools.cd("build")
+    cmaketools.make()
+
+def install():
+    shelltools.cd("build")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    shelltools.cd("..")
+    pisitools.dodoc("README","Changelog","COPYING")
