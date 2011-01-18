a474f5ec0b1ec4b1906c8b30a703a892dcd8d272
Fethican Coşkuner <fethicanc@gmail.com>
2011-01-18 12:21:08 +0000

    Add service script that controls monthly checkin

--- hardware/info/smolt/comar/package.py
+++ hardware/info/smolt/comar/package.py
@@ -2,7 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import os
+from random import randint
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     UUID = file("/proc/sys/kernel/random/uuid").read().strip()
     file("/etc/hw-uuid", "w").write(UUID)
+
+    minute = randint(0,59)
+    hour = randint(0,23)
+    day = randint(0,28)
+
+    template = """# Monhly checkin cron script for Smolt.
+# Cron script runs if and only if smolt service is started.
+# Existance of the script does not proof that auto checkin is enabled.
+%s %s %s * * smolt /usr/bin/smoltSendProfile -c > /dev/null 2>&1"""
+
+    file("/etc/cron.d/smolt", "w").write(template % (minute, hour, day))
