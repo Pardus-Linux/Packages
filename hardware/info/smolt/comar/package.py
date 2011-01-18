89cac7f174907c543e4e7ddc3082d25bf42f132e
Fethican Coşkuner <fethicanc@gmail.com>
2011-01-18 12:43:04 +0000

    fix typo

--- hardware/info/smolt/comar/package.py
+++ hardware/info/smolt/comar/package.py
@@ -14,7 +14,7 @@ def postInstall(fromVersion, fromRelease, toVersion, toRelease):
 
     template = """# Monhly checkin cron script for Smolt.
 # Cron script runs if and only if smolt service is started.
-# Existance of the script does not proof that auto checkin is enabled.
+# Existance of the script does not prove that auto checkin is enabled.
 %s %s %s * * smolt /usr/bin/smoltSendProfile -c > /dev/null 2>&1"""
 
     file("/etc/cron.d/smolt", "w").write(template % (minute, hour, day))
