35051c77055b95c68737bb9f5b79aa61a00cc909
Beyza Ermiş <beyza@pardus.org.tr>
2011-01-19 07:18:33 +0000

    Ready for 2011

---
+++ science/misc/libvolpack/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
+WorkDir = "volpack-%s" % get.srcVERSION().replace("_pre7","c7")
+
+def setup():
+    shelltools.export("CXXFLAGS", get.CXXFLAGS())
+    shelltools.export("CFLAGS", get.CFLAGS())
+    autotools.configure("--enable-static=no")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
+
+    #add examples
+    pisitools.dodir("%s/%s/examples" % (get.docDIR(), get.srcNAME()))
+    pisitools.insinto("%s/%s/examples" % (get.docDIR(), get.srcNAME()), "examples/.libs/*")
+    pisitools.insinto("%s/%s/examples" % (get.docDIR(), get.srcNAME()), "examples/brainsmall.den")
