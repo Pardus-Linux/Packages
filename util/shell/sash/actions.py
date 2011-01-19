5f7a72f290da5a4157812963a3c23ec30d64e9c6
Beyza Ermiş <beyza@pardus.org.tr>
2011-01-19 14:33:44 +0000

    Ready for 2011.

---
+++ util/shell/sash/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dobin("sash","/bin")
+
+    pisitools.dodoc("CHANGES", "README")
+
