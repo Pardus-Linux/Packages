e7d3f437dbac5edfcb543e92c6bb5d810e80c9d0
Gökçen Eraslan <gokcen@pardus.org.tr>
2011-01-20 09:39:20 +0000

    Add locale files of 9 languages and make Turkish translations almost complete.

--- desktop/kde/admin/plasma-applet-networkmanagement/files/getLocales.py
+++ desktop/kde/admin/plasma-applet-networkmanagement/files/getLocales.py
@@ -10,7 +10,7 @@ podirname = "po"
 
 cmaketemplate="""find_package(Gettext REQUIRED)
 file(GLOB _po_files *.po)
-GETTEXT_PROCESS_PO_FILES(tr ALL INSTALL_DESTINATION ${LOCALE_INSTALL_DIR} ${_po_files} )                                                                                                                                                      """
+GETTEXT_PROCESS_PO_FILES(%s ALL INSTALL_DESTINATION ${LOCALE_INSTALL_DIR} ${_po_files} )                                                                                                                                                      """
 
 try:
     shutil.rmtree(podirname)
@@ -19,12 +19,18 @@ except:
 
 os.mkdir(podirname)
 
+maincmake = open("%s/CMakeLists.txt" % podirname, "w")
+
 for lang in locales:
-    os.system("svn export svn://anonsvn.kde.org/home/kde/trunk/l10n-kde4/%s/messages/extragear-base/ %s/%s" % (lang, podirname, lang))
+    os.system("svn export svn+ssh://svn.kde.org/home/kde/trunk/l10n-kde4/%s/messages/extragear-base/ %s/%s" % (lang, podirname, lang))
     for f in os.listdir("%s/%s" % (podirname, lang)):
         if not f in files:
             os.remove("%s/%s/%s" % (podirname, lang, f))
 
     cmake = open("%s/%s/CMakeLists.txt" % (podirname, lang), "w")
-    cmake.write(cmaketemplate)
+    cmake.write(cmaketemplate % lang)
     cmake.close()
+
+    maincmake.write("add_subdirectory(%s)\n" % lang)
+
+maincmake.close()
