6d28a591ebcd213e08acdee67ccd464895b93068
İnanç Yıldırgan <inancyildirgan@gmail.com>
2011-01-21 15:15:51 +0000

    Version bump to bug fix release, add iPhone support

--- hardware/misc/libmtp/actions.py
+++ hardware/misc/libmtp/actions.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2009 TUBITAK/UEKAE
+# Copyright 2006-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-static")
+    autotools.configure("--disable-static \
+                         --disable-rpath")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     #install HAL file for portable audio players
     pisitools.insinto("/usr/share/hal/fdi/information/20thirdparty", "libmtp.fdi", "10-libmtp.fdi")
 
-    # Regenerate udev rules without MODE= property
-    shelltools.system('examples/hotplug -a\'SYMLINK+="libmtp-%k" ENV{ACL_MANAGE}="0" TAG+="udev-acl"\' > libmtp.rules')
-
-    #install UDEV rules
-    pisitools.insinto("/lib/udev/rules.d/", "libmtp.rules", "65-mtp.rules")
+    #rename UDEV rules
+    pisitools.rename("/lib/udev/rules.d/libmtp.rules", "65-mtp.rules")
 
     pisitools.removeDir("/usr/share/doc/libmtp-*")
 
