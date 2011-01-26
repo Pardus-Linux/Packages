f03bb02d3dcc31723c90b44e800c278bd2042c2b
Ozan Çağlayan <ozan@pardus.org.tr>
2011-01-26 09:43:41 +0000

    Copy to 2011
    BUG:FIXED:16305

---
+++ hardware/disk/multipath-tools/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+WorkDir = "."
+
+def build():
+    autotools.make("LIB=/lib")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s \
+                          LIB=/lib \
+                          bindir=/sbin \
+                          syslibdir=/lib \
+                          libmpathdir=/lib/multipath" % get.installDIR())
+
+    pisitools.dodir("/etc/multipath")
+    pisitools.removeDir("/etc/init.d")
+
+    pisitools.dodoc("AUTHOR", "COPYING", "FAQ")
