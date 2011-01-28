cd22ad7ecd2590958fb09bea3c51291271742349
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2011-01-28 10:55:31 +0000

    Move qbittorrent to 2011 repository. Thanks for reviews.
    BUG:FIXED:16518
    BUG:FIXED:16540

---
+++ network/p2p/qbittorrent/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.rawConfigure("--prefix=/usr")
+
+def build():
+    shelltools.system("lrelease src/lang/qbittorrent_tr.ts")
+    autotools.make("CXX=%s" % get.CXX())
+
+def install():
+    autotools.rawInstall("INSTALL_ROOT=%s" % get.installDIR())
+
+    pisitools.dosym("/usr/share/icons/hicolor/128x128/apps/qbittorrent.png", "/usr/share/pixmaps/qbittorrent.png")
+
+    pisitools.dodoc("AUTHORS", "Changelog", "COPYING", "NEWS", "README", "TODO")
