756e56b25fb53e338c1a9096603c20044501de6e
İşbaran Akçayır <isbaran@gmail.com>
2011-01-29 20:25:36 +0000

    add config, enable parallel build, add pc file

--- science/gis/ogdi/actions.py
+++ science/gis/ogdi/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -17,23 +16,24 @@ shelltools.export("TOPDIR", "%s/%s" % (get.workDIR(), WorkDir))
 def setup():
     autotools.configure('--with-zlib \
                          --with-projlib="-L/usr/lib -lproj" \
-                         --with-expat')
+                         --with-expat \
+                         --with-binconfigs')
 
 def build():
     shelltools.export("TARGET", "Linux")
     shelltools.export("CFG", "release")
-    shelltools.export("LD_LIBRARY_PATH", "%s/%s/bin/linux" % (get.workDIR(),get.srcDIR()))
-    autotools.make("-j1")
+    autotools.make()
 
 def install():
     pisitools.dolib_so("bin/Linux/*.so")
+    pisitools.dosym("/usr/lib/libogdi.so", "/usr/lib/libogdi.so.3")
+
     for i in ["gltpd", "ogdi_import", "ogdi_info"]:
         pisitools.dobin("bin/Linux/%s" % i)
+    pisitools.dobin("ogdi-config")
+
+    pisitools.insinto("/usr/lib/pkgconfig", "ogdi.pc")
 
     pisitools.insinto("/usr/include", "ogdi/include/*.h")
 
-    pisitools.dosym("/usr/lib/libogdi.so", "/usr/lib/libogdi.so.3")
-    # pisitools.dosym("/usr/lib/Linux/libogdi.so", "/usr/lib/libogdi.so.3")
-
     pisitools.dodoc("ChangeLog", "NEWS", "README")
-
