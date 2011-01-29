c1daeb168c04c31b03ede48e9300a5c483c448a4
Gökçen Eraslan <gokcen@pardus.org.tr>
2011-01-29 13:16:28 +0000

    Add plasma-applet-message-indicator

---
+++ desktop/kde/addon/plasma-applet-message-indicator/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kde4
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "plasma-widget-message-indicator-%s" % get.srcVERSION()
+
+def setup():
+    kde4.configure()
+
+def build():
+    kde4.make()
+
+def install():
+    kde4.install()
+
+    pisitools.dodoc("COPYING", "NEWS")
