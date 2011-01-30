5aa83376ecca44b8cb8d070ea17f16a8617ffc64
Ozan Çağlayan <ozan@pardus.org.tr>
2011-01-30 18:59:16 +0000

    Rename wbxml2 to libwbxml and bump to 0.10 series

---
+++ hardware/mobile/libwbxml/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2011 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.makedirs("build")
+    shelltools.cd("build")
+    cmaketools.configure(sourceDir="..")
+
+def build():
+    shelltools.cd("build")
+    cmaketools.make()
+
+def check():
+    shelltools.cd("build")
+    cmaketools.make("test")
+
+def install():
+    shelltools.cd("build")
+    cmaketools.install()
