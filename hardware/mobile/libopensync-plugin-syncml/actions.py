afe2928b6115f493aa68920beccc42fa311d4f62
Ozan Çağlayan <ozan@pardus.org.tr>
2011-01-30 19:32:43 +0000

    ready

---
+++ hardware/mobile/libopensync-plugin-syncml/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2011 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    pisitools.dosed("configure.ac", "libsoup-2.2", "libsoup-2.4")
+    autotools.autoreconf("-fi")
+    pisitools.dosed("src/Makefile.in", "-Werror")
+
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING")
