38f7c3caa89cc0f88057353d93af314739a5a4f1
İşbaran Akçayır <isbaran@gmail.com>
2011-01-31 15:17:48 +0000

    enable paralel build, disable full-screen startup

--- game/simulation/flightgear/actions.py
+++ game/simulation/flightgear/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -18,7 +17,7 @@ def setup():
                          --with-x")
 
 def build():
-    autotools.make("-j1")
+    autotools.make()
 
 def install():
     autotools.install()
