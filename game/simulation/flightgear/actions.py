85154831caf192a4dd1c53ccfa5e240a2417b1c2
İşbaran Akçayır <isbaran@gmail.com>
2011-01-31 21:14:45 +0000

    enable osgviewer

--- game/simulation/flightgear/actions.py
+++ game/simulation/flightgear/actions.py
@@ -5,16 +5,18 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 WorkDir = "FlightGear-%s" % get.srcVERSION()
 
 def setup():
+    shelltools.system("./autogen.sh")
     autotools.configure("--enable-sp-fdms \
                          --with-threads \
                          --with-logging \
-                         --enable-sdl \
+                         --enable-osgviewer \
                          --with-x \
                          --with-simgear=/usr")
 
