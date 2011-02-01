3c9e27f29292e6828d2f7abbcd32097a7d8db702
Fatih Arslan <farslan@pardus.org.tr>
2011-02-01 13:55:59 +0000

    * Latest svn to work properly with Google's new api * Use kde4 actionsapi * Change name of the package * Takeover of the package * Modifiy paths to work correctly with 2011 repo

---
+++ desktop/kde/addon/plasma-applet-translatoid/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import kde4
+
+#WorkDir = "translatoid-%s" % get.srcVERSION()
+
+def setup():
+    pisitools.dosed("plasma-applet-translatoid.desktop", "Name=Translator", "Name=Translatoid")
+    pisitools.dosed("plasma-applet-translatoid.desktop", "Icon=translator.png", "Icon=accessories-dictionary.png")
+
+    kde4.configure()
+
+def build():
+    kde4.make()
+
+def install():
+    kde4.install()
+    pisitools.dodoc("licence.txt", "README")
