1e66742ae97ed9d115f3938f19ffb426347a7c0c
Fatih Arslan <farslan@pardus.org.tr>
2011-02-01 12:04:31 +0000

    Ready for 2011

---
+++ desktop/font/Nafees-Web-Naskh-fonts/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "."
+
+def install():
+    pisitools.insinto("/usr/share/fonts/nafees", "NafeesWeb.ttf", "Nafees_Web_Naskh.ttf")
+    shelltools.chmod("%s/usr/share/fonts/nafees/*" % get.installDIR(), 0644)
