0190a4c58e1bd4996d0e790946ba1647ec1af485
Ozan Çağlayan <ozan@pardus.org.tr>
2011-02-02 10:03:50 +0000

    ready

---
+++ system/auth/pam_p11/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2007-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--enable-static=no")
+
+def build():
+    autotools.make("-j1")
+
+def install():
+    pisitools.doexe("src/.libs/pam_p11_opensc.so", "/lib/security")
+    pisitools.doexe("src/.libs/pam_p11_openssh.so", "/lib/security")
+
+    pisitools.dodoc("doc/ChangeLog", "COPYING", "NEWS")
