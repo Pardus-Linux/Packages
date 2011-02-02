e63fe2aa5dc30cfa52b86ae0f2444dc81da03a3f
İnanç Yıldırgan <inancyildirgan@gmail.com>
2011-02-02 12:31:58 +0000

    Version bump, depend new libimobiledevice

--- hardware/misc/libgpod/actions.py
+++ hardware/misc/libgpod/actions.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2010 TUBITAK/UEKAE
+# Copyright 2006-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import libtools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
+WorkDir = "%s" % get.srcNAME()
+
 def setup():
-    autotools.autoreconf("-fi")
+    shelltools.system("./autogen.sh")
     autotools.configure("--disable-static \
                          --disable-gtk-doc \
                          --without-hal \
@@ -24,9 +26,6 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    # remove docs
-    pisitools.removeDir("/usr/share/gtk-doc")
 
     # For temporary mounts
     pisitools.dodir("/var/run/libgpod")
