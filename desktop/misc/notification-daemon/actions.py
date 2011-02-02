2b9a2a4bed0f4590ab878ee8ea0012fcc48ec05e
Gökçen Eraslan <gokcen@pardus.org.tr>
2011-02-02 09:02:39 +0000

    Add notification daemon, rebase patches bump to latest non-gnome3 version.

---
+++ desktop/misc/notification-daemon/actions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-fi")
+
+    #workaround for: "error: po/Makefile.in.in was not created by intltoolize." error at the end of configure
+    shelltools.system("intltoolize --force --copy --automake")
+
+    autotools.configure("--disable-schemas-install \
+                         --disable-static")
+
+    shelltools.cd("notification-daemon-engine-nodoka-0.1.0")
+    autotools.autoreconf("-vfi")
+    autotools.configure()
+
+def build():
+    autotools.make()
+    autotools.make("-C notification-daemon-engine-nodoka-0.1.0")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("-C notification-daemon-engine-nodoka-0.1.0 DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
