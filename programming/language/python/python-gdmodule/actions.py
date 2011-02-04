c148923fa4577d05d778734f22e449bd1637a663
Fatih Arslan <farslan@pardus.org.tr>
2011-02-04 11:38:59 +0000

    Ready for 2011

---
+++ programming/language/python/python-gdmodule/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+
+def build():
+    shelltools.move("Setup.py","setup.py")
+
+def install():
+    pythonmodules.install()
+
+    pisitools.dohtml("*.html")
+    pisitools.dodoc("PKG-INFO", "README")
