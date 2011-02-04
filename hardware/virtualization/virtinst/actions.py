3147d77f6656011b4bd5574b2925c1f098a7a2ed
Ozan Çağlayan <ozan@pardus.org.tr>
2011-02-04 14:29:16 +0000

    add virtinst

---
+++ hardware/virtualization/virtinst/actions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-·
+#
+# Copyright 2009-2011 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pythonmodules
+
+def install():
+    pythonmodules.install()
