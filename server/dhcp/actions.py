461ff356e983f46f42aaecb069ebf4bf43745e81
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2011-02-04 14:48:14 +0000

    Fix Crash After Processing a DHCPv6 Decline Message CVE-2011-0413 (#16700).
    BUG:FIXED:16700

--- server/dhcp/actions.py
+++ server/dhcp/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "dhcp-4.2.0-P2"
+WorkDir = "dhcp-4.2.1b1"
 
 def setup():
     shelltools.export("CFLAGS", "%s -D_GNU_SOURCE -fPIC" % get.CFLAGS())
