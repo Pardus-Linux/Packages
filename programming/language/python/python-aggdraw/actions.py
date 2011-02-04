d2647e6353a41f37eac984e094eba96bdddb9d89
Fatih Arslan <farslan@pardus.org.tr>
2011-02-04 11:15:53 +0000

    Ready for 2011

---
+++ programming/language/python/python-aggdraw/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import pythonmodules
+
+WorkDir="aggdraw-1.2a3-20060212"
+
+def install():
+    pythonmodules.install()
+
+    pisitools.dodoc("CHANGES","README")
