f49f53853061b219a8e3fce85802476a5ecd2991
Mehmet Emre Atasever <memre@pardus.org.tr>
2011-02-07 07:09:05 +0000

    oh, forgotten changes. sync with 2009

--- science/electronics/gpsim/actions.py
+++ science/electronics/gpsim/actions.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2010 TUBITAK/UEKAE
+# Copyright 2006-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 def setup():
+    shelltools.sym(".", "m4")
+    autotools.autoreconf("-fvi")
     autotools.configure("--enable-shared --disable-static")
 
 def build():
