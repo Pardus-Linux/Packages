e0e445a3dabf6b4d4307687e382f5ba806bb1543
Fatih Arslan <farslan@pardus.org.tr>
2011-02-07 07:13:14 +0000

    Add fedora patches. Ready for 2011

---
+++ editor/joe/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("COPYING", "HINTS", "LIST", "NEWS", "README", "TODO")
+
+    # Default joe man includes informations for below applications
+    for app in ["jmacs", "jpico", "jstar", "rjoe"]:
+        pisitools.dosym("/usr/share/man/man1/joe.1", "/usr/share/man/man1/%s.1" % app)
+
