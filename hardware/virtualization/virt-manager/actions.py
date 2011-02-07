0a1bd15bc778c5b1543b3eead122472926cb47d9
Ozan Çağlayan <ozan@pardus.org.tr>
2011-02-07 13:26:38 +0000

    fix dep

---
+++ hardware/virtualization/virt-manager/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-·
+#
+# Copyright 2009-2011 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-rpath \
+                         --with-qemu-user=qemu")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "README*", "NEWS")
