02da9989c972697db62c455ccee0a65785d170e5
Fatih Arslan <farslan@pardus.org.tr>
2011-02-08 15:48:43 +0000

    Add patches from fedora. Change destdir patch. Ready for 2011

---
+++ office/misc/htmldoc/actions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-rpath \
+                         --disable-localpng \
+                         --disable-localjpeg \
+                         --disable-localzlib \
+                         --with-openssl-libs \
+                         --with-openssl-includes")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dohtml("doc/*.html")
+    pisitools.dohtml("doc/*.png")
+
+    pisitools.insinto("/usr/share/applications", "desktop/htmldoc.desktop")
+    pisitools.insinto("/usr/share/icons/", "desktop/htmldoc-48.png")
