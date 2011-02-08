854b00b37d307ff4d897e9927df796d0b62a4f6b
Serdar Dalgıç <serdar@pardus.org.tr>
2011-02-08 08:41:10 +0000

    meeh, forgot to add shelltools :/

--- office/misc/kchmviewer/actions.py
+++ office/misc/kchmviewer/actions.py
@@ -6,6 +6,7 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 from pisi.actionsapi import kde4
 
