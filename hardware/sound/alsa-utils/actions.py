94f4c7eee04d4cf261fa76a940fb6ad53f55ed4b
Ozan Çağlayan <ozan@pardus.org.tr>
2011-02-09 13:19:38 +0000

    Version bump

--- hardware/sound/alsa-utils/actions.py
+++ hardware/sound/alsa-utils/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,12 +10,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-if "_" in get.srcVERSION():
-    WorkDir = get.srcNAME()
-
 def setup():
-    # asound.state is now in /var/lib
-    autotools.autoreconf("-fi")
     autotools.configure("--sbindir=/sbin \
                          --disable-alsaconf")
 
