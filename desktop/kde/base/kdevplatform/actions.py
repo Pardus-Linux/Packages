7bd0d078e44bdf32c174ef44e43263fe623670d0
David Stegbauer <daaste@gmail.com>
2011-02-09 21:21:33 +0000

    kdevplatform-1.2.0 and kdevelop-4.2.0.

--- desktop/kde/base/kdevplatform/actions.py
+++ desktop/kde/base/kdevplatform/actions.py
@@ -22,6 +22,6 @@ def install():
     kde4.install()
     pisitools.dodoc("ChangeLog", "COPYING.*", "HOWTO.*", "NEWS", "README", "TODO")
     #locales fi, sl and th are very incomplete
-    pisitools.removeDir(destinationDirectory="/usr/share/locale/fi")
+    #pisitools.removeDir(destinationDirectory="/usr/share/locale/fi")
     pisitools.removeDir(destinationDirectory="/usr/share/locale/sl")
     pisitools.removeDir(destinationDirectory="/usr/share/locale/th")
