29cb5ffa5c7d617b794c9e9cf6f3dc6a4bc19b6b
Gökhan Özbulak <gozbulak@pardus.org.tr>
2011-02-11 14:09:24 +0000

    Ready for 2011
    BUG:COMMENT:16308

---
+++ programming/language/php/cakephp/actions.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+import os
+
+WorkDir = "cakephp-cakephp-f6748d4"
+BASEDIR = "/usr/share/php5/cakephp"
+
+def install():
+    pisitools.insinto(BASEDIR, "*")
+    pisitools.insinto(BASEDIR, ".htaccess")
+
+    # Remove redundant 'empty' files shipped with archive
+    for root, dirs, files in os.walk("%s%s" %(get.installDIR(), BASEDIR)):
+        for name in files:
+            if name == "empty":
+                shelltools.unlink(os.path.join(root, name))
+
+    pisitools.remove("%s/cake/console/cake.bat" % BASEDIR)
+
+    pisitools.dosym("%s/cake/console/cake" % BASEDIR, "/usr/bin/cake")
+    pisitools.dosym("%s/app/tmp/logs" % BASEDIR, "/var/log/cakephp")
+
+    pisitools.dodoc("README", "cake/LICENSE.txt", "cake/VERSION.txt")
+
+    # Remove redundant doc files
+    pisitools.remove("%s/README" % BASEDIR)
+    pisitools.remove("%s/cake/LICENSE.txt" % BASEDIR)
+    pisitools.remove("%s/cake/VERSION.txt" % BASEDIR)
