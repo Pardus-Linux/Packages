2c9cdaea9f9bdd416864bb87cc90e4dbd9067601
Fatih Arslan <farslan@pardus.org.tr>
2011-02-11 08:28:15 +0000

    Again and again sandbox violations

--- desktop/misc/rsibreak/actions.py
+++ desktop/misc/rsibreak/actions.py
@@ -7,6 +7,10 @@
 
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import kde4
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+shelltools.export("HOME", get.workDIR())
 
 def setup():
     kde4.configure()
