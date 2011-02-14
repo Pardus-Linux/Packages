fafa022af0ad37aeb573ca36f3b9a7f17a67f7f5
Fatih Arslan <farslan@pardus.org.tr>
2011-02-14 06:14:25 +0000

    Ready for 2011, thanks for review
    BUG:FIXED:16682

---
+++ editor/vi/vim-plugin-pisi/actions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2011 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import pisitools
+
+WorkDir = "."
+
+def install():
+    for folder in ["plugin"]:
+        pisitools.insinto("/usr/share/vim/vimfiles/", folder)
