5f9c520821eee32464c78adb2f501c5afea32bc3
Ozan Çağlayan <ozan@pardus.org.tr>
2011-02-14 08:50:21 +0000

    Sync patches from Fedora

--- util/admin/logrotate/actions.py
+++ util/admin/logrotate/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,13 +10,12 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def build():
-    autotools.make("RPM_OPT_FLAGS=\"%s\"" % get.CFLAGS())
+    autotools.make("RPM_OPT_FLAGS=\"%s\" WITH_ACL=yes" % get.CFLAGS())
 
 def install():
     autotools.rawInstall("PREFIX=%s MANDIR=%s" % (get.installDIR(), get.manDIR()))
 
     pisitools.dodir("/etc/logrotate.d")
-    pisitools.dodir("/var/lib")
 
     pisitools.insinto("/etc/cron.daily", "examples/logrotate.cron")
     pisitools.insinto("/etc", "examples/logrotate-default", "logrotate.conf")
