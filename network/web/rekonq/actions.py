16ef650b4c930dbb0ede73d8f31a335189e32004
Fatih Arslan <farslan@pardus.org.tr>
2011-02-15 21:02:59 +0000

    New stable release

--- network/web/rekonq/actions.py
+++ network/web/rekonq/actions.py
@@ -4,7 +4,6 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
@@ -14,15 +13,12 @@ shelltools.export("HOME", get.workDIR())
 
 def setup():
     kde4.configure()
-    #cmaketools.configure(sourceDir=".")
 
 def build():
     kde4.make()
-    #cmaketools.make()
 
 def install():
     kde4.install()
-    #cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
     pisitools.dosym("/usr/share/icons/hicolor/128x128/apps/reqonk.png", "/usr/share/pixmaps/reqonk.png")
 
     pisitools.dodoc("COPYING", "AUTHORS", "TODO", "ChangeLog")
