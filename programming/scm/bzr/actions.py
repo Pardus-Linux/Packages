0f6a4d5e2d32273bf1a89c5a574759370f294eab
Serdar Dalgıç <serdar@pardus.org.tr>
2011-02-15 12:35:06 +0000

    Version bump, package takeover and split docs package.

--- programming/scm/bzr/actions.py
+++ programming/scm/bzr/actions.py
@@ -4,22 +4,20 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 from pisi.actionsapi import pythonmodules
 
 def build():
     pythonmodules.compile()
+    autotools.make("docs")
 
 def install():
     pythonmodules.install()
 
     pisitools.insinto("/etc/bash_completion.d/", "contrib/bash/bzr")
-    dirs = ["/etc/apport", "/usr/share/apport",
-            "/usr/lib/%s/site-packages/bzrlib/util/elementtree" % get.curPYTHON()]
-    for dir in dirs:
-        pisitools.removeDir(dir)
+    pisitools.insinto("%s/%s" % (get.docDIR(), get.srcNAME()), "doc/*")
+    pisitools.removeDir("/usr/lib/%s/site-packages/bzrlib/util/elementtree" % get.curPYTHON())
 
     pisitools.dodoc("COPYING.txt", "README", "NEWS", "TODO")
-
-    pisitools.insinto("%s/%s" % (get.docDIR(), get.srcNAME()), "doc/*")
