7856a20ae94944720673821010573f40eefaac22
Mehmet Nur Olcay <mnurolcay@gmail.com>
2011-02-15 00:20:21 +0000

    Ready for 2011 repositories, thanks for reviews BUG:FIXED:16740

---
+++ util/misc/keepassx/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import qt4
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+
+def setup():
+    qt4.configure()
+
+def build():
+    qt4.make()
+
+def install():
+    qt4.install()
+
+    # Update Turkish translation
+    shelltools.system("lrelease src/translations/keepassx-tr_TR.ts")
+    pisitools.insinto("/usr/share/keepassx/i18n/", "src/translations/*tr*.qm")
+
+    pisitools.dodoc("changelog", "COPYING")
