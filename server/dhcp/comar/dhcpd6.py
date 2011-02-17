86fec2bcdc9a6ed7b8d1d0f7d67fa1f6a3e21496
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2011-02-17 12:45:06 +0000

    Make NM dependency optional.

--- server/dhcp/comar/dhcpd6.py
+++ server/dhcp/comar/dhcpd6.py
@@ -12,14 +12,18 @@ MSG_NM_NOT_RUNNING = _({"en": "NetworkManager service is not running",
                        })
 
 PIDFILE = "/var/run/dhcpd6.pid"
-TIMEOUT = config.get("TIMEOUT", 5)
+TIMEOUT = config.get("TIMEOUT", 10)
 
 @synchronized
 def start():
-    startDependencies("NetworkManager")
-    if run("/usr/bin/nm-online -q -t %s" % TIMEOUT) != 0:
-        # NM is not running
-        fail(MSG_NM_NOT_RUNNING)
+    try:
+        startDependencies("NetworkManager")
+        # Prevent race condition between NM and dhcpd
+        if run("/usr/bin/nm-online -q -t %s" % TIMEOUT) != 0:
+            # NM is not running
+            fail(MSG_NM_NOT_RUNNING)
+    except:
+        pass
 
     startService(command="/usr/sbin/dhcpd",
                  args="-6 -cf %s %s %s" % (config.get("DHCPD_CONF", "/etc/dhcp/dhcpd.conf"), config.get("DHCPD_ARGS", ""), config.get("INTERFACES", "")),
