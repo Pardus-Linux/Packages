a8d52732f1cf87c1f7cd7c9786815451039dcd28
Fatih Arslan <farslan@pardus.org.tr>
2011-02-28 12:42:38 +0000

    Add lib32 pacakge for glib2

--- system/base/glib2/actions.py
+++ system/base/glib2/actions.py
@@ -1,34 +1,45 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import libtools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
-WorkDir = "glib-%s" % get.srcVERSION()
 
 def setup():
-    autotools.autoconf()
-    autotools.configure("--disable-gtk-doc \
-                         --with-pcre=system \
-                         --disable-fam \
-                         --disable-static")
+    options = "--disable-gtk-doc \
+               --with-pcre=system \
+               --disable-fam \
+               --disable-static"
+
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+        shelltools.export("PKG_CONFIG_LIBDIR", "/usr/lib32/pkgconfig")
+
+    autotools.autoreconf("-vif")
+    autotools.configure(options)
 
     pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared ")
 
 def build():
-    autotools.make()
-
-def check():
-    autotools.make("LC_ALL=C check")
+    autotools.make("-j1")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    if get.buildTYPE() == "emul32":
+        pisitools.domove("/emul32/bin/gio-querymodules", "/usr/bin/32/")
+        pisitools.removeDir("/emul32")
+
     pisitools.removeDir("/usr/share/gtk-doc")
+    pisitools.removeDir("/usr/share/gdb")
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "README", "NEWS")
