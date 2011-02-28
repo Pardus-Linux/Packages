ccdbe72d3df3173850f20c0bedfbe43e2b8fa3dd
Fatih Arslan <farslan@pardus.org.tr>
2011-02-28 09:50:31 +0000

    Add lib32 package for libXrandr

--- x11/library/libXrandr/actions.py
+++ x11/library/libXrandr/actions.py
@@ -5,14 +5,22 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
-    autotools.configure("--disable-static")
+    options = "--disable-static"
+
+    if get.buildTYPE() == "emul32":
+        options += " --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
