965312b127a4744e9539786fc6a9b0e71cb0584a
Fatih Arslan <farslan@pardus.org.tr>
2011-02-28 12:46:39 +0000

    Add lib32 package for libcap

--- system/base/libcap/actions.py
+++ system/base/libcap/actions.py
@@ -8,15 +8,27 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
-    pisitools.dosed("Make.Rules", "^CFLAGS := .*$", "CFLAGS := %s" % get.CFLAGS())
-    pisitools.dosed("Make.Rules", "^LDFLAGS := .*$", "LDFLAGS := %s" % get.LDFLAGS())
+    if get.buildTYPE() == "emul32":
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    pisitools.dosed("Make.Rules", "pardusCFLAGS", get.CFLAGS())
+    pisitools.dosed("Make.Rules", "pardusLDFLAGS", get.LDFLAGS())
 
 def build():
-    autotools.make('CC="%s"' % get.CC())
+    args = "emul32=1" if get.buildTYPE() == "emul32" else ""
+    autotools.make('CC="%s" %s' % (get.CC(), args))
 
 def install():
+    if get.buildTYPE() == "emul32":
+        autotools.rawInstall("FAKEROOT=%s \
+                              LIBDIR=%s/usr/lib32 \
+                              emul32=1" % ((get.installDIR(),)*2))
+        pisitools.remove("/usr/lib32/*.a")
+        return
+
     autotools.rawInstall("FAKEROOT=%s" % get.installDIR())
 
     pisitools.insinto("/etc/security", "pam_cap/capability.conf")
