85e378d6346ac794820624fc8253473a236f5e0c
Fatih Arslan <farslan@pardus.org.tr>
2011-03-01 08:58:43 +0000

    Add lib32 package for libv4l

--- multimedia/video/libv4l/actions.py
+++ multimedia/video/libv4l/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2008-2009 TUBITAK/UEKAE
+# Copyright 2008-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
@@ -10,17 +10,21 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "v4l-utils-%s" % get.srcVERSION()
+# Raw building
+args = "PREFIX=/usr \
+        CFLAGS='%s'" % get.CFLAGS().replace("-O2", "-O3")
+
+if get.buildTYPE() == "emul32":
+    args += " -C lib LIBDIR=/usr/lib32"
+    shelltools.export("CC", "%s -m32" % get.CC())
+    shelltools.export("CXX", "%s -m32" % get.CXX())
+else:
+    args += " LIBDIR=/usr/lib"
 
 def build():
-    autotools.make(' \
-                    PREFIX=/usr \
-                    LIBDIR=/usr/lib \
-                    CFLAGS="%s"' % get.CFLAGS().replace("-O2", "-O3"))
+    autotools.make(args)
 
 def install():
-    autotools.rawInstall("PREFIX=/usr \
-                          LIBDIR=/usr/lib \
-                          DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("%s DESTDIR=%s" % (args, get.installDIR()))
 
     pisitools.dodoc("ChangeLog", "COPYING*", "README*", "TODO")
