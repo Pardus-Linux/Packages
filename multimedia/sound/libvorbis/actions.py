fc1f3b2e94475c5fd079f2cf1d6276fec3733dda
Fatih Arslan <farslan@pardus.org.tr>
2011-03-01 08:59:11 +0000

    Add lib32 package for libvorbis

--- multimedia/sound/libvorbis/actions.py
+++ multimedia/sound/libvorbis/actions.py
@@ -8,6 +8,7 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
     # sanitize flags
@@ -16,7 +17,13 @@ def setup():
     pisitools.dosed("configure", "-mcpu=750")
     pisitools.dosed("configure", "-O20")
 
-    autotools.configure("--disable-static")
+    options = "--disable-static"
+
+    if get.buildTYPE() == "emul32":
+        options += " --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
