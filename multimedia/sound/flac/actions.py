b69dc1cc9f43f364b6b0df092e87feb5b2000382
Fatih Arslan <farslan@pardus.org.tr>
2011-03-01 12:08:37 +0000

    Prevent override of 64-bit executables

--- multimedia/sound/flac/actions.py
+++ multimedia/sound/flac/actions.py
@@ -20,7 +20,8 @@ def setup():
                 --disable-static"
 
     if get.buildTYPE() == "emul32":
-        options += " --libdir=/usr/lib32"
+        options += " --prefix=/emul32 \
+                     --libdir=/usr/lib32"
         shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
         shelltools.export("CXXFLAGS", "%s -m32" % get.CXXFLAGS())
 
@@ -35,6 +36,9 @@ def check():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/emul32")
 
     pisitools.removeDir("/usr/share/doc/")
 
