beda6ff39d91e278f9165514c6554de52c5eee6f
Metin Akdere <metin@pardus.org.tr>
2011-03-02 13:29:19 +0000

    * Fix file paths for -devel subpackage * Disable gtk-doc * Fix docdir

--- programming/misc/raptor/actions.py
+++ programming/misc/raptor/actions.py
@@ -9,12 +9,16 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+docdir = "/%s/%s" % (get.docDIR(), get.srcNAME())
+
 def setup():
     autotools.autoreconf("-fi")
 
     autotools.configure("--with-xml-parser=libxml\
                          --with-www=curl \
-                         --disable-static")
+                         --disable-gtk-doc \
+                         --with-html-dir=%s/html\
+                         --disable-static" % docdir)
 
 def build():
     autotools.make()
@@ -22,4 +26,7 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
+    pisitools.insinto("%s/html" % docdir, "%s/%s/html/raptor/*" % (get.installDIR(), docdir))
+    pisitools.removeDir("%s/html/raptor" % docdir)
+    pisitools.dohtml("*.html")
     pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README")
