152fbaef97c10802bb1bea70ff3210221f82f936
Fatih Arslan <farslan@pardus.org.tr>
2011-03-03 12:53:15 +0000

    Add lib32 package for talloc

--- programming/misc/libtalloc/actions.py
+++ programming/misc/libtalloc/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 TUBITAK/UEKAE
+# Copyright 2010-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -11,10 +11,18 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 WorkDir = "talloc-%s" % get.srcVERSION()
+libdir = "lib32" if get.buildTYPE() == "emul32" else "lib"
 
 def setup():
     shelltools.system("./autogen.sh")
-    autotools.configure("--enable-talloc-compat1")
+
+    options = "--enable-talloc-compat1"
+
+    if get.buildTYPE() == "emul32":
+        options += " --libdir=/usr/lib32"
+        shelltools.export("CC", "%s -m32" % get.CC())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
@@ -22,13 +30,13 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.remove("/usr/lib/*.a")
-    pisitools.remove("/usr/lib/libtalloc-compat1-%s.so" % get.srcVERSION())
+    pisitools.removeDir("/usr/share/swig")
+
+    pisitools.remove("/usr/lib*/*.a")
+    pisitools.remove("/usr/lib*/libtalloc-compat1-%s.so" % get.srcVERSION())
 
     # Create symlinks for so file
-    pisitools.dosym("libtalloc.so.%s" % get.srcVERSION(), "/usr/lib/libtalloc.so.%s" % get.srcVERSION().split(".")[0])
-    pisitools.dosym("libtalloc.so.%s" % get.srcVERSION(), "/usr/lib/libtalloc.so")
-
-    pisitools.removeDir("/usr/share/swig")
+    pisitools.dosym("libtalloc.so.%s" % get.srcVERSION(), "/usr/%s/libtalloc.so.%s" % (libdir, get.srcVERSION().split(".")[0]))
+    pisitools.dosym("libtalloc.so.%s" % get.srcVERSION(), "/usr/%s/libtalloc.so" % libdir)
 
     pisitools.dodoc("NEWS")
