68b5805a43709c8ed0a65e73b296d38570714831
Fatih Arslan <farslan@pardus.org.tr>
2011-03-03 08:00:06 +0000

    Add lib32 package for libXdamage

--- x11/library/libXdamage/actions.py
+++ x11/library/libXdamage/actions.py
@@ -5,13 +5,26 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
+    options = "--disable-static \
+               --disable-specs \
+               --without-xmlto"
+
+    if get.buildTYPE() == "emul32":
+        options += " --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
     autotools.autoreconf("-vif")
-    autotools.configure("--disable-static")
+    autotools.configure(options)
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
