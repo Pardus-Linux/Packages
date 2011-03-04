f0924a23da9c32bd89aecdfd61479ef51734c96e
Fatih Arslan <farslan@pardus.org.tr>
2011-03-04 07:39:46 +0000

    Add lib32 packages for libdrm

--- x11/library/libdrm/actions.py
+++ x11/library/libdrm/actions.py
@@ -4,15 +4,25 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 def setup():
+
+    options = "--enable-udev \
+               --enable-vmwgfx-experimental-api \
+               --enable-nouveau-experimental-api"
+
+
+    if get.buildTYPE() == "emul32":
+        options += " --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
     autotools.autoreconf("-vif")
-    autotools.configure("--enable-udev \
-                         --enable-vmwgfx-experimental-api \
-                         --enable-nouveau-experimental-api")
+    autotools.configure(options)
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
