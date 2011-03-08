e74fd6522c8210683904bd985a867c97fa4ca9aa
Ozan Çağlayan <ozan@pardus.org.tr>
2011-03-08 11:30:11 +0000

    - Switch to latest trunk, - Add udev rules to correctly tag FW devices for ACL,

--- hardware/firewire/libffado/actions.py
+++ hardware/firewire/libffado/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 TUBITAK/UEKAE
+# Copyright 2010-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,14 +9,15 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import scons
 from pisi.actionsapi import get
 
-WorkDir = "%s" % get.srcDIR().replace("_", "-")
+WorkDir = "libffado-2.1.0"
 
 def build():
     pisitools.dosed("SConstruct", "usr/local", "usr")
-    scons.make()
+
+    scons.make("BUILD_TESTS=0")
 
 def install():
-    scons.install("install DESTDIR=%s" % get.installDIR())
+    scons.install("install WILL_DEAL_WITH_XDG_MYSELF=1 DESTDIR=%s" % get.installDIR())
 
     pisitools.dodir("/usr/share/applications")
     pisitools.dosym("/usr/share/libffado/icons/hi64-apps-ffado.png", "/usr/share/pixmaps/ffado-mixer.png")
