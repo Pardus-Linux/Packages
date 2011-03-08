09e328793820ed3aa144f953a524bed4ae9c08fc
Fatih Arslan <farslan@pardus.org.tr>
2011-03-08 07:13:52 +0000

    * New stable release * This release includes several bug fixes, as well as the usual set of recent   build configuration improvements and janitorial cleanups.  The minor version   was bumped to indicate addition of XtAsprintf to exported API. * Add lib32 package for libXt for future use

--- x11/library/libXt/actions.py
+++ x11/library/libXt/actions.py
@@ -1,16 +1,21 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
-    autotools.configure("--disable-static")
+    options = "--disable-static"
+
+    if get.buildTYPE() == "emul32":
+        options += " --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
@@ -18,3 +23,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
+    pisitools.dodoc("ChangeLog", "COPYING", "README")
