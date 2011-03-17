1d36c500359d582e5b96705800c383c83b476829
Metin Akdere <metin@pardus.org.tr>
2011-03-17 09:36:50 +0000

    Add istanbul to 2011

---
+++ multimedia/video/istanbul/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2011 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+import os
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    os.environ["GST_REGISTRY"] = get.workDIR() + "/registry.cache.xml"
+    autotools.autoreconf("-vfi")
+    autotools.configure("--disable-gconftool \
+                         --disable-schemas-install")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" %get.installDIR())
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
