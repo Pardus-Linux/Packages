89a9b6f6b8641e0f1fc389629bacda0c94400dea
Fatih Arslan <farslan@pardus.org.tr>
2011-03-18 09:08:26 +0000

    * Bump to 0.62.0 * Change configure option to work in sync with texlive-bin * Remove lots of useless line. * We have to think about merge it into texlive-bing latter

--- tex/tool/luatex/actions.py
+++ tex/tool/luatex/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,101 +9,53 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import libtools
-from pisi.actionsapi import cmaketools
-
-WorkDir = "luatex-beta-%s" % get.srcVERSION()
-GetWorkdir = "%s/%s" % (get.workDIR(), WorkDir)
-
 
 def setup():
-    libtools.libtoolize()
-    shelltools.export("LC_ALL","C")
-    shelltools.cd("%s/source/texk/web2c" % GetWorkdir)
+    shelltools.cd("source")
 
-    autotools.configure("--disable-cxx-runtime-hack \
-                        --disable-afm2pl    \
-                        --disable-aleph		\
-                        --disable-bibtex	\
-                        --disable-bibtex8	\
-                        --disable-cfftot1	\
-                        --disable-cjkutils	\
-                        --disable-detex		\
-                        --disable-devnag	\
-                        --disable-dialog	\
-                        --disable-dtl		\
-                        --enable-dump-share	\
-                        --disable-dvi2tty	\
-                        --disable-dvidvi	\
-                        --without-dviljk    \
-                        --disable-dvipdfm	\
-                        --disable-dvipdfmx	\
-                        --disable-dvipos	\
-                        --disable-dvipsk	\
-                        --disable-gsftopk	\
-                        --disable-ipc		\
-                        --disable-lacheck	\
-                        --disable-lcdf-typetools \
-                        --disable-makeindexk \
-                        --disable-mf		\
-                        --disable-mmafm		\
-                        --disable-mmpfb		\
-                        --disable-mp		\
-                        --disable-musixflx	\
-                        --disable-otfinfo	\
-                        --disable-otftotfm	\
-                        --disable-pdfopen	\
-                        --disable-pdftex	\
-                        --disable-ps2eps	\
-                        --disable-ps2pkm	\
-                        --disable-psutils	\
-                        --disable-ptex		\
-                        --disable-seetexk	\
-                        --disable-t1dotlessj  \
-                        --disable-t1lint	\
-                        --disable-t1rawafm	\
-                        --disable-t1reencode	\
-                        --disable-t1testpage \
-                        --disable-t1utils	\
-                        --disable-tex		\
-                        --disable-tex4htk	\
-                        --disable-tpic2pdftex	\
-                        --disable-ttf2pk	\
-                        --disable-ttfdump	\
-                        --disable-ttftotype42	\
-                        --disable-vlna		\
-                        --disable-web-progs \
-                        --disable-xdv2pdf	\
-                        --disable-xdvipdfmx \
-                        --disable-xetex		\
-                        --without-x			\
-                        --with-system-kpathsea	\
-                        --with-system-gd	\
-                        --with-system-libpng	\
-                        --with-system-teckit \
-                        --with-system-zlib \
-                        --with-system-t1lib \
-                        --with-system-xpdf \
-                        --with-system-zziplib \
-                        --disable-largefile \
-                        --disable-multiplatform \
-                        --disable-shared")
+    autotools.configure(' --disable-native-texlive-build \
+                          --with-banner-add="/Pardus" \
+                          --enable-cxx-runtime-hack \
+                          --disable-all-pkgs \
+                          --disable-dump-share \
+                          --disable-ptex \
+                          --enable-luatex  \
+                          --without-system-ptexenc \
+                          --with-system-graphite \
+                          --without-system-icu \
+                          --without-system-kpathsea \
+                          --with-system-freetype2 \
+                          --with-system-poppler \
+                          --with-freetype2-libdir=/usr/lib \
+                          --with-freetype2-include=/usr/include/freetype2 \
+                          --with-system-gd \
+                          --with-system-libpng \
+                          --without-system-teckit \
+                          --with-system-zlib \
+                          --with-system-t1lib \
+                          --disable-shared \
+                          --disable-largefile \
+                          --disable-ipc \
+                          --without-mf-x-toolkit \
+                          --without-x')
 
-    for i in ["libs/obsdcompat", "texk/kpathsea"]:
-        shelltools.cd("%s/source/%s" % (GetWorkdir, i))
-        autotools.configure()
 
 def build():
-    for i in ["libs/obsdcompat", "texk/kpathsea"]:
-        shelltools.cd("%s/source/%s" % (GetWorkdir, i))
-        autotools.make()
-
-    shelltools.cd("%s/source/texk/web2c" % GetWorkdir)
+    shelltools.cd("source")
     autotools.make()
+    autotools.make("-C libs/zziplib")
+    autotools.make("-C libs/obsdcompat")
+    autotools.make("-C texk/kpathsea")
+    autotools.make("-C texk/web2c luatex")
 
 def install():
-    shelltools.cd("%s/source/texk/web2c" % GetWorkdir)
+    shelltools.cd("source")
+    autotools.install()
 
-    autotools.rawInstall("DESTDIR=%s bin_PROGRAMS='luatex' SUBDIRS='' nodist_man_MANS=''" % get.installDIR()) 
+    ## install luatex binary
+    pisitools.dobin("texk/web2c/luatex")
 
-    pisitools.dodoc("%s/README" % GetWorkdir, "%s/manual/*.pdf" % GetWorkdir)
+    ## install luatex reference file
+    shelltools.cd("..")
+    pisitools.dodoc("manual/luatexref-t.pdf")
+
