dc44226d57e767a519cfc445b70e78c83da8af46
Ozan Çağlayan <ozan@pardus.org.tr>
2011-03-18 12:38:22 +0000

    bump to 1.7.0 which switches to udev instead of hal, yay.

--- hardware/smartcard/pcsc-lite/actions.py
+++ hardware/smartcard/pcsc-lite/actions.py
@@ -5,23 +5,41 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-def setup():
-    autotools.autoreconf("-fi")
+USBDROPDIR   = "/usr/lib%s/pcsc/drivers" % ("32" if \
+        get.buildTYPE() == "emul32" else "")
 
-    autotools.configure("--enable-usbdropdir=/usr/lib/pcsc/drivers \
-                         --enable-libhal \
-                         --disable-dependency-tracking \
-                         --disable-static")
+def setup():
+    print USBDROPDIR
+    options = "--enable-usbdropdir=%s \
+               --disable-dependency-tracking \
+               --disable-static" % USBDROPDIR
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    autotools.autoreconf("-fi")
+    autotools.configure(options)
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodir(USBDROPDIR)
+
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/emul32")
+        return
+
+    pisitools.dodir("/etc/reader.conf.d")
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "DRIVERS", "HELP", "NEWS",
                     "README", "SECURITY", "doc/README.DAEMON")
