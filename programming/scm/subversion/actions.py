3413843242992ea3b1d1b73b0e70b092e2bdcdcb
Ozan Çağlayan <ozan@pardus.org.tr>
2011-03-21 08:38:38 +0000

    - Version bump to the latest maintenance release,  - Drop unneeded postinstall Apache hacks,  - avoid a crash in mod_dav_svn when using locks (CVE-2011-0715) (bzp#17235).
    BUG:FIXED:17235

--- programming/scm/subversion/actions.py
+++ programming/scm/subversion/actions.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import get
-from pisi.actionsapi import libtools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
--- programming/scm/subversion/comar/package.py
+++ programming/scm/subversion/comar/package.py
@@ -4,38 +4,8 @@ import os
 import re
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    module_enable('SVN')
-    module_enable('SVN_AUTHZ')
     os.system("/bin/mkdir -p /var/svn/repos/default")
     os.system("/usr/bin/svnadmin create /var/svn/repos/default")
     os.system("/bin/chown -Rf svn:svn /var/svn")
     os.system("/bin/chmod -Rf 774 /var/svn/repos")
     os.system("/bin/chmod 775 /var/svn/repos")
-
-def preRemove():
-    module_disable('SVN')
-    module_disable('SVN_AUTHZ')
-
-def module_enable(mod):
-    s = open('/etc/conf.d/apache2').read()
-    modules = [i.strip() for i in re.findall('APACHE2_OPTS="(.*)"',  s)[0].split('-D') if i.strip()]
-
-    if mod not in modules:
-        s2 = re.sub('APACHE2_OPTS="(.*)"', 'APACHE2_OPTS="\\1 -D %s"' % mod, s)
-        open('/etc/conf.d/apache2', 'w').write(s2)
-        return True
-
-    return False
-
-def module_disable(mod):
-    s = open('/etc/conf.d/apache2').read()
-    modules = [i.strip() for i in re.findall('APACHE2_OPTS="(.*)"',  s)[0].split('-D') if i.strip()]
-
-    if mod in modules:
-        modules.remove(mod)
-        mods = ' '.join(['-D %s' % i for i in modules])
-        s2 = re.sub('APACHE2_OPTS="(.*)"', 'APACHE2_OPTS="%s"' % mods, s)
-        open('/etc/conf.d/apache2', 'w').write(s2)
-        return True
-
-    return False
