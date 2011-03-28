4e491cf55a133861e47a082e67dea846ed7193e7
Ozan Çağlayan <ozan@pardus.org.tr>
2011-03-28 07:47:03 +0000

    Prune gtk2 deps from poppler-utils

--- office/postscript/poppler/actions.py
+++ office/postscript/poppler/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -15,6 +15,7 @@ def setup():
                          --disable-poppler-qt \
                          --disable-gtk-doc-html \
                          --disable-zlib \
+                         --disable-gtk-test \
                          --enable-poppler-qt4 \
                          --enable-cairo-output \
                          --enable-xpdf-headers \
