9ac586e994b9eec4240b4cc05650202a28e05324
Gökhan Özbulak <gozbulak@pardus.org.tr>
2011-03-30 17:43:48 +0000

    Fix access issue to tmp dir for caching and logging
    BUG:COMMENT:17538

--- programming/language/php/cakephp/actions.py
+++ programming/language/php/cakephp/actions.py
@@ -17,16 +17,22 @@ def install():
     pisitools.insinto(BASEDIR, "*")
     pisitools.insinto(BASEDIR, ".htaccess")
 
-    # Remove redundant 'empty' files shipped with archive
+    # Remove redundant 'empty' and bat files shipped with archive
     for root, dirs, files in os.walk("%s%s" %(get.installDIR(), BASEDIR)):
         for name in files:
             if name == "empty":
                 shelltools.unlink(os.path.join(root, name))
-
     pisitools.remove("%s/cake/console/cake.bat" % BASEDIR)
 
     pisitools.dosym("%s/cake/console/cake" % BASEDIR, "/usr/bin/cake")
-    pisitools.dosym("%s/app/tmp/logs" % BASEDIR, "/var/log/cakephp")
+
+    # Move the tmp dir into /var/tmp
+    pisitools.domove("%s/app/tmp/*" % BASEDIR, "/var/tmp/cakephp")
+    pisitools.removeDir("%s/app/tmp" % BASEDIR)
+    pisitools.dosym("/var/tmp/cakephp", "%s/app/tmp" % BASEDIR)
+
+    # To make access to log files easier
+    pisitools.dosym("/var/tmp/cakephp/logs", "/var/log/cakephp")
 
     pisitools.dodoc("README", "cake/LICENSE.txt", "cake/VERSION.txt")
 
