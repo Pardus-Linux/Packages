da477f214dffcc3bd6e73112c16703c4b74d399d
Gökçen Eraslan <gokcen@pardus.org.tr>
2011-03-30 11:57:11 +0000

    Version bump to 3.12.9.1, this update marks several fraudulent HTTPS certificates as unstrusted.

--- util/crypt/nss/actions.py
+++ util/crypt/nss/actions.py
@@ -10,7 +10,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir="%s-%s/mozilla" % (get.srcNAME(), get.srcVERSION())
+#WorkDir="%s-%s/mozilla" % (get.srcNAME(), get.srcVERSION())
+WorkDir="%s-3.12.9/mozilla" % get.srcNAME()
 
 def setup():
     # Create nss.pc and nss-config dynamically
