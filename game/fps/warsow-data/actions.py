b12e2ebb5b4d6b5bb7ea8e9a56617798288306e6
Onur Küçük <onur@pardus.org.tr>
2011-03-30 12:33:45 +0000

    version bump for new warsow

--- game/fps/warsow-data/actions.py
+++ game/fps/warsow-data/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2008 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,7 +8,9 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "./"
+# workdir changes all the time
+#WorkDir = "./"
+WorkDir = "warsow_0.61_unified"
 NoStrip = "/"
 
 import os
