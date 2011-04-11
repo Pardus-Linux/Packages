2378571a51baa9ec927b0d795625d9a51ca5cd90
Beyza Ermiş <beyza@pardus.org.tr>
2011-04-11 12:44:24 +0000

    Version bump BUG:FIXED:17727

--- multimedia/misc/FreeImage/actions.py
+++ multimedia/misc/FreeImage/actions.py
@@ -6,9 +6,11 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 WorkDir = get.srcNAME()
+shelltools.export("HOME", get.workDIR())
 
 def build():
     autotools.make()
