7dcf92e27449c2207f049b48549507e61ce1a11f
Ozan Çağlayan <ozan@pardus.org.tr>
2011-04-14 13:56:44 +0000

    Version bump

--- hardware/optical/cdrtools/actions.py
+++ hardware/optical/cdrtools/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
