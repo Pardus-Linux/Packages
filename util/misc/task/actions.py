e1705e11269878f00357ce6acbe9c6176c72c852
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2011-04-15 06:22:29 +0000

    Version bump.

--- util/misc/task/actions.py
+++ util/misc/task/actions.py
@@ -6,6 +6,7 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
@@ -19,3 +20,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
+    pisitools.remove("/usr/share/doc/task/INSTALL")
