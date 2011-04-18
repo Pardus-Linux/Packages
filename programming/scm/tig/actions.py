f7999403320de6f3ba59ba0baa56839e191d5f29
Ozan Çağlayan <ozan@pardus.org.tr>
2011-04-18 12:07:28 +0000

    Fix parsing of i-containing keymaps from tigrc

--- programming/scm/tig/actions.py
+++ programming/scm/tig/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2009-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
