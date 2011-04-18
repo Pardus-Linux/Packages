248ea7a5835f29026714d54372ab7b324431cfaf
Ozan Çağlayan <ozan@pardus.org.tr>
2011-04-18 08:15:08 +0000

    Version bump

--- util/misc/strace/actions.py
+++ util/misc/strace/actions.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def setup():
-    autotools.autoreconf("-vfi")
+    #autotools.autoreconf("-vfi")
     autotools.configure()
 
 def build():
     autotools.make()
 
-def install():
-    pisitools.dobin("strace")
-    pisitools.dobin("strace-graph")
+def check():
+    autotools.make("check")
 
-    pisitools.doman("strace.1")
-    pisitools.dodoc("ChangeLog", "CREDITS", "NEWS", "PORTING", "README*", "TODO")
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("ChangeLog", "CREDITS", "NEWS", "PORTING", "README", "README-linux", "TODO")
