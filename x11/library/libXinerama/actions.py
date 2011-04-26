29d27fcc35cb36354c8dbdbaf45a8ed8c1c6ee94
Fatih Arslan <farslan@pardus.org.tr>
2011-04-26 18:51:30 +0000

    Add emul32 package for libXinerama. Thanks to Emre Erenoglu for contribution

--- x11/library/libXinerama/actions.py
+++ x11/library/libXinerama/actions.py
@@ -6,9 +6,16 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
-    autotools.configure("--disable-static")
+    options = "--disable-static"
+
+    if get.buildTYPE() == "emul32":
+        options += " --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
