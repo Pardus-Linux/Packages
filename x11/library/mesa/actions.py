44e9e9ebc6e73bc43c5488097d76df11e366896d
Fatih Arslan <farslan@pardus.org.tr>
2011-04-26 12:29:04 +0000

    * Add emul32 package for mesa * libdrm-nouveau has to be written, we've enabled nouveau gallium drivers

--- x11/library/mesa/actions.py
+++ x11/library/mesa/actions.py
@@ -14,25 +14,41 @@ def setup():
     shelltools.export("CFLAGS", "%s -DNDEBUG" % get.CFLAGS())
 
     autotools.autoreconf("-vif")
-    autotools.configure("--enable-pic \
-                         --disable-xcb \
-                         --enable-glx-tls \
-                         --disable-gl-osmesa \
-                         --disable-egl \
-                         --disable-glw \
-                         --disable-glut \
-                         --enable-gallium \
-                         --enable-gallium-llvm \
-                         --disable-gallium-svga \
-                         --disable-gallium-i915 \
-                         --disable-gallium-i965 \
-                         --enable-gallium-radeon \
-                         --enable-gallium-r600 \
-                         --enable-gallium-nouveau \
-                         --with-driver=dri \
-                         --with-dri-driverdir=/usr/lib/xorg/modules/dri \
-                         --with-dri-drivers=i810,i915,i965,mach64,nouveau,r128,r200,r600,radeon,sis,tdfx \
-                         --with-state-trackers=dri,glx")
+
+    options ="--enable-pic \
+              --disable-xcb \
+              --enable-glx-tls \
+              --disable-gl-osmesa \
+              --disable-egl \
+              --disable-glw \
+              --disable-glut \
+              --enable-gallium \
+              --enable-gallium-llvm \
+              --disable-gallium-svga \
+              --disable-gallium-i915 \
+              --disable-gallium-i965 \
+              --enable-gallium-radeon \
+              --enable-gallium-r600 \
+              --enable-gallium-nouveau \
+              --enable-gallium-swrast \
+              --with-driver=dri \
+              --with-dri-driverdir=/usr/lib/xorg/modules/dri \
+              --with-dri-drivers=i810,i915,i965,mach64,nouveau,r128,r200,r600,radeon,sis,tdfx \
+              --with-state-trackers=dri,glx"
+
+
+    if get.buildTYPE() == "emul32":
+        # compile with llvm doesn't work for now, test it later
+        options += " --libdir=/usr/lib32 \
+                     --with-dri-driverdir=/usr/lib32/xorg/modules/dri \
+                     --disable-gallium-llvm \
+                     --enable-32-bit"
+
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+        shelltools.export("CXXFLAGS", "%s -m32" % get.CXXFLAGS())
+        shelltools.export("LDFLAGS", "%s -m32" % get.LDFLAGS())
+
+    autotools.configure(options)
 
     pisitools.dosed("configs/autoconf", "(PYTHON_FLAGS) = .*", r"\1 = -t")
 
@@ -42,6 +58,10 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    if get.buildTYPE() == "emul32":
+        pisitools.rename("/usr/lib32/xorg/modules/dri/swrastg_dri.so", "swrast_dri.so")
+        return
 
     # Use llvmpipe instead of classic swrast driver
     pisitools.rename("/usr/lib/xorg/modules/dri/swrastg_dri.so", "swrast_dri.so")
