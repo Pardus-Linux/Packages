5cfa3ae6e68a11f16bfe4c9da7a73fecc83a6167
Mete Alpaslan <mete@pardus.org.tr>
2011-04-27 13:25:41 +0000

    Merged koffice dep to 2011

---
+++ office/misc/pstoedit/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    pisitools.dosed("configure.ac", "-pedantic ", "")
+    pisitools.dosed("configure.ac", "CXXFLAGS=\"-g\"", "")
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-static \
+                         --with-emf \
+                         --with-swf")
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.doman("doc/pstoedit.1")
+    pisitools.dohtml("doc/*.htm")
+
+    pisitools.dodoc("doc/readme.txt")
