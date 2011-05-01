ecc6b22e5314278d873191c30d49e159aca78828
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2011-05-01 17:48:35 +0000

    Rename the package from wormux-bonusmaps to warmux-bonusmaps.

--- game/strategy/wormux-bonusmaps/actions.py
+++ game/strategy/wormux-bonusmaps/actions.py
@@ -9,11 +9,10 @@ from pisi.actionsapi import pisitools
 WorkDir = "."
 
 def install():
-    pisitools.insinto("/usr/share/wormux/map", "*")
+    pisitools.insinto("/usr/share/warmux/map", "*")
 
-    # These are now in the main Wormux distribution
-    pisitools.removeDir("/usr/share/wormux/map/cowland")
-    pisitools.removeDir("/usr/share/wormux/map/grenouilles")
+    # These are now in the main Warmux distribution
+    pisitools.removeDir("/usr/share/warmux/map/urbanheights")
 
-    pisitools.remove("/usr/share/wormux/map/README")
-    pisitools.remove("/usr/share/wormux/map/pisiBuildState")
+    pisitools.remove("/usr/share/warmux/map/README")
+    pisitools.remove("/usr/share/warmux/map/pisiBuildState")
