51d09a3ef1f0a513a9f1a056c7710a957fb393d1
Ozan Çağlayan <ozan@pardus.org.tr>
2011-05-03 06:37:25 +0000

    Bump to 1.3

--- multimedia/misc/frei0r-plugins/actions.py
+++ multimedia/misc/frei0r-plugins/actions.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2009-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+
+WorkDir = "frei0r-1.3"
 
 def setup():
     autotools.autoreconf("-fi")
