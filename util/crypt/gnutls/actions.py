8adca2a1d7aed239744b1ec7e817b4e534b2aa19
Fatih Arslan <farslan@pardus.org.tr>
2011-05-03 07:11:54 +0000

    Add emul32 component

--- util/crypt/gnutls/actions.py
+++ util/crypt/gnutls/actions.py
@@ -7,17 +7,28 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
 
 def setup():
     shelltools.export("AUTOPOINT", "true")
     autotools.autoreconf("-fi")
-    autotools.configure("--disable-static \
-                         --disable-rpath \
-                         --disable-dependency-tracking \
-                         --enable-guile \
-                         --with-lzo \
-                         --with-zlib \
-                         --with-included-libcfg")
+
+    options = "--disable-static \
+               --disable-rpath \
+               --disable-dependency-tracking \
+               --enable-guile \
+               --with-lzo \
+               --with-zlib \
+               --with-included-libcfg"
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --libdir=/usr/lib32"
+        shelltools.export("CC", "%s -m32" % get.CC())
+        shelltools.export("CXX", "%s -m32" % get.CXX())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
@@ -26,5 +37,8 @@ def check():
     autotools.make("check")
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/emul32")
 
