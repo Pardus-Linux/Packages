3e0422117d44023056158b1fd37115877decbc44
Fatih Arslan <farslan@pardus.org.tr>
2011-05-03 06:07:17 +0000

    Add emul32 component

--- util/crypt/libgpg-error/actions.py
+++ util/crypt/libgpg-error/actions.py
@@ -5,12 +5,20 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
-    autotools.configure("--enable-nls \
-                         --disable-rpath \
-                         --disable-languages")
+    options = "--enable-nls \
+               --disable-rpath \
+               --disable-languages"
 
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    autotools.configure(options)
 def build():
     autotools.make()
 
@@ -18,6 +26,11 @@ def check():
     autotools.make("check")
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    if get.buildTYPE() == "emul32":
+        # contains 32bit binaries under /usr/bin
+        pisitools.removeDir("/emul32")
+        return
 
     pisitools.dodoc("AUTHORS", "COPYING*", "ChangeLog", "NEWS", "README", "THANKS")
