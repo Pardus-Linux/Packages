fc93c19ade45f2f15c261d3f3cd993447f8b507d
Onur Küçük <onur@pardus.org.tr>
2011-05-03 12:54:37 +0000

    Force enable libmpeg2 usage, needed for dvdrip fixes pb#17892
    BUG:COMMENT:17892

--- multimedia/converter/transcode/actions.py
+++ multimedia/converter/transcode/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -19,17 +18,16 @@ def setup():
     shelltools.export("CFLAGS", "%s -DDCT_YUV_PRECISION=1" % get.CFLAGS())
     shelltools.export("CXXFLAGS", "%s -DDCT_YUV_PRECISION=1" % get.CXXFLAGS())
 
-    autotools.configure("--with-default-xvid=xvid4 \
-                         --enable-deprecated \
+    autotools.configure("--enable-deprecated \
                          --enable-experimental \
                          --enable-mmx \
                          --enable-3dnow \
                          --enable-sse \
                          --enable-sse2 \
-                         --enable-netstream \
                          --enable-freetype2 \
+                         --enable-libv4l2 \
+                         --enable-libv4lconvert \
                          --enable-v4l \
-                         --enable-v4l2 \
                          --enable-lame \
                          --enable-x264 \
                          --enable-xvid \
@@ -41,25 +39,24 @@ def setup():
                          --enable-libquicktime \
                          --enable-lzo \
                          --enable-a52 \
-                         --enable-libmpeg3 \
+                         --enable-libmpeg2 \
+                         --enable-libmpeg2convert \
                          --enable-libxml2 \
                          --enable-mjpegtools \
                          --enable-nuv \
                          --enable-sdl \
-                         --enable-libfame \
                          --enable-imagemagick \
                          --enable-libjpeg \
                          --with-mod-path=/usr/lib/transcode \
                          --with-x \
-                         --with-libpostproc-builddir=/usr/lib \
                          --with-lzo-includes=/usr/include/lzo \
-                         --disable-avifile \
-                         --disable-gtk")
+                         ")
 
 def build():
     autotools.make("all")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
     pisitools.dodoc("AUTHORS", "ChangeLog", "README", "TODO")
 
