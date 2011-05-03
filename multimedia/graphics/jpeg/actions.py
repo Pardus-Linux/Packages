a08e0db05f95771e60fb5495706403db69952b2a
Fatih Arslan <farslan@pardus.org.tr>
2011-05-03 12:58:28 +0000

    Add emul32 component

--- multimedia/graphics/jpeg/actions.py
+++ multimedia/graphics/jpeg/actions.py
@@ -9,19 +9,30 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import libtools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
     autotools.autoreconf("-vfi")
-    autotools.configure("--enable-shared \
-                         --disable-static \
-                         --enable-maxmem=64 \
-                         --disable-dependency-tracking")
+    options = "--enable-shared \
+               --disable-static \
+               --enable-maxmem=64 \
+               --disable-dependency-tracking"
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall('DESTDIR="%s"' % get.installDIR())
+
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/emul32")
+        return
 
     # they say some programs use this
     pisitools.insinto("/usr/include", "jpegint.h")
