51f1d5755dcf73de779549d51d2abc89c9a93e77
Fatih Arslan <farslan@pardus.org.tr>
2011-05-03 09:14:30 +0000

    Add emul32 component

--- multimedia/graphics/giflib/actions.py
+++ multimedia/graphics/giflib/actions.py
@@ -9,19 +9,32 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import libtools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
     libtools.libtoolize("--force --install")
     autotools.autoreconf("-fi")
-    autotools.configure("--with-x \
-                         --disable-gl \
-                         --disable-static")
+
+    options = "--with-x \
+               --disable-gl \
+               --disable-static"
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --libdir=/usr/lib32"
+
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    autotools.configure(options)
 
 def build():
     autotools.make("-j1")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/emul32")
 
     pisitools.dohtml("doc/")
     pisitools.dodoc("AUTHORS","BUGS","ChangeLog","NEWS","doc/*.txt")
