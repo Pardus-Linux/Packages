55bead62e386b9af419397e42d249590d7c29bac
Fatih Arslan <farslan@pardus.org.tr>
2011-05-03 08:09:02 +0000

    Add emul32 component

--- system/base/readline/actions.py
+++ system/base/readline/actions.py
@@ -13,17 +13,25 @@ from pisi.actionsapi import get
 def setup():
     #Force link to ncurses instead of tinfo, which we don't have, will be needed when we use as-needed ;)
     pisitools.dosed("support/shobj-conf", "SHLIB_LIBS='-ltinfo'", "SHLIB_LIBS=-lncurses")
-
     shelltools.export("CFLAGS", "%s -D_GNU_SOURCE" % get.CFLAGS())
-    autotools.configure("--with-curses \
-                         --libdir=/lib \
-                         --disable-static")
 
+    options = "--with-curses \
+               --libdir=/lib \
+               --disable-static"
+
+    if get.buildTYPE() == "emul32":
+        options += " --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    autotools.configure(options)
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s install" % get.installDIR())
+
+    if get.buildTYPE() == "emul32":
+        return
 
     pisitools.dohtml("doc/")
     pisitools.dodoc("CHANGELOG", "CHANGES", "README", "USAGE", "NEWS")
