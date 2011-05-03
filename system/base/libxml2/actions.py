0fab39ee814de952258cda58bad81b7a216c97a5
Fatih Arslan <farslan@pardus.org.tr>
2011-05-03 08:09:50 +0000

    Add emul32 component

--- system/base/libxml2/actions.py
+++ system/base/libxml2/actions.py
@@ -8,13 +8,22 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
-    autotools.configure("--with-zlib \
-                         --with-python \
-                         --with-readline \
-                         --enable-ipv6 \
-                         --disable-static")
+    options = "--with-zlib \
+               --with-python \
+               --with-readline \
+               --enable-ipv6 \
+               --disable-static"
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --without-python \
+                     --libdir=/usr/lib32"
+        shelltools.export("CC", "%s -m32" % get.CC())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
@@ -24,6 +33,12 @@ def check():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    if get.buildTYPE() == "emul32":
+        # prevent that binaries under /usr/bin overwrites 64-bit binaries
+        pisitools.removeDir("/emul32")
+        pisitools.removeDir("/usr/share/gtk-doc")
+        return
 
     for i in ["", "-python"]:
         pisitools.rename("/%s/libxml2%s-%s" % (get.docDIR(), i, get.srcVERSION()), "libxml2%s" % i)
