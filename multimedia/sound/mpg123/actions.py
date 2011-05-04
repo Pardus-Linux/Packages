bb2b535f72fde2403ff76b0f34bf4d7248531ba8
Fatih Arslan <farslan@pardus.org.tr>
2011-05-04 08:50:24 +0000

    * New version 1.13.3, new functions and bug fixes * Add emul32 component * Fix an endless loop on bad headers, bug 3267863 * Prevent compiler warnings about unused variables with gcc 4.6 * Fix return value handling of getaddrinfo(), bug 3288333 * Make decwin size computation work for OPT_ARM and sizeof(long) != 4 (bug 3288360)

--- multimedia/sound/mpg123/actions.py
+++ multimedia/sound/mpg123/actions.py
@@ -7,6 +7,7 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 #This package using ltdl. .la files should be deleted only for plugins
 KeepSpecial = ["libtool"]
@@ -15,17 +16,29 @@ def setup():
     cpu = "x86-64" if get.ARCH() == "x86_64" else "sse"
 
     pisitools.dosed("configure", "-faltivec")
-    autotools.configure('--with-audio="alsa oss" \
-                         --with-cpu=%s \
-                         --with-optimization=2 \
-                         --enable-network=yes \
-                         --disable-ltdl-install' % cpu)
+    options = '--with-audio="alsa oss" \
+               --with-cpu=%s \
+               --with-optimization=2 \
+               --enable-network=yes \
+               --disable-ltdl-install' % cpu
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --with-cpu=i586 \
+                     --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/emul32")
+        return
 
     pisitools.dodoc("ChangeLog", "COPYING", "NEWS", "README", "AUTHORS")
 
