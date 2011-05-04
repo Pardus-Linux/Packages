04808205188339daf0f843d30db92770962b2532
Fatih Arslan <farslan@pardus.org.tr>
2011-05-04 13:40:01 +0000

    Add emul32 component

--- multimedia/sound/gsm/actions.py
+++ multimedia/sound/gsm/actions.py
@@ -7,18 +7,26 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 WorkDir="gsm-1.0-pl13"
 
 def setup():
-    pisitools.dosed("Makefile", "pardusCC", get.CC())
-    pisitools.dosed("Makefile", "pardusCFLAGS", get.CFLAGS())
+    multilib = " -m32" if get.buildTYPE() == "emul32" else ""
+    pisitools.dosed("Makefile", "pardusCC", "%s %s" % (get.CC(), multilib))
+    pisitools.dosed("Makefile", "pardusCFLAGS", "%s %s" % (get.CFLAGS(), multilib))
 
 def build():
     autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s bindir=/usr/bin" % get.installDIR())
+    if get.buildTYPE() == "emul32":
+        autotools.rawInstall("DESTDIR=%s bindir=/emul32 libdir=/usr/lib32" % get.installDIR())
+        pisitools.removeDir("/emul32")
+        pisitools.remove("/usr/lib32/libgsm.a")
+        return
+    else:
+        autotools.rawInstall("DESTDIR=%s bindir=/usr/bin" % get.installDIR())
 
     for bin in ["tcat","untoast"]:
         pisitools.remove("/usr/bin/%s" % bin)
