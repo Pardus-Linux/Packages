68a4c38c79670bae8a0c3fb641dad9ea476e040e
Fatih Arslan <farslan@pardus.org.tr>
2011-05-04 09:23:50 +0000

    Add emul32 component

--- multimedia/sound/openal/actions.py
+++ multimedia/sound/openal/actions.py
@@ -8,18 +8,33 @@
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 WorkDir = "openal-soft-%s" % get.srcVERSION()
 
 def setup():
-    cmaketools.configure("-DALSA=1 \
-                          -DPULSEAUDIO=1 \
-                          -DOSS=1")
+    options = "-DALSA=1 \
+               -DPULSEAUDIO=1 \
+               -DOSS=1"
+
+    if get.buildTYPE() == "emul32":
+        options += " -DCMAKE_INSTALL_PREFIX=/emul32 \
+                     -DLIB_SUFFIX=32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    cmaketools.configure(options)
 
 def build():
     cmaketools.make()
 
 def install():
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    # is there any "libdir" prefix for cmake ?
+    if get.buildTYPE() == "emul32":
+        from distutils.dir_util import copy_tree
+        copy_tree("%s/emul32/lib32/" % get.installDIR(), "%s/usr/lib32" % get.installDIR())
+        pisitools.removeDir("/emul32")
+        return
 
     pisitools.dodoc("COPYING", "alsoftrc.sample")
