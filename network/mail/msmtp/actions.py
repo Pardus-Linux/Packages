1305265d4ffe7e5090953b8b87ebbbf2e99c6996
Fatih Arslan <farslan@pardus.org.tr>
2011-05-17 06:08:27 +0000

    * New stable release * Remove the unmaintained pt_BR translation. * Simplify manual license: use a simple permissive license instead of GNU FDL. * Replace a call to gnutls_protocol_set_priority(), which is deprecated in GnuTLS >= 2.12.0. Fixes Debian bug #624048 (reported for mpop). * Remove gnulib, for simplicity and maintainability. * Update GNU autotools files, in particular configure.ac.

--- network/mail/msmtp/actions.py
+++ network/mail/msmtp/actions.py
@@ -20,5 +20,4 @@ def install():
     pisitools.insinto("/usr/share/doc/%s/examples"% get.srcTAG(), "doc/*.example")
     pisitools.insinto("/usr/share/doc/%s/examples"% get.srcTAG(), "scripts/msmtpqueue/*")
 
-    pisitools.dohtml("doc/msmtp.html")
-    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "THANKS", "doc/msmtp.pdf", "doc/Mutt+msmtp.txt")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "THANKS", "doc/Mutt+msmtp.txt")
