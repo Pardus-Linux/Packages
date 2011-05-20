8385d9af62a471290cd837fb65450c5e30681242
Ozan Çağlayan <ozan@pardus.org.tr>
2011-05-20 12:02:20 +0000

    - Refactor postinstall script,  - Use pure_install target to avoid touching system's ParserDetails.ini,  - Drop old patches.

--- programming/language/perl/perl-XML-SAX/actions.py
+++ programming/language/perl/perl-XML-SAX/actions.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2009 TUBITAK/UEKAE
+# Copyright 2006-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import perlmodules
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 WorkDir = "%s-%s" % (get.srcNAME()[5:], get.srcVERSION())
@@ -22,6 +22,6 @@ def check():
     perlmodules.make("test")
 
 def install():
-    perlmodules.install()
-
-    shelltools.touch("%s/usr/lib/perl5/vendor_perl/5.12.2/XML/SAX/ParserDetails.ini" % get.installDIR())
+    # pure_install avoids touching system's ParserDetails.ini
+    # This ini file gets generated/updated on post/preInstall
+    perlmodules.install("pure_install")
