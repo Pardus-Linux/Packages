fbf329e203acaa89a52917bad9caaeadafab9c30
Gökçen Eraslan <gokcen@pardus.org.tr>
2011-05-20 11:40:18 +0000

    Remove trunk translations from l10n package.

--- desktop/kde/l10n/kde-l10n-de/actions.py
+++ desktop/kde/l10n/kde-l10n-de/actions.py
@@ -9,12 +9,18 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import get
 from pisi.actionsapi import kde4
+from pisi.actionsapi import pisitools
 
 WorkDir="%s-%s" % (get.srcNAME(), get.srcVERSION())
 
 shelltools.export("HOME", get.workDIR())
 
 def setup():
+    # Remove after switching to new KDEPIM 4.6 tree
+    # We already have those translations in kdepim-4.4 package
+    pisitools.dosed("messages/CMakeLists.txt", "add_subdirectory\\(kdepim\\-runtime\\)", "#add_subdirectory(kdepim-runtime)")
+    pisitools.dosed("messages/CMakeLists.txt", "add_subdirectory\\(kdepim\\)", "#add_subdirectory(kdepim)")
+
     kde4.configure()
 
 def build():
@@ -22,3 +28,4 @@ def build():
 
 def install():
     kde4.install()
+
