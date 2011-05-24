072550d9d2b2af261a92f608fa82928095deeed1
Ozan Çağlayan <ozan@pardus.org.tr>
2011-05-24 11:50:23 +0000

    - Version bump,  - Add systemd files,  - Fix arping's broadcast address parsing from sysfs (Kudos to İbrahim),  - Update man pages,  - Add ifenslave command for ethernet bonding.

--- system/base/iputils/actions.py
+++ system/base/iputils/actions.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir="iputils-s%s" % get.srcVERSION()
-
 def build():
-    autotools.make()
+    autotools.make("CCOPT='%s -fpie' CC=%s" % (get.CFLAGS(), get.CC()))
+
+    autotools.compile("ifenslave.c -o ifenslave")
 
 def install():
-    for app in ["ping","ping6"]:
+    for app in ["ping", "ping6"]:
         pisitools.dobin(app)
 
-    for app in ["clockdiff","arping","rdisc","tracepath","tracepath6","traceroute6"]:
+    for app in ["clockdiff", "arping", "rdisc", "tracepath", "tracepath6", "traceroute6", "ifenslave"]:
         pisitools.dosbin(app)
 
+    # We will not need these if we set cap on postInstall like Fedora
     shelltools.chmod("%s/usr/bin/ping" % get.installDIR(), 04711)
     shelltools.chmod("%s/usr/bin/ping6" % get.installDIR(), 04711)
 
