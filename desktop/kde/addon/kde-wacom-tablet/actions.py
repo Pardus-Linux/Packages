ca4665bbe2eaf1dd82b430646e71115dc4eeb8fc
Metin Akdere <metin@pardus.org.tr>
2011-05-24 07:53:52 +0000

    * Version bump, listed some of the enhancements below, see homepage for the rest * Support for the new 0.10.11 wacom driver * Automatic tablet rotation when the XRandR screen rotates * Calibration tool * Support for more tablets (Pen and Touch series and some other) * Add support for global shortcuts

--- desktop/kde/addon/kde-wacom-tablet/actions.py
+++ desktop/kde/addon/kde-wacom-tablet/actions.py
@@ -6,10 +6,11 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 from pisi.actionsapi import kde4
 
-WorkDir = "wacomtablet-%s" % get.srcVERSION()
+WorkDir = "wacomtablet-v%s" % get.srcVERSION()
 shelltools.export("HOME",get.workDIR())
 
 def setup():
@@ -20,3 +21,5 @@ def build():
 
 def install():
     kde4.install()
+
+    pisitools.dodoc("AUTHORS", "COPYING", "README")
