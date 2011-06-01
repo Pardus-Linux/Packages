10168eae5ecffd1b67d0768185de6b166176576e
Fatih Arslan <farslan@pardus.org.tr>
2011-06-01 17:31:38 +0000

    * New development release * Change build options to make a single configure and build for each buildTYPE()

--- hardware/emulator/wine/actions.py
+++ hardware/emulator/wine/actions.py
@@ -8,32 +8,10 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-wine64 = get.ARCH() == "x86_64"
-
 def setup():
     autotools.autoreconf("-vif")
 
-    if wine64:
-        shelltools.makedirs("build-wow64")
-        shelltools.sym("../configure", "build-wow64/configure")
-
-    shelltools.makedirs("build")
-    shelltools.sym("../configure", "build/configure")
-
-    shelltools.cd("build")
-    args = "--enable-win64" if wine64 else ""
-    autotools.configure("--without-capi \
-                         --with-curses \
-                         --without-esd \
-                         --with-opengl \
-                         --with-pulse \
-                         %s" % args)
-
-def build():
-    autotools.make("-C build")
-
-    if wine64:
-        shelltools.cd("build-wow64")
+    if get.buildTYPE() == "emul32":
         autotools.configure("--without-capi \
                              --with-curses \
                              --without-esd \
@@ -41,16 +19,28 @@ def build():
                              --with-pulse \
                              --with-x \
                              --libdir=/usr/lib32 \
-                             --with-wine64=../build")
+                             --with-wine64=%s/work/%s" % (get.pkgDIR(), get.srcDIR()))
+    else:
+        args = ""
+        if get.ARCH() == "x86_64":
+            args = " --enable-win64"
 
-        autotools.make()
+        autotools.configure("--without-capi \
+                             --with-curses \
+                             --without-esd \
+                             --with-opengl \
+                             --with-pulse \
+                             --with-x \
+                             %s" % args)
 
+
+def build():
+    autotools.make()
 
 def install():
-    autotools.install("-C build UPDATE_DESKTOP_DATABASE=/bin/true")
-
-    if wine64:
-        autotools.install("-C build-wow64 UPDATE_DESKTOP_DATABASE=/bin/true libdir=%s/usr/lib32 dlldir=%s/usr/lib32/wine" % (get.installDIR(), get.installDIR()))
-
+    if get.buildTYPE() == "emul32":
+        autotools.install("UPDATE_DESKTOP_DATABASE=/bin/true libdir=%s/usr/lib32 dlldir=%s/usr/lib32/wine" % (get.installDIR(), get.installDIR()))
+    else:
+        autotools.install("UPDATE_DESKTOP_DATABASE=/bin/true libdir=%s/usr/lib dlldir=%s/usr/lib/wine" % (get.installDIR(), get.installDIR()))
 
     pisitools.dodoc("ANNOUNCE", "AUTHORS", "COPYING.LIB", "LICENSE*", "README", "documentation/README.*")
