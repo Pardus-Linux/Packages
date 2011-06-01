ea645826caef7de41cb15db7bbc8bc0a3becd48f
Gökhan Özbulak <gozbulak@pardus.org.tr>
2011-06-01 07:13:50 +0000

    Make Django-docs package to let users utilizing from compherensive Django documentation

--- programming/language/python/django/Django/actions.py
+++ programming/language/python/django/Django/actions.py
@@ -6,10 +6,17 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pythonmodules
-
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 def build():
     pythonmodules.compile()
 
+    shelltools.cd("docs")
+    autotools.make("html")
+
 def install():
     pythonmodules.install()
+
+    shelltools.copytree("docs/_build/html", "%s/%s/%s" % (get.installDIR(), get.docDIR(), get.srcNAME()))
