2baf704f389f7c466eff10e97e0a6e65a013e01e
Fatih Arslan <farslan@pardus.org.tr>
2011-06-01 06:03:21 +0000

    Add docbook-xsl entries and change path to match the new catalog entries

--- office/docbook/docbook-xml/actions.py
+++ office/docbook/docbook-xml/actions.py
@@ -11,9 +11,9 @@ WorkDir="."
 
 def install():
     for version in ["4.1.2", "4.2", "4.3", "4.4", "4.5"]:
-        pisitools.insinto("/usr/share/sgml/docbook/xml-dtd-%s" % version, "*.dtd")
-        pisitools.insinto("/usr/share/sgml/docbook/xml-dtd-%s" % version, "*.mod")
-        pisitools.insinto("/usr/share/sgml/docbook/xml-dtd-%s" % version, "docbook.cat")
-        pisitools.insinto("/usr/share/sgml/docbook/xml-dtd-%s/ent" % version, "ent/*.ent")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "*.dtd")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "*.mod")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "docbook.cat")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s/ent" % version, "ent/*.ent")
 
     pisitools.dodoc("ChangeLog", "README")
