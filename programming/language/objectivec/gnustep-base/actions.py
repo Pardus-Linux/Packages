9036634131656f1a0bd6fc7d76b44e87b967b396
Meltem Parmaksız <meltem@pardus.org.tr>
2011-06-09 12:42:29 +0000

    fix sandbox violation

--- programming/language/objectivec/gnustep-base/actions.py
+++ programming/language/objectivec/gnustep-base/actions.py
@@ -10,22 +10,25 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
+WorkPath="%s/%s-%s"  % (get.workDIR(), get.srcNAME(), get.srcVERSION())
+
 def setup():
     autotools.autoreconf("-vfi")
     autotools.configure("--enable-ffcall \
                         --enable-gnutls \
                         --enable-tls \
                         --enable-zeroconf \
-                        --with-default-config=/etc/GNUstep/GNUstep.conf")
+                        --with-default-config=%s/GNUstep.conf" % WorkPath)
 
 def build():
     autotools.make()
 
     shelltools.export("LD_LIBRARY_PATH", "%s/%s/Source/obj:${LD_LIBRARY_PATH}" % (get.workDIR(), get.srcDIR()))
-    autotools.make("-C Documentation")
+    autotools.make("-C Documentation \
+                   GNUSTEP_CONFIG_FILE=%s/GNUstep.conf" % WorkPath)
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    autotools.rawInstall("-C Documentation")
+    autotools.rawInstall("DESTDIR=%s -C Documentation" % get.installDIR())
 
     pisitools.dodoc("COPYING", "NEWS")
