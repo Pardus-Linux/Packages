bfbf98a1c05907a262e1b79ddaba7be42d262cb8
Fatih Arslan <farslan@pardus.org.tr>
2011-06-27 13:47:48 +0000

    Add tmux to repo, thanks for reviews
    BUG:FIXED:18302

---
+++ util/misc/tmux/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2011 TUBITAK/BILGEM
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("PREFIX=/%s DESTDIR=%s" % (get.defaultprefixDIR(), get.installDIR()))
+
+    # we love vim
+    pisitools.insinto("/usr/share/vim/vimfiles/syntax/", "examples/tmux.vim", "tmux.vim")
+    pisitools.insinto("/usr/share/doc/tmux/examples", "examples/*")
+    pisitools.remove("/usr/share/doc/tmux/examples/tmux.vim")
+
+    # Bash completion
+    pisitools.domove("/usr/share/doc/tmux/examples/bash_completion_tmux.sh", "/etc/bash_completion.d", "tmux")
+
+    pisitools.dodoc("CHANGES", "FAQ", "NOTES")
