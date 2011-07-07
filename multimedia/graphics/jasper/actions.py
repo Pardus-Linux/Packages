bd5976813f23899e7c54e8767363c5e43ee3ce6c
Fatih Arslan <farslan@pardus.org.tr>
2011-07-07 19:35:23 +0000

    Add emul32 component
    BUG:FIXED:18636

--- multimedia/graphics/jasper/actions.py
+++ multimedia/graphics/jasper/actions.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import libtools
 from pisi.actionsapi import get
 
 def setup():
+    options = "--enable-libjpeg \
+               --enable-opengl \
+               --enable-shared \
+               --disable-static"
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
     autotools.autoreconf("-fi")
+
     libtools.libtoolize("--force --install")
 
-    autotools.configure("--enable-libjpeg \
-                         --enable-opengl \
-                         --enable-shared \
-                         --disable-static")
+    autotools.configure(options)
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/emul32")
 
     pisitools.dodoc("NEWS", "README", "doc/*")
