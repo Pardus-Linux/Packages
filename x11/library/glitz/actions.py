5216b3fbbe49314b7c431a615263c78e83784efe
Fatih Arslan <farslan@pardus.org.tr>
2011-07-08 06:41:49 +0000

    Add emul32 component
    BUG:FIXED:18639

--- x11/library/glitz/actions.py
+++ x11/library/glitz/actions.py
@@ -1,14 +1,22 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-static")
+    options = "--disable-static"
+
+    if get.buildTYPE() == "emul32":
+        options += " --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
