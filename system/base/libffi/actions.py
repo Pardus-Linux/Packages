cb2f777f038a125a589cf51dd069b2a4ef9aa1a3
Fatih Arslan <farslan@pardus.org.tr>
2011-07-08 11:38:22 +0000

    Add emul32 component
    BUG:FIXED:18646

--- system/base/libffi/actions.py
+++ system/base/libffi/actions.py
@@ -1,14 +1,24 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-static")
+    options = "--disable-static"
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --includedir=/emul32/include \
+                     --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
@@ -21,3 +31,7 @@ def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("ChangeLog*", "LICENSE", "README*")
+
+    # Remove duplicated header files
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/usr/lib32/%s" % get.srcDIR())
