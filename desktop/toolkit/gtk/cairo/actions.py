ed285cbb148961e75087942077aa12182156f664
Fatih Arslan <farslan@pardus.org.tr>
2011-07-08 07:44:48 +0000

    Add emul32 component
    BUG:FIXED:18644

--- desktop/toolkit/gtk/cairo/actions.py
+++ desktop/toolkit/gtk/cairo/actions.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    options = "--disable-static \
+               --disable-gtk-doc \
+               --enable-glitz \
+               --enable-xlib \
+               --enable-xlib-xrender \
+               --enable-xcb \
+               --enable-ft \
+               --enable-ps \
+               --enable-pdf \
+               --enable-svg \
+               --enable-tee \
+               --enable-png \
+               --with-x"
+
+    if get.buildTYPE() == "emul32":
+        options += " --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
     autotools.autoreconf("-vfi")
-    autotools.configure("--disable-static \
-                         --disable-gtk-doc \
-                         --enable-glitz \
-                         --enable-xlib \
-                         --enable-xlib-xrender \
-                         --enable-xcb \
-                         --enable-ft \
-                         --enable-ps \
-                         --enable-pdf \
-                         --enable-svg \
-                         --enable-tee \
-                         --enable-png \
-                         --with-x")
+    autotools.configure(options)
 
 def build():
     autotools.make()
