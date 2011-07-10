5746dc47a251cc6ae311417a3f8356339c6460d8
Gökçen Eraslan <gokcen@pardus.org.tr>
2011-07-10 12:57:02 +0000

    Switch to unzip solution instead of post-install + unopkg.

--- office/libreoffice/zemberek-office/actions.py
+++ office/libreoffice/zemberek-office/actions.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2009 TUBITAK/UEKAE
+# Copyright 2006-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+import glob
 
 WorkDir="."
 
 def install():
-    pisitools.insinto("/usr/share/zemberek","*.oxt")
+    zemberekdir = "/opt/LibreOffice/lib/libreoffice/share/extensions/zemberek-spell-checker"
+    pisitools.dodir(zemberekdir)
+    zemberekfile = glob.glob("*.oxt")[0]
+    shelltools.system("unzip %s -d %s/%s" % (zemberekfile, get.installDIR(), zemberekdir))
