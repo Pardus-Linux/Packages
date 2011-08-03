52201012865b0bda522f24ed3978575ef3ffa38f
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2011-08-03 11:39:10 +0000

    Major version bump bring many features, bugfixes and improvements that i can't count now (pb#17772).
    BUG:COMMENT:17772

--- game/action/teeworlds/actions.py
+++ game/action/teeworlds/actions.py
@@ -9,23 +9,24 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "%s-src" % get.srcDIR()
-datadir = "/usr/share/teeworlds"
-NoStrip = [datadir]
+WorkDir = "%s-b177-r50edfd37-source" % get.srcNAME()
+DATADIR = "/usr/share/teeworlds"
+NoStrip = [DATADIR]
 
 def setup():
-    shelltools.cd("bam-0.2.0")
+    #shelltools.unlinkDir("src/engine/external/")
+    shelltools.cd("../bam-0.4.0")
     shelltools.chmod("make_unix.sh", 0755)
     shelltools.system("./make_unix.sh")
 
 def build():
-    shelltools.system('CFLAGS="%s" ./bam-0.2.0/src/bam release' % get.CFLAGS())
+    shelltools.system('CFLAGS="%s" ../bam-0.4.0/bam -v release' % get.CFLAGS())
 
 def install():
     pisitools.dobin("teeworlds")
     pisitools.dobin("teeworlds_srv")
     pisitools.rename("/usr/bin/teeworlds_srv", "teeworlds-server")
 
-    pisitools.insinto(datadir, "data/*")
+    pisitools.insinto(DATADIR, "data")
 
     pisitools.dodoc("*.txt")
