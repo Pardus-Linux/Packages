2ecd5065e44da152f3eecc87b21c5746c3b46fa5
Serdar Dalgıç <serdar@pardus.org.tr>
2011-08-03 11:51:55 +0000

    Version bump: * New AutoOpts attribute * the --version option default * Changes in the Licenses and various fixes.

--- programming/build/autogen/actions.py
+++ programming/build/autogen/actions.py
@@ -21,4 +21,5 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", \
+                    "NOTES", "README", "THANKS", "TODO", "VERSION")
