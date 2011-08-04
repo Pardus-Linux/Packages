b9a3c8f07d0ef317b64941b5405a8f25a3f2e35f
Onur Küçük <onur@pardus.org.tr>
2011-08-04 10:18:42 +0000

    Add lib32 package for smpeg, fixes pb#18834 Thanks to Anıl Özbek
    BUG:FIXED:18834

--- multimedia/video/smpeg/actions.py
+++ multimedia/video/smpeg/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -16,19 +15,40 @@ def setup():
     shelltools.export("RANLIB", get.RANLIB())
     shelltools.export("AR", get.AR())
 
-    # enable-debug is bogus, it should stay here
-    autotools.configure("--with-x \
-                         --enable-opengl-player \
-                         --disable-gtk-player \
-                         --enable-mmx \
-                         --disable-assertions \
-                         --disable-static \
-                         --enable-debug")
+    # enable-debug is bogus, it should stay here
+    options = "--with-x \
+               --enable-opengl-player \
+               --disable-gtk-player \
+               --enable-mmx \
+               --disable-assertions \
+               --disable-static \
+               --enable-debug"
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --libdir=/usr/lib32 \
+                     --bindir=/emul32/bin \
+                     --includedir=/emul32/include \
+                     --mandir=/emul32/man \
+                     --disable-sdltest \
+                     --disable-gtktest"
+
+        shelltools.export("CC", "%s -m32" % get.CC())
+        shelltools.export("CXX", "%s -m32" % get.CXX())
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+        shelltools.export("CXXFLAGS", "%s -m32" % get.CFLAGS())
+        shelltools.export("LDFLAGS", "%s -m32" % get.LDFLAGS())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/emul32")
+
     pisitools.dodoc("CHANGES", "README*", "TODO")
 
