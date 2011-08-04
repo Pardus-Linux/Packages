6edc6ad31b66bd121bec63f10ca32e079453c3cb
Onur Küçük <onur@pardus.org.tr>
2011-08-04 10:17:48 +0000

    Add lib32 package for libmikmod, fixes pb#18839 Thanks to Anıl Özbek
    BUG:FIXED:18839

--- multimedia/sound/libmikmod/actions.py
+++ multimedia/sound/libmikmod/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -20,11 +19,25 @@ def setup():
 
     autotools.autoreconf("-vfi -Im4")
     libtools.gnuconfig_update()
-    autotools.configure("--disable-esd \
-                         --disable-af \
-                         --disable-alsa \
-                         --enable-oss \
-                         --disable-static")
+
+    options = "--disable-esd \
+               --disable-af \
+               --disable-alsa \
+               --enable-oss \
+               --disable-static"
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --libdir=/usr/lib32 \
+                     --bindir=/emul32/bin"
+
+        shelltools.export("CC", "%s -m32" % get.CC())
+        shelltools.export("CXX", "%s -m32" % get.CXX())
+        shelltools.export("CFLAGS", "%s -DDRV_RAW -m32" % get.CFLAGS())
+        shelltools.export("CXXFLAGS", "%s -m32" % get.CFLAGS())
+        shelltools.export("LDFLAGS", "%s -m32" % get.LDFLAGS())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
@@ -35,3 +48,5 @@ def install():
     pisitools.dodoc("AUTHORS", "NEWS", "README", "TODO")
     pisitools.dohtml("docs/*.html")
 
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/emul32")
