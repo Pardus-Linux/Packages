a58d65d0c0acd876c8d76b3cc218ace253f44692
Fatih Arslan <farslan@pardus.org.tr>
2011-08-04 12:36:14 +0000

    Add emul32 component. Thanks Anil Ozbek for providing necessary files
    BUG:FIXED:18838

--- system/base/libidn/actions.py
+++ system/base/libidn/actions.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--enable-nls \
-                         --disable-java \
-                         --disable-csharp \
-                         --disable-rpath \
-                         --disable-gtk-doc \
-                         --disable-static")
+    options = "--enable-nls \
+               --disable-java \
+               --disable-csharp \
+               --disable-rpath \
+               --disable-gtk-doc \
+               --disable-static"
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --libdir=/usr/lib32 \
+                     --bindir=/emul32/bin"
+
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
@@ -27,3 +37,6 @@ def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "FAQ", "NEWS", "README", "THANKS", "TODO")
+
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/emul32")
