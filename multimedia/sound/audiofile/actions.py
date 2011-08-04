66170eb95c12770d5a92ee8d84e556dcfaa808be
Onur Küçük <onur@pardus.org.tr>
2011-08-04 10:16:54 +0000

    Add lib32 package for audiofile, fixes pb#18836 Thanks to Anıl Özbek
    BUG:FIXED:18836

--- multimedia/sound/audiofile/actions.py
+++ multimedia/sound/audiofile/actions.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
     pisitools.dosed("test/Makefile.am", "noinst_PROGRAMS", "check_PROGRAMS")
+
     autotools.autoreconf("-vfi")
-    autotools.configure("--disable-dependency-tracking \
-                         --enable-largefile \
-                         --disable-static")
+
+    options = "--disable-dependency-tracking \
+               --enable-largefile \
+               --disable-static"
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --libdir=/usr/lib32 \
+                     --bindir=/emul32/bin"
+
+        shelltools.export("CC", "%s -m32" % get.CC())
+        shelltools.export("CXX", "%s -m32" % get.CXX())
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+        shelltools.export("CXXFLAGS", "%s -m32" % get.CFLAGS())
+        shelltools.export("LDFLAGS", "%s -m32" % get.LDFLAGS())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
@@ -23,3 +38,6 @@ def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "README", "TODO", "NEWS")
+
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/emul32")
