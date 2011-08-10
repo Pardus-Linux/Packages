3963949844a1acd173b6fe74bc249ca7813c3eef
Ozan Çağlayan <ozan@pardus.org.tr>
2011-08-10 05:58:15 +0000

    Version bump
    BUG:FIXED:18914

--- programming/language/vala/actions.py
+++ programming/language/vala/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2010 TUBITAK/UEKAE
+# Copyright (C) 2010-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,6 +10,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    autotools.autoreconf("-fi")
     autotools.configure("--enable-vapigen")
 
 def build():
