1e342c0b5e65d0c09cdfcf826a277929f40488c6
İşbaran Akçayır <isbaran@gmail.com>
2011-08-18 16:59:52 +0000

    new stable release 2.4

--- game/simulation/flightgear/actions.py
+++ game/simulation/flightgear/actions.py
@@ -9,8 +9,6 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "FlightGear-%s" % get.srcVERSION()
-
 def setup():
     shelltools.system("./autogen.sh")
     autotools.configure("--enable-sp-fdms \
