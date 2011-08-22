d1d0696268fc8ed621f6fdf40f597cd748309aef
Fatih Arslan <farslan@pardus.org.tr>
2011-08-22 12:30:51 +0000

    * Update 1.10 * Added --tight option * Backends: simplified dimension calculations * Eps backend: prevent integer overflow in bounding box. * Disallow resolution of 0 dpi. * Man page: added BACKEND TYPES section * Fixed default scaling at 72dpi for variable-sized backends * New gimppath backend produces SVG output suitable for current versions of Gimp. * Added a --flat option for the SVG backend. * Test suite: renamed *.out files to prevent make's built-in implicit rules from accidentally overwriting them. * Added DXF backend. Thanks to Jonas Benedett Dorr for testing. * Use high resolution bounding box in EPS, PDF, and SVG backends * Autoconf: CADD can now be given at compile time

--- multimedia/converter/potrace/actions.py
+++ multimedia/converter/potrace/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006,2009 TUBITAK/UEKAE
+# Copyright 2011 TUBITAK/BILGEM
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -17,7 +17,11 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("AUTHORS", "NEWS", "README")
+
+    pisitools.dodoc("AUTHORS", "NEWS", "README", "COPYING")
 
     pisitools.removeDir("/usr/lib")
     pisitools.removeDir("/usr/include")
+
+    # Useless example output file
+    pisitools.remove("/usr/share/doc/potrace/placement.pdf")
