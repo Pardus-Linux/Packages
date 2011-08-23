e78f16923f0dce9b5d3fbae0ae69e3bb5d80f629
Fatih Arslan <farslan@pardus.org.tr>
2011-08-23 06:55:27 +0000

    Add lib32 package for gdk-pixbuf
    BUG:FIXED:18637

--- desktop/toolkit/gtk/gdk-pixbuf/actions.py
+++ desktop/toolkit/gtk/gdk-pixbuf/actions.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 TUBITAK/UEKAE
+# Copyright 2010-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
-from pisi.actionsapi import shelltools
 
 shelltools.export("HOME", get.workDIR())
 
 def setup():
+    options = "--disable-static \
+               --enable-silent-rules \
+               --enable-introspection \
+               --with-libjasper \
+               --with-included-loaders=png"
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --libdir=/usr/lib32"
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
     autotools.autoreconf("-fiv")
-    autotools.configure("--disable-static \
-                         --enable-silent-rules \
-                         --enable-introspection \
-                         --with-libjasper \
-                         --with-included-loaders=png")
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
-    pisitools.remove("/usr/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("emul32")
 
     pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
