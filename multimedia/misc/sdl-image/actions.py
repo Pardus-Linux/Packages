494c217c4b29325568bb7cd812f7ddfd15dd3aac
Onur Küçük <onur@pardus.org.tr>
2011-08-24 22:25:06 +0000

    Add lib32 package for sdl-image, fixes pb#18995 thanks to Anıl
    BUG:FIXED:18995

--- multimedia/misc/sdl-image/actions.py
+++ multimedia/misc/sdl-image/actions.py
@@ -1,36 +1,50 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 WorkDir = "SDL_image-%s" % get.srcVERSION()
 
 def setup():
-    autotools.configure("--disable-dependency-tracking \
-                         --disable-static \
-                         --enable-gif \
-                         --enable-jpeg \
-                         --enable-tif \
-                         --enable-png \
-                         --enable-pnm \
-                         --enable-bmp \
-                         --enable-xcf \
-                         --enable-xpm \
-                         --enable-tga \
-                         --enable-lbm \
-                         --enable-pcx")
+    options = "--disable-dependency-tracking \
+               --disable-static \
+               --enable-gif \
+               --enable-jpg \
+               --enable-tif \
+               --enable-png \
+               --enable-pnm \
+               --enable-bmp \
+               --enable-xcf \
+               --enable-xpm \
+               --enable-tga \
+               --enable-lbm \
+               --enable-pcx"
+
+    if get.buildTYPE() == "emul32":
+        #options += " --prefix=/emul32 \
+        options += " --includedir=/usr/include \
+                     --libdir=/usr/lib32"
+
+        shelltools.export("PKG_CONFIG_PATH", "/usr/lib32/pkgconfig")
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+        shelltools.export("CXXFLAGS", "%s -m32" % get.CXXFLAGS())
+        shelltools.export("LDFLAGS", "%s -m32" % get.LDFLAGS())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dobin(".libs/showimage")
+
+    if get.buildTYPE() != "emul32":
+        pisitools.dobin(".libs/showimage")
 
     pisitools.dodoc("CHANGES", "COPYING", "README")
