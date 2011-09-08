7b102f42406dbdc5aeba02fd67711e5827e9c406
Fatih Arslan <farslan@pardus.org.tr>
2011-09-08 12:11:48 +0000

    * New beta release * Rearrange dependencies

--- network/web/chromium-browser/actions.py
+++ network/web/chromium-browser/actions.py
@@ -9,6 +9,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+from pisi.actionsapi import kde4
 
 WorkDir = "chromium-%s" % get.srcVERSION()
 
