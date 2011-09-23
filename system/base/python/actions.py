0d0eec6cab9e78fae5e5bf64b6da370ebbaa7472
Ozan Çağlayan <ozan@pardus.org.tr>
2011-09-23 08:49:50 +0000

    - Version bump to 2.7.2, - Add support for multistream bzip2 files.
    With this multistream support, pisi can now unpack newly prepared kde 4.7
    tarballs.

--- system/base/python/actions.py
+++ system/base/python/actions.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 import os
