2366e4887ffefcddebd8634f38afd1b06fa6e454
Ozan Çağlayan <ozan@pardus.org.tr>
2011-09-26 08:45:32 +0000

    Version bump to add support for newer Broadcom chipsets.

--- kernel/default/drivers/module-compat-wireless/actions.py
+++ kernel/default/drivers/module-compat-wireless/actions.py
@@ -5,23 +5,18 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import kerneltools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-from pisi.actionsapi import kerneltools
 
 KDIR = kerneltools.getKernelVersion()
 
 def setup():
-    # Dump code-metrics before building :)
-    shelltools.system("cat code-metrics.txt")
-
-    pisitools.dosed("config.mk", "^KERNEL_SUBLEVEL.*", 'KERNEL_SUBLEVEL := $(shell echo "@KERNELRELEASE@" | sed -n \'s/^2\.6\.\([0-9]\+\).*/\\\\1/p\')')
-
-    for f in ("config.mk", "scripts/gen-compat-autoconf.sh"):
-        pisitools.dosed(f, "@KERNELRELEASE@", KDIR)
+    pisitools.dosed("config.mk", "\$\(MAKE\) -C \$\(KLIB_BUILD\) kernelversion", "echo \"%s\"" % KDIR)
+    pisitools.dosed("scripts/gen-compat-autoconf.sh", "make -C \$KLIB_BUILD kernelversion", "echo \"%s\"" % KDIR)
 
 def build():
     autotools.make("KLIB=/lib/modules/%s" % KDIR)
