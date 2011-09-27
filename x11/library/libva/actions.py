42138fc6f48f1092365af215e436c522b539db9d
Fatih Arslan <farslan@pardus.org.tr>
2011-09-27 11:25:29 +0000

    Add emul32 component. Kudost to Taner
    BUG:COMMENT:18858

--- x11/library/libva/actions.py
+++ x11/library/libva/actions.py
@@ -5,19 +5,33 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
     pisitools.dosed("test/Makefile.am", "^(SUBDIRS =).*$", r"\1 vainfo")
 
     autotools.autoreconf("-vif")
-    autotools.configure("--enable-glx \
-                         --enable-dummy-driver \
-                         --enable-i965-driver")
+    options = "--enable-glx \
+               --enable-dummy-driver \
+               --enable-i965-driver"
+
+    if get.buildTYPE() == "emul32":
+        options += " --prefix=/emul32 \
+                     --libdir=/usr/lib32"
+
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+
+    autotools.configure(options)
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("emul32")
+        return
 
     pisitools.dodoc("COPYING")
