9524262387846bce0dff65707b6486909a5339d7
Fatih Arslan <farslan@pardus.org.tr>
2011-09-30 07:29:52 +0000

    Add lib32 package for pango (pb#18649)
    BUG:FIXED:18649

--- desktop/toolkit/gtk/pango/actions.py
+++ desktop/toolkit/gtk/pango/actions.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
-from pisi.actionsapi import shelltools
 
 shelltools.export("HOME", get.workDIR())
 
 def setup():
-    #autotools.autoreconf("-fiv")
-    autotools.configure("--disable-static")
+    options = "--disable-static"
+
+    if get.buildTYPE() == "emul32":
+        options += " --libdir=/usr/lib32 \
+                     --libexecdir=/emul32/lib \
+                     --bindir=/emul32/bin \
+                     --sbindir=/emul32/sbin \
+                     --mandir=/emul32/usr/share/man"
+
+        shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+        shelltools.export("CXXFLAGS", "%s -m32" % get.CXXFLAGS())
+
+    # autotools.autoreconf("-fiv")
+    autotools.configure(options)
 
 def build():
     autotools.make()
@@ -23,3 +35,7 @@ def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("AUTHORS", "ChangeLog*", "COPYING", "README", "NEWS")
+
+    if get.buildTYPE() == "emul32":
+        pisitools.removeDir("/emul32")
+        return
