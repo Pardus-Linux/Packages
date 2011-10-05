7ad82f0c09798bffbb98deb81b5fa367e9e4599f
Ozan Çağlayan <ozan@pardus.org.tr>
2011-10-05 13:50:05 +0000

    Version bump to SVN snapshot, enable latin extended by default

--- hardware/graphics/cellwriter/actions.py
+++ hardware/graphics/cellwriter/actions.py
@@ -9,12 +9,12 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--with-x \
-                         --without-gnome")
+    autotools.configure()
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
     pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "README", "TODO")
