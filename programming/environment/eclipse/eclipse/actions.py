085599c1b79d3aa9a59ab1ac7301a6d42eaed2c2
Gökhan Özbulak <gozbulak@pardus.org.tr>
2011-10-13 12:09:47 +0000

    Version bump to latest stable release

--- programming/environment/eclipse/eclipse/actions.py
+++ programming/environment/eclipse/eclipse/actions.py
@@ -13,6 +13,3 @@ WorkDir = get.ARCH()
 def install():
     pisitools.dodir("/opt")
     pisitools.insinto("/opt", "eclipse")
-
-    # Make eclipse icon visible on start menu
-    # pisitools.insinto("/usr/share/pixmaps", "%s/opt/eclipse/plugins/org.eclipse.platform_3.6.2.v201102101200/eclipse48.png" % get.installDIR(), "eclipse.png")
