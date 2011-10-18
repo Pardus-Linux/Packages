e3e6b1994630a4b0630920a2414495af56fb0e59
H. İbrahim Güngör <ibrahim@pardus.org.tr>
2011-10-18 06:06:48 +0000

    Move pardus-dev-tools to util/admin. Thanks for reviews.
    BUG:FIXED:18205

---
+++ util/admin/pardus-dev-tools/actions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2011 TUBITAK/BILGEM
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+
+def install():
+    pisitools.dobin("scripts/*")
+
+    pisitools.dodoc("COPYING", "README", "TODO")
