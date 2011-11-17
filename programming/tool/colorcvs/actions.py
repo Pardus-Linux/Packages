1537f8e81a9cce78e795ba931dc10c23d8228b35
Gökhan Özbulak <gozbulak@pardus.org.tr>
2011-11-17 13:15:18 +0000

    * Remove unused pisitools * Add Turkish description for translation

--- programming/tool/colorcvs/actions.py
+++ programming/tool/colorcvs/actions.py
@@ -5,7 +5,6 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
