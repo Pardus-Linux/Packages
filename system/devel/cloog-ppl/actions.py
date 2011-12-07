5aa053c8910d56d23071b2c57bed4f2abf7d4379
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-07 12:12:49 +0200

    Version bump to 0.5.11

--- system/devel/cloog-ppl/actions.py
+++ system/devel/cloog-ppl/actions.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 
 def setup():
-    shelltools.sym(".", "m4")
-    shelltools.sym("LICENSE", "COPYING")
-    for i in ["NEWS", "AUTHORS", "ChangeLog"]:
-        shelltools.touch(i)
-
-    pisitools.dosed("configure.in", "^ppl_minor_version=.*", "ppl_minor_version=11")
-
-    autotools.autoreconf("-vfi")
     autotools.configure("--disable-static \
                          --enable-shared \
                          --with-ppl")
