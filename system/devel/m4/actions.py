a3aecf4b87913e73ff523f6d3cc4881b8000dbcf
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-14 10:14:53 +0200

    Version bump to 1.4.16

--- system/devel/m4/actions.py
+++ system/devel/m4/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,11 +9,13 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--enable-nls \
-                         --enable-changeword")
+    autotools.configure()
 
 def build():
     autotools.make()
+
+def check():
+    autotools.make("check")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
