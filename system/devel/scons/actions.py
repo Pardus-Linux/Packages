e559dd0a7c57af0d98daa160914bb004aab11e30
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-14 10:30:34 +0200

    Version bump to 2.1.0

--- system/devel/scons/actions.py
+++ system/devel/scons/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,9 +8,13 @@ from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 
 def build():
+    pisitools.dosed("script/*", "/usr/bin/env python", "/usr/bin/python")
     pythonmodules.compile()
 
 def install():
-    pythonmodules.install()
+    pythonmodules.install("--no-version-script \
+                           --standalone-lib \
+                           --install-scripts=/usr/bin \
+                           --install-data=/usr/share")
 
     pisitools.dodoc("CHANGES*", "LICENSE*", "README*", "RELEASE*")
