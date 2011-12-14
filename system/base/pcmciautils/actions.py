5999d98f93bdc2fe3053ceb42be44aa169d546a6
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-14 14:49:02 +0200

    Move under /usr

--- system/base/pcmciautils/actions.py
+++ system/base/pcmciautils/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -14,5 +13,8 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.domove("/sbin/*", "/usr/sbin")
+    pisitools.removeDir("/sbin")
 
     pisitools.dodoc("doc/*")
