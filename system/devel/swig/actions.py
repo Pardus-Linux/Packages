4491c60da7f169ce2077e8c6b5c5add4da4fb829
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-14 11:23:20 +0200

    Version bump to 2.0.4

--- system/devel/swig/actions.py
+++ system/devel/swig/actions.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.system("./autogen.sh")
     autotools.configure("--with-python \
                          --with-perl")
 
@@ -19,4 +20,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("ANNOUNCE", "CHANGES*", "FUTURE", "LICENSE", "NEW", "README", "TODO")
+    pisitools.dodoc("ANNOUNCE", "CHANGES*", "LICENSE", "README", "TODO")
