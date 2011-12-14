96c4b60fa9c7c03e4afe51fe9a107d345bf16a2b
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-14 10:08:44 +0200

    Version bump to 0.152

--- system/devel/elfutils/actions.py
+++ system/devel/elfutils/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -22,8 +21,8 @@ def setup():
                          --enable-thread-safety \
                          --disable-nls \
                          --with-zlib \
-                         --with-bzlib")
-                         # --with-lzma \
+                         --with-bzlib \
+                         --with-lzma")
 
 def build():
     autotools.make()
@@ -33,6 +32,8 @@ def check():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.remove("/usr/bin/eu-ld")
 
     # Don't remove all the static libs as libebl.a is needed by other packages
     pisitools.remove("/usr/lib/libelf.a")
