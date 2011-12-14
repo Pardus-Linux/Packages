d34865ee9d5d2a2764dac1f47397e5c37d67a415
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-14 13:32:12 +0200

    Version bump to 2.4.46

--- system/base/attr/actions.py
+++ system/base/attr/actions.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.rawConfigure("--libdir=/lib \
+    autotools.rawConfigure("--libdir=/usr/lib \
                             --mandir=/usr/share/man \
-                            --libexecdir=/lib \
-                            --bindir=/bin")
+                            --libexecdir=/usr/lib \
+                            --bindir=/usr/bin")
 def build():
     autotools.make()
 
 def install():
     autotools.make("DESTDIR=%s install install-lib install-dev" % get.installDIR())
 
-    pisitools.remove("/lib/*.a")
-    shelltools.chmod("%s/lib/libattr.so.*.*.*" % get.installDIR(), 0755)
+    pisitools.remove("/usr/lib/*.a")
+    shelltools.chmod("%s/usr/lib/libattr.so.*.*.*" % get.installDIR(), 0755)
+
+    pisitools.removeDir("/usr/share/doc/attr-*")
+    pisitools.dodoc("README", "doc/COPYING")
