cdeeb8ccf56810105a64acf6de433e2eba258626
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-15 15:08:01 +0200

    Bump to 1.0.9_rc3

--- system/base/libusb1/actions.py
+++ system/base/libusb1/actions.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "libusb-%s" % get.srcVERSION()
-
 def setup():
+    shelltools.system("./autogen.sh")
     autotools.configure("--disable-static")
 
 def build():
