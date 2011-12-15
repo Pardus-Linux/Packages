0a198d8c2f37550facb26ae1010a7f564753494b
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-15 15:46:52 +0200

    Bump to 005

--- system/base/usbutils/actions.py
+++ system/base/usbutils/actions.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    pisitools.dosed("lsusb.py", "share\/usb.ids", "share/misc/usb.ids")
+    shelltools.system("./autogen.sh")
     autotools.configure("--datadir=/usr/share/misc \
                          --disable-zlib")
 
