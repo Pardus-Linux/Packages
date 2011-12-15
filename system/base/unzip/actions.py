bf75d8a4864a32ed10d329e531cf45857feb1b9c
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-15 16:38:35 +0200

    Refactor

--- system/base/unzip/actions.py
+++ system/base/unzip/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -12,13 +11,9 @@ from pisi.actionsapi import get
 WorkDir="%s%s" % (get.srcNAME(), get.srcVERSION().replace(".",""))
 
 def build():
-    autotools.make('-f unix/Makefile LF2="" CC="%s" CF="%s -I. -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64" unzips' % (get.CC(),get.CFLAGS()))
+    autotools.make('-f unix/Makefile CC="%s" CF_NOOPT="%s -I. -D_FILE_OFFSET_BITS=64 -DUNIX" generic_gcc' % (get.CC(), get.CFLAGS()))
 
 def install():
-    for bin in ["unzip", "funzip", "unzipsfx", "unix/zipgrep"]:
-        pisitools.dobin(bin)
+    autotools.rawInstall("-f unix/Makefile prefix=%s/usr MANDIR=%s/usr/share/man/man1 INSTALL='cp -p'" % (get.installDIR(), get.installDIR()))
 
-    pisitools.dosym("/usr/bin/unzip", "/usr/bin/zipinfo")
-
-    pisitools.doman("man/*.1")
-    pisitools.dodoc("BUGS", "History*", "README", "ToDo", "WHERE")
+    pisitools.dodoc("BUGS", "History*", "LICENSE", "README", "ToDo", "WHERE")
