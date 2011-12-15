3ae39ba04a3bf952c2659f51556f8f53b4da1b0f
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-15 13:17:14 +0200

    Add Fedora patches

--- system/base/sed/actions.py
+++ system/base/sed/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,11 +9,13 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--enable-nls \
-                         --bindir=%s/bin" % get.installDIR())
+    autotools.configure()
 
 def build():
     autotools.make("LDFLAGS=%s" % get.LDFLAGS())
+
+def check():
+    autotools.make("check")
 
 def install():
     autotools.install()
