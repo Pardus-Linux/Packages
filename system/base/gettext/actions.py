c7d5123845b7e205034f023c19b4d3e82e7c7b15
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-15 12:01:27 +0200

    Refactor, add fedora patch

--- system/base/gettext/actions.py
+++ system/base/gettext/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -15,29 +14,24 @@ docdir = "/%s/%s" % (get.docDIR(), get.srcNAME())
 shelltools.export("LC_ALL", "C")
 
 def setup():
-    # shelltools.export("CFLAGS","%s -Dgcc_is_lint" % get.CFLAGS())
-    # shelltools.export("CXXFLAGS","%s -Dgcc_is_lint" % get.CXXFLAGS())
-
-    # Build with --without-included-gettext (will use that of glibc), as we
-    # need preloadable_libintl.so for new help2man
-
     # autoreconf breaks linker, graaaaaaaaggggghhhhhhh
-    # autotools.autoreconf("-vfi")
+    # External glib and croco generates cyclic dependency hell between glib, croco and gettext
+    shelltools.system("./autogen.sh")
     autotools.configure("--disable-java \
                          --disable-native-java \
                          --disable-csharp \
+                         --disable-git \
+                         --disable-rpath \
+                         --disable-static \
+                         --disable-openmp \
                          --without-included-gettext \
+                         --without-emacs \
                          --with-included-libcroco \
                          --with-included-glib \
                          --with-included-libxml \
                          --with-pic=yes \
-                         --without-emacs \
-                         --disable-openmp \
                          --enable-nls \
-                         --enable-shared \
-                         --disable-git \
-                         --disable-rpath \
-                         --disable-static")
+                         --enable-shared")
 
 def build():
     autotools.make("GMSGFMT=../src/msgfmt")
