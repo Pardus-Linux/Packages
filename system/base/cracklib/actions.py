032b4fde76b38d54ba491ee612c26a601717249b
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-16 16:46:56 +0200

    Bump to 2.8.18

--- system/base/cracklib/actions.py
+++ system/base/cracklib/actions.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
     autotools.configure("--with-default-dict=/usr/share/cracklib/pw_dict \
+                         --with-pic \
+                         --with-python \
                          --disable-static")
 
 def build():
     autotools.make("all")
 
+def check():
+    autotools.make("check")
+
 def install():
     autotools.install()
 
     # Create dictionary files
+    shelltools.export("LD_PRELOAD", "%s/usr/lib/libcrack.so" % get.installDIR())
     shelltools.system("cat /usr/share/dict/words|%s/usr/sbin/cracklib-packer %s/usr/share/cracklib/pw_dict" % (get.installDIR(),get.installDIR()))
 
     # pisitools.domo("po/tr.po","tr","cracklib.mo")
