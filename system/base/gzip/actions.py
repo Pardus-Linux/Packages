9e931204e9bd57632fc946fdcbfd39dc93411a7e
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-16 08:34:46 +0200

    Refactor, add Fedora patches.

--- system/base/gzip/actions.py
+++ system/base/gzip/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -12,7 +11,8 @@ from pisi.actionsapi import get
 
 def setup():
     shelltools.export("DEFS", "NO_ASM")
-    autotools.configure("--exec-prefix=/")
+    shelltools.export("CPPFLAGS", "-DHAVE_LSTAT")
+    autotools.configure()
 
 def build():
     autotools.make()
@@ -21,6 +21,8 @@ def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     # add missing gzcat
-    pisitools.dosym("zcat", "/bin/gzcat")
+    pisitools.dosym("zcat", "/usr/bin/gzcat")
+
+    pisitools.remove("/usr/bin/uncompress")
 
     pisitools.dodoc("ChangeLog", "NEWS", "README", "THANKS", "TODO")
