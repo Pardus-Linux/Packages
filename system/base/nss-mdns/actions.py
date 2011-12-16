9055362a1ac593c56e6ea3952d7954b4b4c2ddec
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-16 09:29:14 +0200

    Refactor, drop useless mdns.allow file

--- system/base/nss-mdns/actions.py
+++ system/base/nss-mdns/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,7 +9,8 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-legacy \
+    autotools.configure("--enable-legacy=no \
+                         --enable-avahi=yes \
                          --disable-lynx")
 
 def build():
@@ -18,4 +18,5 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("README")
+
+    pisitools.dodoc("README", "LICENSE")
