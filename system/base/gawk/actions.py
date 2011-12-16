7eb0845326418cb8b240b783aa712340b2d49262
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-16 10:27:48 +0200

    Bump to 4.0.0

--- system/base/gawk/actions.py
+++ system/base/gawk/actions.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2008 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def setup():
     shelltools.export("ac_cv_libsigsegv", "no")
     shelltools.export("AUTOPOINT", "true")
-    autotools.autoreconf("-vfi") # have a buggy mktime check
-
-    autotools.configure("--bindir=/bin \
-                         --enable-switch \
-                         --enable-nls")
+    autotools.configure("--with-libsigsegv-prefix=no")
 
 def build():
     autotools.make()
+
+def check():
+    autotools.make("-j1 check diffout")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     # Remove versioned binaries
-    pisitools.remove("/bin/*-*")
+    pisitools.remove("/usr/bin/*-%s" % get.srcVERSION())
 
     pisitools.dosym("gawk.1", "/usr/share/man/man1/awk.1")
+
     pisitools.dodoc("AUTHORS", "ChangeLog", "LIMITATIONS", "NEWS", "PROBLEMS", "README")
