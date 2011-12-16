eda152fd891613e02c1574065c383d0ba97a3785
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-16 14:23:10 +0200

    Bump to 1.5

--- system/base/miscfiles/actions.py
+++ system/base/miscfiles/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,9 +9,15 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure()
+    autotools.configure("--datadir=/usr/share/misc")
 
 def install():
-    autotools.rawInstall("prefix=%s/usr" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    for dic in ("connectives", "web2", "web2a", "propernames"):
+        pisitools.domove("/usr/share/misc/%s" % dic, "/usr/share/dict")
+
+    pisitools.dosym("web2", "/usr/share/dict/words")
+    pisitools.dosym("web2a", "/usr/share/dict/extra.words")
 
     pisitools.dodoc("GNU*", "NEWS", "ORIGIN", "README", "dict-README")
