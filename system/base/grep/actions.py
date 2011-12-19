6223292811fe72adf1e0f71077a59e0afc8aea63
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-19 15:26:48 +0200

    Bump to 2.10

--- system/base/grep/actions.py
+++ system/base/grep/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,10 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--enable-nls \
-                         --bindir=/bin \
-                         --without-included-regex \
-                         --enable-perl-regexp")
+    autotools.configure("--without-included-regex")
 
 def build():
     autotools.make()
