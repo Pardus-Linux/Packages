974d4b09ebff51d44907578aad6d86d57ae6f12c
Ozan Çağlayan <ozan@pardus.org.tr>
2011-12-19 15:29:48 +0200

    Refactor

--- system/base/dhcpcd/actions.py
+++ system/base/dhcpcd/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,19 +9,17 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--libexecdir=/lib/dhcpcd \
-                         --dbdir=/var/lib/dhcpcd \
-                         --sbindir=/sbin \
-                         --localstatedir=/var")
+    autotools.configure("--libexecdir=/usr/lib/dhcpcd \
+                         --dbdir=/var/lib/dhcpcd")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.rawInstall("DBDIR=/var/lib/dhcpcd LIBEXECDIR=/lib/dhcpcd DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DBDIR=/var/lib/dhcpcd LIBEXECDIR=/usr/lib/dhcpcd DESTDIR=%s" % get.installDIR())
 
     # Remove hooks install the compat one
-    pisitools.remove("/lib/dhcpcd/dhcpcd-hooks/*")
-    pisitools.insinto("/lib/dhcpcd/dhcpcd-hooks", "dhcpcd-hooks/50-dhcpcd-compat")
+    pisitools.remove("/usr/lib/dhcpcd/dhcpcd-hooks/*")
+    pisitools.insinto("/usr/lib/dhcpcd/dhcpcd-hooks", "dhcpcd-hooks/50-dhcpcd-compat")
 
     pisitools.dodoc("README")
