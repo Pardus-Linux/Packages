ca7d123bb2377514723c53e87610dbfbfde9ba05
Ozan Çağlayan <ozan@pardus.org.tr>
2012-01-19 14:23:59 +0200

    irqbalance: Bump to 1.0.3, add systemd support.

--- hardware/cpu/irqbalance/actions.py
+++ hardware/cpu/irqbalance/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -11,17 +10,14 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    for f in ("NEWS", "AUTHORS", "README", "ChangeLog"):
-        shelltools.touch("%s/%s" % (get.curDIR(), f))
-
-    shelltools.chmod("%s/autogen.sh" % get.curDIR())
-    shelltools.system("./autogen.sh")
-
     autotools.configure()
-    pisitools.dosed("Makefile", "\-g.*\-Os ", "")
 
 def build():
     autotools.make("CFLAGS='%s'" % get.CFLAGS())
 
 def install():
+    pisitools.insinto("/lib/systemd/system", "misc/irqbalance.service")
+    pisitools.doman("irqbalance.1")
     pisitools.dosbin("irqbalance")
+
+    pisitools.dodoc("COPYING", "NEWS", "README")
